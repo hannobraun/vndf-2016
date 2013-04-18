ba8d07ec8660eb6ae52ce2a16d56e2b6aad578e6
Hanno Braun <mail@hannobraun.com>
2013-04-18 22:10:51 +0200

    Updated app to open a window.
    The app will now open a window using GLFW. If the close button on the
    window is pressed, it is closed.
    This is an important step towards displaying graphics using OpenGL.

--- source/main.c
+++ source/main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
+#include "GL/glfw.h"
+
 int main(int argc, char const *argv[])
 {
-	printf("Hello, World!\n");
+	if (!glfwInit())
+	{
+		printf("Error initializing GLFW.\n");
+		return 1;
+	}
+
+	if (
+		!glfwOpenWindow(
+			800, 600,
+			8, 8, 8, 8,
+			0, 0,
+			GLFW_WINDOW))
+	{
+		printf("Error opening GLFW window.\n");
+		return 1;
+	}
+
+	while (glfwGetWindowParam(GLFW_OPENED))
+    {
+		glfwSwapBuffers();
+    }
+
 	return 0;
 }
