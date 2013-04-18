d6cc401757dfa233dc0e1dba161701ddb96820dc
Hanno Braun <mail@hannobraun.com>
2013-04-18 22:41:30 +0200

    Rendered triangle using OpenGL.

--- source/main.c
+++ source/main.c
@@ -24,8 +24,22 @@ int main(int argc, char const *argv[])
 		return 1;
 	}
 
+	glOrtho(
+		-screenWidth/2, screenWidth/2,
+		-screenHeight/2, screenHeight/2,
+		-1, 1);
+
 	while (glfwGetWindowParam(GLFW_OPENED))
     {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glBegin(GL_TRIANGLE_STRIP);
+			glVertex3f(  0.0f, 20.0f, 0.0f);
+			glVertex3f(-20.0f,-10.0f, 0.0f);
+			glVertex3f( 20.0f,-10.0f, 0.0f);
+		glEnd();
+
 		glfwSwapBuffers();
     }
 
