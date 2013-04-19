e4d54497dbc3d1b7ae163f0144e0f2029ccf7328
Hanno Braun <mail@hannobraun.com>
2013-04-19 16:14:39 +0200

    Refactoring.
    Moved all render code into a single render function.

--- source/main.c
+++ source/main.c
@@ -2,8 +2,13 @@
 
 #include "GL/glfw.h"
 
+
 const int screenWidth  = 800;
 const int screenHeight = 600;
+
+
+void render();
+
 
 int main(int argc, char const *argv[])
 {
@@ -28,24 +33,28 @@ int main(int argc, char const *argv[])
 		glfwGetWindowParam(GLFW_OPENED) &&
 		glfwGetKey(GLFW_KEY_ESC) == GLFW_RELEASE)
     {
-		glClear(GL_COLOR_BUFFER_BIT);
-		glLoadIdentity();
-		glOrtho(
-			-screenWidth/2, screenWidth/2,
-			-screenHeight/2, screenHeight/2,
-			-1, 1);
-
-		glTranslatef(50.0f, 0.0f, 0.0f);
-
-		glColor3f(0.0f, 0.0f, 1.0f);
-		glBegin(GL_TRIANGLE_STRIP);
-			glVertex3f(  0.0f, 20.0f, 0.0f);
-			glVertex3f(-20.0f,-10.0f, 0.0f);
-			glVertex3f( 20.0f,-10.0f, 0.0f);
-		glEnd();
-
-		glfwSwapBuffers();
+		render();
     }
 
 	return 0;
+}
+
+void render() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glLoadIdentity();
+	glOrtho(
+		-screenWidth/2, screenWidth/2,
+		-screenHeight/2, screenHeight/2,
+		-1, 1);
+
+	glTranslatef(50.0f, 0.0f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glBegin(GL_TRIANGLE_STRIP);
+		glVertex3f(  0.0f, 20.0f, 0.0f);
+		glVertex3f(-20.0f,-10.0f, 0.0f);
+		glVertex3f( 20.0f,-10.0f, 0.0f);
+	glEnd();
+
+	glfwSwapBuffers();
 }
