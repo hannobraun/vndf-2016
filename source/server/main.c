8e0d405521b2957f4d84f97851c84bba7a90f9f7
Hanno Braun <mail@hannobraun.com>
2013-04-19 22:23:31 +0200

    Extended server to accept connection.
    The server will set up a socket to listen on, accept a single
    connection and then exit.

--- source/server/main.c
+++ source/server/main.c
@@ -1,8 +1,71 @@
+#include <errno.h>
 #include <stdio.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <netdb.h>
 
 
 int main(int argc, char const *argv[])
 {
-	printf("Hello, World!\n");
+	int status;
+
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family   = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags    = AI_PASSIVE;
+
+	struct addrinfo *servinfo;
+
+	status = getaddrinfo(NULL, "34481", &hints, &servinfo);
+
+	if (status != 0)
+	{
+		printf("Error getting address info: %s\n", strerror(errno));
+		return 1;
+	}
+
+	int socket_fd = socket(
+		servinfo->ai_family,
+		servinfo->ai_socktype,
+		servinfo->ai_protocol);
+
+	if (socket_fd == -1)
+	{
+		printf("Error creating socket: %s\n", strerror(errno));
+		return 1;
+	}
+
+	int yes=1;
+	if (setsockopt(socket_fd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int)) == -1) {
+		printf("Error setting socket option: %s", strerror(errno));
+		return 1;
+	}
+
+	status = bind(socket_fd, servinfo->ai_addr, servinfo->ai_addrlen);
+	if (status != 0)
+	{
+		printf("Error binding socket: %s\n", strerror(errno));
+		return 1;
+	}
+
+	status = listen(socket_fd, 20);
+	if (status != 0)
+	{
+		printf("Error listening on socket: %s\n", strerror(errno));
+		return 1;
+	}
+
+	struct sockaddr_storage remote_address;
+	socklen_t address_size = sizeof remote_address;
+
+	int client_fd = accept(
+		socket_fd,
+		(struct sockaddr *)&remote_address,
+		&address_size);
+
+	freeaddrinfo(servinfo);
+
 	return 0;
 }
