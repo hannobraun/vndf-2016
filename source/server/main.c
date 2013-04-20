db4a9044880fe71308be130753a34e8b79dd5338
Hanno Braun <mail@hannobraun.com>
2013-04-20 21:45:08 +0200

    Refactoring.
    Replaced use of "return 1" to return error codes with "exit(1)" in the
    server, too. Again, this makes the code more robust in the face of
    refactorings.

--- source/server/main.c
+++ source/server/main.c
@@ -1,5 +1,6 @@
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include <sys/socket.h>
@@ -23,7 +24,7 @@ int main(int argc, char const *argv[])
 	if (status != 0)
 	{
 		printf("Error getting address info: %s\n", strerror(errno));
-		return 1;
+		exit(1);
 	}
 
 	int socket_fd = socket(
@@ -34,28 +35,28 @@ int main(int argc, char const *argv[])
 	if (socket_fd == -1)
 	{
 		printf("Error creating socket: %s\n", strerror(errno));
-		return 1;
+		exit(1);
 	}
 
 	int yes=1;
 	if (setsockopt(socket_fd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int)) == -1)
 	{
 		printf("Error setting socket option: %s", strerror(errno));
-		return 1;
+		exit(1);
 	}
 
 	status = bind(socket_fd, servinfo->ai_addr, servinfo->ai_addrlen);
 	if (status != 0)
 	{
 		printf("Error binding socket: %s\n", strerror(errno));
-		return 1;
+		exit(1);
 	}
 
 	status = listen(socket_fd, 20);
 	if (status != 0)
 	{
 		printf("Error listening on socket: %s\n", strerror(errno));
-		return 1;
+		exit(1);
 	}
 
 	struct sockaddr_storage remote_address;
@@ -72,12 +73,12 @@ int main(int argc, char const *argv[])
 	if (bytes_sent < 0)
 	{
 		printf("Error sending message: %s\n", strerror(errno));
-		return 1;
+		exit(1);
 	}
 	if ((size_t)bytes_sent != message_length)
 	{
 		printf("Only sent %ld of %lu bytes.\n", bytes_sent, message_length);
-		return 1;
+		exit(1);
 	}
 
 	freeaddrinfo(servinfo);
