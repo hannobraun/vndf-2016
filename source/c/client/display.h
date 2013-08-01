4390533d478827501e1df6f9156a92cef88f7933
Hanno Braun <mail@hannobraun.com>
2013-08-01 12:04:09 +0200

    Added camera struct.
    This will make camera-related code more clear and concise.

--- source/c/client/display.h
+++ source/c/client/display.h
@@ -1,5 +1,11 @@
 #include "pos.h"
 
 
+typedef struct {
+	float h;
+	float v;
+} camera;
+
+
 GLuint display_init(void);
 void display_render(GLfloat h, GLfloat v, posMap positions, GLuint textureName);
