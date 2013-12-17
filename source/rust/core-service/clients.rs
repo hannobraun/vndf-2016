8d43a97ff6f50ca108212366e3b6b53b3e7cbbfc
Hanno Braun <mail@hannobraun.com>
2013-12-17 23:31:17 +0100

    Replaced use of int in clients.rs with c_int.
    This is closer to the original, although it might not make a difference.

--- source/rust/core-service/clients.rs
+++ source/rust/core-service/clients.rs
@@ -29,7 +29,7 @@ struct Stack {
 }
 
 struct Client {
-	socketFD: int,
+	socketFD: std::libc::c_int,
 	id:       std::libc::size_t,
 	ship:     dynamics::Body
 }
@@ -67,7 +67,7 @@ pub extern fn clients_canAdd(c: &ClientMap) -> bool {
 }
 
 #[no_mangle]
-pub extern fn clients_add(c: &mut ClientMap, socketFD: int, pos: vec::Vec2, vel: vec::Vec2) {
+pub extern fn clients_add(c: &mut ClientMap, socketFD: std::libc::c_int, pos: vec::Vec2, vel: vec::Vec2) {
 	// Get id from pool.
 	let clientId = unsafe {
 		let ptr = std::ptr::mut_offset(c.idPool.elems, c.idPool.size as int);
