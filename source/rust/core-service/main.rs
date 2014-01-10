d0dc70d3b461591ba0bb05487e6703b3b48fd274
Hanno Braun <mail@hannobraun.de>
2014-01-10 16:18:06 +0100

    Refactoring.
    Subsumed net crate into core service crate.

--- source/rust/core-service/main.rs
+++ source/rust/core-service/main.rs
@@ -3,8 +3,8 @@
 #[crate_id = "core-service"];
 
 extern mod common;
-extern mod net;
 
 pub mod clients;
 pub mod events;
+pub mod net;
 pub mod protocol;
--- source/rust/core-service/net.rs
+++ source/rust/core-service/net.rs
@@ -1,41 +1,36 @@
-#[crate_type = "rlib"];
-#[crate_type = "staticlib"];
-#[crate_id = "net"];
-
-
 extern {
-	fn epoll_create(size: std::libc::c_int) -> std::libc::c_int;
-	fn epoll_ctl(epfd: std::libc::c_int, op: std::libc::c_int, fd: std::libc::c_int, event: *EpollEvent) -> std::libc::c_int;
-	fn getaddrinfo(name: *std::libc::c_char, service: *std::libc::c_char, req: *AddrInfo, pai: **AddrInfo) -> std::libc::c_int;
-	fn socket(domain: std::libc::c_int, theType: std::libc::c_int, protocol: std::libc::c_int) -> std::libc::c_int;
-	fn setsockopt(sockfd: std::libc::c_int, level: std::libc::c_int, optname: std::libc::c_int, optval: *std::libc::c_void, optlen: std::libc::c_uint) -> std::libc::c_int;
-	fn bind(sockfd: std::libc::c_int, addr: *SockAddr, addrlen: std::libc::c_uint) -> std::libc::c_int;
-	fn listen(sockfd: std::libc::c_int, backlog: std::libc::c_int) -> std::libc::c_int;
+	fn epoll_create(size: ::std::libc::c_int) -> ::std::libc::c_int;
+	fn epoll_ctl(epfd: ::std::libc::c_int, op: ::std::libc::c_int, fd: ::std::libc::c_int, event: *EpollEvent) -> ::std::libc::c_int;
+	fn getaddrinfo(name: *::std::libc::c_char, service: *::std::libc::c_char, req: *AddrInfo, pai: **AddrInfo) -> ::std::libc::c_int;
+	fn socket(domain: ::std::libc::c_int, theType: ::std::libc::c_int, protocol: ::std::libc::c_int) -> ::std::libc::c_int;
+	fn setsockopt(sockfd: ::std::libc::c_int, level: ::std::libc::c_int, optname: ::std::libc::c_int, optval: *::std::libc::c_void, optlen: ::std::libc::c_uint) -> ::std::libc::c_int;
+	fn bind(sockfd: ::std::libc::c_int, addr: *SockAddr, addrlen: ::std::libc::c_uint) -> ::std::libc::c_int;
+	fn listen(sockfd: ::std::libc::c_int, backlog: ::std::libc::c_int) -> ::std::libc::c_int;
 	fn freeaddrinfo(res: *AddrInfo);
-	fn accept(sockfd: std::libc::c_int, addr: *SockAddr, addrlen: *std::libc::c_uint) -> std::libc::c_int;
-	fn send(sockfd: std::libc::c_int, buf: *std::libc::c_void, len: std::libc::size_t, flags: std::libc::c_int) -> std::libc::ssize_t;
+	fn accept(sockfd: ::std::libc::c_int, addr: *SockAddr, addrlen: *::std::libc::c_uint) -> ::std::libc::c_int;
+	fn send(sockfd: ::std::libc::c_int, buf: *::std::libc::c_void, len: ::std::libc::size_t, flags: ::std::libc::c_int) -> ::std::libc::ssize_t;
 }
 
 
 struct Net {
-	pollerFD: std::libc::c_int,
-	serverFD: std::libc::c_int
+	pollerFD: ::std::libc::c_int,
+	serverFD: ::std::libc::c_int
 }
 
 struct AddrInfo {
-	ai_flags    : std::libc::c_int,
-	ai_family   : std::libc::c_int,
-	ai_socktype : std::libc::c_int,
-	ai_protocol : std::libc::c_int,
+	ai_flags    : ::std::libc::c_int,
+	ai_family   : ::std::libc::c_int,
+	ai_socktype : ::std::libc::c_int,
+	ai_protocol : ::std::libc::c_int,
 	ai_addrlen  : u32,
 	ai_addr     : *SockAddr,
-	ai_canonname: *std::libc::c_char,
+	ai_canonname: *::std::libc::c_char,
 	ai_next     : *AddrInfo
 }
 
 struct SockAddr {
-	sa_family: std::libc::c_ushort,
-	sa_data  : [std::libc::c_char, ..14]
+	sa_family: ::std::libc::c_ushort,
+	sa_data  : [::std::libc::c_char, ..14]
 }
 
 struct EpollEvent {
@@ -45,7 +40,7 @@ struct EpollEvent {
 
 
 #[no_mangle]
-pub extern fn net_init(port: *std::libc::c_char) -> Net {
+pub extern fn net_init(port: *::std::libc::c_char) -> Net {
 	let serverFD = net_initSocket(port);
 	let pollerFD = initPoller();
 
@@ -58,7 +53,7 @@ pub extern fn net_init(port: *std::libc::c_char) -> Net {
 
 
 #[no_mangle]
-pub extern fn net_initSocket(port: *std::libc::c_char) -> std::libc::c_int {
+pub extern fn net_initSocket(port: *::std::libc::c_char) -> ::std::libc::c_int {
 	let AI_PASSIVE  = 1;
 	let AF_UNSPEC   = 0;
 	let SOCK_STREAM = 1;
@@ -69,24 +64,24 @@ pub extern fn net_initSocket(port: *std::libc::c_char) -> std::libc::c_int {
 		ai_socktype : SOCK_STREAM,
 		ai_protocol : 0,
 		ai_addrlen  : 0,
-		ai_addr     : std::ptr::null(),
-		ai_canonname: std::ptr::null(),
-		ai_next     : std::ptr::null() };
+		ai_addr     : ::std::ptr::null(),
+		ai_canonname: ::std::ptr::null(),
+		ai_next     : ::std::ptr::null() };
 
-	let servinfo = std::ptr::null::<AddrInfo>();
+	let servinfo = ::std::ptr::null::<AddrInfo>();
 
 	unsafe {
 		let status = getaddrinfo(
-			std::ptr::null(),
+			::std::ptr::null(),
 			port,
 			&hints,
 			&servinfo);
 
 		if status != 0 {
 			"Error getting address info".to_c_str().with_ref(|c_str| {
-				std::libc::perror(c_str);
+				::std::libc::perror(c_str);
 			});
-			std::libc::exit(1);
+			::std::libc::exit(1);
 		}
 
 	};
@@ -99,9 +94,9 @@ pub extern fn net_initSocket(port: *std::libc::c_char) -> std::libc::c_int {
 
 		if (socketFD == -1) {
 			"Error creating socket".to_c_str().with_ref(|c_str| {
-				std::libc::perror(c_str);
+				::std::libc::perror(c_str);
 			});
-			std::libc::exit(1);
+			::std::libc::exit(1);
 		}
 
 		socketFD };
@@ -115,14 +110,14 @@ pub extern fn net_initSocket(port: *std::libc::c_char) -> std::libc::c_int {
 			socketFD,
 			SOL_SOCKET,
 			SO_REUSEADDR,
-			std::ptr::to_unsafe_ptr(&yes) as *std::libc::c_void,
-			std::mem::size_of::<std::libc::c_int>() as u32);
+			::std::ptr::to_unsafe_ptr(&yes) as *::std::libc::c_void,
+			::std::mem::size_of::<::std::libc::c_int>() as u32);
 
 		if status == -1 {
 			"Error setting socket option".to_c_str().with_ref(|c_str| {
-				std::libc::perror(c_str);
+				::std::libc::perror(c_str);
 			});
-			std::libc::exit(1);
+			::std::libc::exit(1);
 		}
 	}
 
@@ -134,9 +129,9 @@ pub extern fn net_initSocket(port: *std::libc::c_char) -> std::libc::c_int {
 
 		if status != 0 {
 			"Error binding socket".to_c_str().with_ref(|c_str| {
-				std::libc::perror(c_str);
+				::std::libc::perror(c_str);
 			});
-			std::libc::exit(1);
+			::std::libc::exit(1);
 		}
 	}
 
@@ -146,9 +141,9 @@ pub extern fn net_initSocket(port: *std::libc::c_char) -> std::libc::c_int {
 			1024);
 		if status != 0 {
 			"Error listening on socket".to_c_str().with_ref(|c_str| {
-				std::libc::perror(c_str);
+				::std::libc::perror(c_str);
 			});
-			std::libc::exit(1);
+			::std::libc::exit(1);
 		}
 	}
 
@@ -160,14 +155,14 @@ pub extern fn net_initSocket(port: *std::libc::c_char) -> std::libc::c_int {
 }
 
 #[no_mangle]
-pub extern fn initPoller() -> std::libc::c_int {
+pub extern fn initPoller() -> ::std::libc::c_int {
 	unsafe {
 		let pollerFD = epoll_create(1);
 		if pollerFD < 0 {
 			"Error initiating epoll".to_c_str().with_ref(|c_str| {
-				std::libc::perror(c_str);
+				::std::libc::perror(c_str);
 			});
-			std::libc::exit(1);
+			::std::libc::exit(1);
 		}
 
 		pollerFD
@@ -175,41 +170,41 @@ pub extern fn initPoller() -> std::libc::c_int {
 }
 
 #[no_mangle]
-pub extern fn registerAccept(pollerFD: std::libc::c_int, serverFD: std::libc::c_int) {
+pub extern fn registerAccept(pollerFD: ::std::libc::c_int, serverFD: ::std::libc::c_int) {
 	let EPOLLIN = 1;
 	let EPOLL_CTL_ADD = 1;
 
 	let event = EpollEvent { events: EPOLLIN, data: 0 };
 
 	unsafe {
-		let status = epoll_ctl(pollerFD, EPOLL_CTL_ADD, serverFD, std::ptr::to_unsafe_ptr(&event));
+		let status = epoll_ctl(pollerFD, EPOLL_CTL_ADD, serverFD, ::std::ptr::to_unsafe_ptr(&event));
 		if status != 0 {
 			"Error registering server socket with epoll".to_c_str().with_ref(|c_str| {
-				std::libc::perror(c_str);
+				::std::libc::perror(c_str);
 			});
-			std::libc::exit(1);
+			::std::libc::exit(1);
 		}
 	}
 }
 
 #[no_mangle]
-pub extern fn net_acceptClient(serverFD: std::libc::c_int) -> std::libc::c_int {
+pub extern fn net_acceptClient(serverFD: ::std::libc::c_int) -> ::std::libc::c_int {
 	unsafe {
 		accept(
 			serverFD,
-			std::ptr::null(),
-			std::ptr::null())
+			::std::ptr::null(),
+			::std::ptr::null())
 	}
 }
 
 #[no_mangle]
-pub extern fn net_send(clientFD: std::libc::c_int, message: *std::libc::c_char, messageLength: std::libc::size_t) -> std::libc::c_int {
+pub extern fn net_send(clientFD: ::std::libc::c_int, message: *::std::libc::c_char, messageLength: ::std::libc::size_t) -> ::std::libc::c_int {
 	let MSG_NOSIGNAL = 0x4000;
 
 	unsafe {
 		let bytesSent = send(
 			clientFD,
-			message as *std::libc::c_void,
+			message as *::std::libc::c_void,
 			messageLength,
 			MSG_NOSIGNAL);
 
@@ -221,7 +216,7 @@ pub extern fn net_send(clientFD: std::libc::c_int, message: *std::libc::c_char, 
 				"Only sent {:d} of {:u} bytes.\n",
 				bytesSent,
 				messageLength);
-			std::libc::exit(1)
+			::std::libc::exit(1)
 
 		}
 		else {
