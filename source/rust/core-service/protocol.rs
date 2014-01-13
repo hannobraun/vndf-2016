53c846d31dc97f78f0751b7c2d386c82fbfd8f4d
Hanno Braun <mail@hannobraun.de>
2014-01-13 22:08:03 +0100

    Refactoring.

--- source/rust/core-service/protocol.rs
+++ source/rust/core-service/protocol.rs
@@ -9,44 +9,44 @@ pub fn send_update(
 	yPos    : libc::c_double) -> libc::c_int {
 
 	unsafe {
-		let mut message: [libc::c_char, ..256] = [0, ..256];
+		let mut buffer: [libc::c_char, ..256] = [0, ..256];
 
 		("UPDATE id: " +id.to_str()+ ", pos: (" +xPos.to_str()+ ", " +yPos.to_str()+ ")").to_c_str().with_ref(|c_str| {
 			let messageLength = libc::strlen(c_str);
 
 			ptr::set_memory(
-				message.as_mut_ptr(),
+				buffer.as_mut_ptr(),
 				(messageLength + 1) as u8,
 				1);
 
 			ptr::copy_memory(
-				ptr::mut_offset(message.as_mut_ptr(), 1),
+				ptr::mut_offset(buffer.as_mut_ptr(), 1),
 				c_str,
 				messageLength as uint);
 
-			::net::send_message(clientFD, message.as_ptr(), messageLength + 1)
+			::net::send_message(clientFD, buffer.as_ptr(), messageLength + 1)
 		})
 	}
 }
 
 pub fn send_remove(clientFD: libc::c_int, id: libc::size_t) -> libc::c_int {
 	unsafe {
-		let mut message: [libc::c_char, ..256] = [0, ..256];
+		let mut buffer: [libc::c_char, ..256] = [0, ..256];
 
 		("REMOVE id: " + id.to_str()).to_c_str().with_ref(|c_str| {
 			let messageLength = libc::strlen(c_str);
 
 			ptr::set_memory(
-				message.as_mut_ptr(),
+				buffer.as_mut_ptr(),
 				(messageLength + 1) as u8,
 				1);
 
 			ptr::copy_memory(
-				ptr::mut_offset(message.as_mut_ptr(), 1),
+				ptr::mut_offset(buffer.as_mut_ptr(), 1),
 				c_str,
 				messageLength as uint);
 
-			::net::send_message(clientFD, message.as_ptr(), messageLength + 1)
+			::net::send_message(clientFD, buffer.as_ptr(), messageLength + 1)
 		})
 	}
 }
