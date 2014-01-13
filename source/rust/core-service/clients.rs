8de82155de4d6ee3d9d53129740327bf0bf7bd1c
Hanno Braun <mail@hannobraun.de>
2014-01-13 23:09:40 +0100

    Refactoring.
    Converted stack's init function into a constructor.

--- source/rust/core-service/clients.rs
+++ source/rust/core-service/clients.rs
@@ -33,22 +33,29 @@ struct Client {
 }
 
 impl Stack {
-	fn init(&mut self, capacity: libc::size_t) {
+	fn new(capacity: libc::size_t) -> Stack {
 		let idPoolSize =
 			capacity * ::std::mem::size_of::<libc::size_t>() as libc::size_t;
 
-		self.cap = capacity;
-		self.size = 0;
-		self.elems = unsafe {
+		let mut stack = Stack {
+			cap  : 0,
+			size : 0,
+			elems: ::std::ptr::null::<u64>() as *mut u64 };
+
+		stack.cap = capacity;
+		stack.size = 0;
+		stack.elems = unsafe {
 			libc::malloc(idPoolSize) as *mut libc::size_t };
 
-		while self.size < capacity {
+		while stack.size < capacity {
 			unsafe {
-				let ptr = ptr::mut_offset(self.elems, self.size as int);
-				*ptr = (capacity - self.size - 1) as libc::size_t; };
+				let ptr = ptr::mut_offset(stack.elems, stack.size as int);
+				*ptr = (capacity - stack.size - 1) as libc::size_t; };
 
-			self.size += 1;
+			stack.size += 1;
 		}
+
+		stack
 	}
 }
 
@@ -58,18 +65,13 @@ pub fn new_client_map(cap: libc::size_t) -> ~ClientMap {
 		clients: IdMap {
 			cap  : 0,
 			elems: ::std::ptr::null::<IdMapEntry>() as *mut IdMapEntry },
-		idPool: Stack {
-			cap  : 0,
-			size : 0,
-			elems: ::std::ptr::null::<u64>() as *mut u64 } };
+		idPool: Stack::new(cap) };
 
 	// Init IdMap
 	c.clients.cap = cap;
 	let memSize = cap * ::std::mem::size_of::<IdMapEntry>() as libc::size_t;
 	c.clients.elems = unsafe { libc::malloc(memSize) as *mut IdMapEntry };
 	unsafe { ptr::set_memory(c.clients.elems, 0, cap as uint) };
-
-	c.idPool.init(cap);
 
 	~c
 }
