7715c25c738a65e32272658d823fac80953466b4
Hanno Braun <mail@hannobraun.de>
2014-01-13 22:38:17 +0100

    Refactoring.

--- source/rust/core-service/clients.rs
+++ source/rust/core-service/clients.rs
@@ -1,4 +1,5 @@
 use std::libc;
+use std::ptr;
 
 use common::dynamics;
 
@@ -36,7 +37,7 @@ pub fn init_client_map(c: &mut ClientMap, cap: libc::size_t) {
 	c.clients.cap = cap;
 	let memSize = cap * ::std::mem::size_of::<IdMapEntry>() as libc::size_t;
 	c.clients.elems = unsafe { libc::malloc(memSize) as *mut IdMapEntry };
-	unsafe { ::std::ptr::set_memory(c.clients.elems, 0, cap as uint) };
+	unsafe { ptr::set_memory(c.clients.elems, 0, cap as uint) };
 
 	// Init Stack
 	c.idPool.cap = cap;
@@ -50,7 +51,7 @@ pub fn init_client_map(c: &mut ClientMap, cap: libc::size_t) {
 	let mut i: int = 0;
 	while i < cap as int {
 		unsafe {
-			let ptr = ::std::ptr::mut_offset(c.idPool.elems, i);
+			let ptr = ptr::mut_offset(c.idPool.elems, i);
 			*ptr = (cap as int - i - 1) as libc::size_t; };
 		i += 1;
 	}
@@ -63,7 +64,7 @@ pub fn can_add(c: &ClientMap) -> bool {
 pub fn add(c: &mut ClientMap, socketFD: libc::c_int, pos: ::common::vec::Vec2, vel: ::common::vec::Vec2) {
 	// Get id from pool.
 	let clientId = unsafe {
-		let ptr = ::std::ptr::mut_offset(c.idPool.elems, (c.idPool.size - 1) as int);
+		let ptr = ptr::mut_offset(c.idPool.elems, (c.idPool.size - 1) as int);
 		*ptr };
 	c.idPool.size -= 1;
 
@@ -75,7 +76,7 @@ pub fn add(c: &mut ClientMap, socketFD: libc::c_int, pos: ::common::vec::Vec2, v
 
 	// Add client to map
 	unsafe {
-		let ptr = ::std::ptr::mut_offset(c.clients.elems, clientId as int);
+		let ptr = ptr::mut_offset(c.clients.elems, clientId as int);
 		(*ptr).isOccupied = 1;
 		(*ptr).value = client;
 	};
@@ -83,7 +84,7 @@ pub fn add(c: &mut ClientMap, socketFD: libc::c_int, pos: ::common::vec::Vec2, v
 
 pub fn remove(c: &mut ClientMap, id: libc::size_t) {
 	unsafe {
-		let clientPtr = ::std::ptr::mut_offset(c.clients.elems, id as int);
+		let clientPtr = ptr::mut_offset(c.clients.elems, id as int);
 		let containsClient = (*clientPtr).isOccupied == 1;
 
 		if containsClient {
@@ -92,7 +93,7 @@ pub fn remove(c: &mut ClientMap, id: libc::size_t) {
 
 			// Add id back to pool
 			let idPtr =
-				::std::ptr::mut_offset(c.idPool.elems, c.idPool.size as int);
+				ptr::mut_offset(c.idPool.elems, c.idPool.size as int);
 			(*idPtr) = id;
 			c.idPool.size += 1;
 		}
