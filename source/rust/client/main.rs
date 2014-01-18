d22a375e8c6e32bd3f194a3a5c2214a3efd38fe4
Hanno Braun <mail@hannobraun.de>
2014-01-18 09:06:03 +0100

    Rewrote net_connect in Rust.
    This is the last function of the client's net module.

--- source/rust/client/main.rs
+++ source/rust/client/main.rs
@@ -1,4 +1,6 @@
 #[crate_id = "client"];
 #[crate_type = "staticlib"];
 
+extern mod common;
+
 pub mod net;
--- source/rust/client/net.rs
+++ source/rust/client/net.rs
@@ -1,9 +1,13 @@
 use std::libc;
+use std::ptr;
+
+use common::net;
 
 
 extern {
 	fn __errno_location() -> *libc::c_int;
 	fn recv(sockfd: libc::c_int, buf: *libc::c_void, len: libc::size_t, flags: libc::c_int) -> libc::ssize_t;
+	fn connect(sockfd: libc::c_int, addr: *net::SockAddr, addrlen: libc::c_uint) -> libc::c_int;
 }
 
 fn errno() -> libc::c_int {
@@ -12,6 +16,62 @@ fn errno() -> libc::c_int {
 	}
 }
 
+
+#[no_mangle]
+pub extern fn net_connect(hostname: *libc::c_char, port: *libc::c_char) -> libc::c_int {
+	let hints = net::AddrInfo {
+		ai_flags    : net::AI_PASSIVE,
+		ai_family   : net::AF_UNSPEC,
+		ai_socktype : net::SOCK_STREAM,
+		ai_protocol : 0,
+		ai_addrlen  : 0,
+		ai_addr     : ptr::null(),
+		ai_canonname: ptr::null(),
+		ai_next     : ptr::null() };
+
+	let servinfo = ptr::null::<net::AddrInfo>();
+
+	unsafe {
+		let mut status = net::getaddrinfo(
+			hostname,
+			port,
+			&hints,
+			&servinfo);
+
+		if status != 0 {
+			"Error getting address info".to_c_str().with_ref(|c_message| {
+				libc::perror(c_message);
+			});
+			libc::exit(1);
+		}
+
+		let socketFD = net::socket(
+			(*servinfo).ai_family,
+			(*servinfo).ai_socktype,
+			(*servinfo).ai_protocol);
+		if socketFD == -1 {
+			"Error creating socket".to_c_str().with_ref(|c_message| {
+				libc::perror(c_message);
+			});
+			libc::exit(1);
+		}
+
+		status = connect(
+			socketFD,
+			(*servinfo).ai_addr,
+			(*servinfo).ai_addrlen);
+		if status != 0 {
+			"Error connecting to server".to_c_str().with_ref(|c_message| {
+				libc::perror(c_message);
+			});
+			libc::exit(1);
+		}
+
+		net::freeaddrinfo(servinfo);
+
+		socketFD
+	}
+}
 
 #[no_mangle]
 pub extern fn net_receive(socketFD: libc::c_int, buffer: *libc::c_char, bufferSize: libc::size_t) -> libc::ssize_t {
