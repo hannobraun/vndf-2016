0a93d4e7d625bfa400c0ac8c43662755b6553292
Hanno Braun <mail@hannobraun.de>
2014-02-13 18:59:11 +0100

    Rewrote main function in Rust.
    This concludes the rewrite of the client's main module.

--- source/rust/client/main.rs
+++ source/rust/client/main.rs
@@ -1,6 +1,3 @@
-#[crate_id = "client"];
-#[crate_type = "staticlib"];
-
 extern mod common;
 
 extern mod gl;
@@ -9,7 +6,11 @@ extern mod stb_image;
 
 
 use std::from_str;
+use std::io;
 use std::libc;
+use std::mem;
+use std::os;
+use std::path;
 use std::ptr;
 use std::str;
 
@@ -35,6 +36,73 @@ struct Connection {
 	bufferPos: libc::c_int
 }
 
+
+fn main() {
+	let screenWidth  = 800;
+	let screenHeight = 600;
+
+	let args = os::args();
+
+	if args.len() > 2 {
+		print!("Usage: {:s} serverAddress\n", args[0]);
+		return
+	}
+
+	let serverAddress = if args.len() == 2 {
+		args[1]
+	}
+	else {
+		let mut file = match io::File::open(&path::posix::Path::new("server")) {
+			Ok(file) => file,
+			Err(e)   => {
+				print!("ERROR {}\n", e);
+				fail!();
+			}
+		};
+
+		let contents = match file.read_to_end() {
+			Ok(contents) => contents,
+			Err(e)       => {
+				print!("ERROR {}\n", e);
+				fail!();
+			}
+		};
+
+		str::from_utf8(contents).unwrap_or_else(|| { fail!() }).to_owned()
+	};
+
+	let window  = display::display_init(screenWidth, screenHeight);
+	let texture = images::images_load();
+
+	let socketFD = serverAddress.to_c_str().with_ref(|c_address| {
+		"34481".to_c_str().with_ref(|c_port| {
+			net::net_connect(c_address, c_port)
+		})
+	});
+
+	let mut c = Connection {
+		socketFD : socketFD,
+		buffer   : [0, ..BUFFER_SIZE],
+		bufferPos: 0 };
+
+	unsafe {
+		let positions = display::PosMap {
+			cap  : 4,
+			elems: libc::malloc(4 * mem::size_of::<display::PosMapEntry>() as u64) as *mut display::PosMapEntry };
+		ptr::zero_memory(positions.elems, 4);
+
+		let mut cam = camera::Camera {
+			v: 0.0f32,
+			h: 0.0f32 };
+
+		while glfw::ffi::glfwWindowShouldClose(window) == 0 &&
+			glfw::ffi::glfwGetKey(window, glfw::ffi::KEY_ESCAPE) == glfw::ffi::RELEASE {
+			receivePositions(&mut c, positions);
+			input::input_apply(window, &mut cam);
+			display::display_render(window, cam, positions, texture);
+		}
+	};
+}
 
 #[no_mangle]
 pub extern fn receivePositions(c: *mut Connection, positions: display::PosMap) {
