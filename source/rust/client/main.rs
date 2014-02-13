2719e0cf98722463f26d50778525bd5872344867
Hanno Braun <mail@hannobraun.de>
2014-02-13 17:50:47 +0100

    Rewrote receivePositions in Rust.

--- source/rust/client/main.rs
+++ source/rust/client/main.rs
@@ -7,6 +7,13 @@ extern mod gl;
 extern mod glfw;
 extern mod stb_image;
 
+
+use std::from_str;
+use std::libc;
+use std::ptr;
+use std::str;
+
+
 pub mod camera;
 pub mod display;
 pub mod images;
@@ -14,5 +21,74 @@ pub mod input;
 pub mod net;
 pub mod texture;
 
+
 #[link(name = "stb-image", kind = "static")]
 extern {}
+
+
+static BUFFER_SIZE : libc::c_int = 256;
+
+
+struct Connection {
+	socketFD : libc::c_int,
+	buffer   : [libc::c_char, ..BUFFER_SIZE],
+	bufferPos: libc::c_int
+}
+
+
+#[no_mangle]
+pub extern fn receivePositions(c: *mut Connection, positions: display::PosMap) {
+	unsafe {
+		let bytesReceived = net::net_receive(
+			(*c).socketFD,
+			ptr::offset((*c).buffer.as_ptr(), (*c).bufferPos as int),
+			(BUFFER_SIZE - (*c).bufferPos) as u64);
+
+		(*c).bufferPos += bytesReceived as i32;
+
+		while (*c).bufferPos > 0 && (*c).buffer[0] as i32 <= (*c).bufferPos {
+			let messageSize = (*c).buffer[0];
+			assert!(messageSize >= 0);
+
+			let message = str::raw::from_buf_len(
+				ptr::offset((*c).buffer.as_ptr() as *u8, 1),
+				(messageSize - 1) as uint);
+
+			if message.starts_with("UPDATE") {
+				let parts: ~[&str] = message.words().collect();
+
+				let id_str = parts[2].trim_chars(&',');
+				let x_str  = parts[4].trim_chars(&',').trim_chars(&'(');
+				let y_str  = parts[5].trim_chars(&')');
+
+				let id: int = from_str::from_str(id_str).unwrap_or_else(|| { fail!() });
+
+				let x: f32 = from_str::from_str(x_str).unwrap_or_else(|| { fail!() });
+				let y: f32 = from_str::from_str(y_str).unwrap_or_else(|| { fail!() });
+
+
+				(*ptr::mut_offset(positions.elems, id)).isOccupied = 1;
+				(*ptr::mut_offset(positions.elems, id)).value = display::Pos { x: x, y: y };
+			}
+			else if message.starts_with("REMOVE") {
+				let parts: ~[&str] = message.words().collect();
+
+				let id_str = parts[2];
+
+				let id: int = from_str::from_str(id_str).unwrap_or_else(|| { fail!() });
+
+				(*ptr::mut_offset(positions.elems, id)).isOccupied = 0;
+			}
+			else {
+				print!("Unknown message type in message: {:s}\n", message);
+				fail!();
+			}
+
+			ptr::copy_memory(
+				(*c).buffer.as_mut_ptr(),
+				ptr::offset((*c).buffer.as_ptr(), messageSize as int),
+				(BUFFER_SIZE - messageSize as i32) as uint);
+			(*c).bufferPos -= messageSize as i32;
+		}
+	}
+}
