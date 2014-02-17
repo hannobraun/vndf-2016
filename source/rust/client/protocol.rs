0929533cc2b0d283e7d3a3320ef04ef546a8a158
Hanno Braun <mail@hannobraun.de>
2014-02-17 19:28:56 +0100

    Refactoring.

--- source/rust/client/protocol.rs
+++ source/rust/client/protocol.rs
@@ -18,49 +18,51 @@ pub struct Connection {
 
 
 pub fn receive_positions(c: &mut Connection, positions: &mut display::PosMap) {
-	unsafe {
-		let bytesReceived = net::receive(
-			c.socket_fd,
-			c.buffer.slice_from(c.bufferPos));
+	let bytesReceived = net::receive(
+		c.socket_fd,
+		c.buffer.slice_from(c.bufferPos));
 
-		c.bufferPos += bytesReceived as uint;
+	c.bufferPos += bytesReceived as uint;
 
-		while c.bufferPos > 0 && c.buffer[0] as uint <= c.bufferPos {
-			let messageSize = c.buffer[0];
-			assert!(messageSize >= 0);
+	while c.bufferPos > 0 && c.buffer[0] as uint <= c.bufferPos {
+		let messageSize = c.buffer[0];
+		assert!(messageSize >= 0);
 
-			let message = str::raw::from_buf_len(
+		let message = unsafe {
+			str::raw::from_buf_len(
 				(c.buffer.as_ptr() as *u8).offset(1),
-				(messageSize - 1) as uint);
+				(messageSize - 1) as uint)
+		};
 
-			if message.starts_with("UPDATE") {
-				let parts: ~[&str] = message.words().collect();
+		if message.starts_with("UPDATE") {
+			let parts: ~[&str] = message.words().collect();
 
-				let id_str = parts[2].trim_chars(&',');
-				let x_str  = parts[4].trim_chars(&',').trim_chars(&'(');
-				let y_str  = parts[5].trim_chars(&')');
+			let id_str = parts[2].trim_chars(&',');
+			let x_str  = parts[4].trim_chars(&',').trim_chars(&'(');
+			let y_str  = parts[5].trim_chars(&')');
 
-				let id: int = from_str::from_str(id_str).unwrap_or_else(|| { fail!() });
+			let id: int = from_str::from_str(id_str).unwrap_or_else(|| { fail!() });
 
-				let x: f32 = from_str::from_str(x_str).unwrap_or_else(|| { fail!() });
-				let y: f32 = from_str::from_str(y_str).unwrap_or_else(|| { fail!() });
+			let x: f32 = from_str::from_str(x_str).unwrap_or_else(|| { fail!() });
+			let y: f32 = from_str::from_str(y_str).unwrap_or_else(|| { fail!() });
 
-				positions.insert(id, display::Position { x: x, y: y });
-			}
-			else if message.starts_with("REMOVE") {
-				let parts: ~[&str] = message.words().collect();
+			positions.insert(id, display::Position { x: x, y: y });
+		}
+		else if message.starts_with("REMOVE") {
+			let parts: ~[&str] = message.words().collect();
 
-				let id_str = parts[2];
+			let id_str = parts[2];
 
-				let id: int = from_str::from_str(id_str).unwrap_or_else(|| { fail!() });
+			let id: int = from_str::from_str(id_str).unwrap_or_else(|| { fail!() });
 
-				positions.remove(&id);
-			}
-			else {
-				print!("Unknown message type in message: {:s}\n", message);
-				fail!();
-			}
+			positions.remove(&id);
+		}
+		else {
+			print!("Unknown message type in message: {:s}\n", message);
+			fail!();
+		}
 
+		unsafe {
 			ptr::copy_memory(
 				c.buffer.as_mut_ptr(),
 				c.buffer.as_ptr().offset(messageSize as int),
