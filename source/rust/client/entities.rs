7a94cbf11e99894286609de0d528733f64b2ef7f
Hanno Braun <mail@hannobraun.de>
2014-03-01 20:50:21 +0100

    Refactoring.

--- source/rust/client/entities.rs
+++ source/rust/client/entities.rs
@@ -1,6 +1,5 @@
 use collections::HashMap;
 
-use ccore::protocol::Handler;
 use common::vec::Vec2;
 
 use visual::Visual;
@@ -22,20 +21,18 @@ impl Entities {
 			visuals  : HashMap::new() }
 	}
 
-	pub fn update_asteroid(&mut self, id: Id, x: f64, y: f64) {
-		self.positions.insert(id, Vec2 { x: x, y: y });
-		self.visuals.insert(id, Visual { texture: ~"A" });
-	}
-}
-
-impl Handler for Entities {
-	fn update_ship(&mut self, id: Id, x: f64, y: f64) {
+	pub fn update_ship(&mut self, id: Id, x: f64, y: f64) {
 		self.positions.insert(id, Vec2 { x: x, y: y });
 		self.visuals.insert(id, Visual { texture: ~"images/spaceship.png" });
 	}
 
-	fn remove_ship(&mut self, id: Id) {
+	pub fn remove_ship(&mut self, id: Id) {
 		self.positions.remove(&id);
 		self.visuals.remove(&id);
+	}
+
+	pub fn update_asteroid(&mut self, id: Id, x: f64, y: f64) {
+		self.positions.insert(id, Vec2 { x: x, y: y });
+		self.visuals.insert(id, Visual { texture: ~"A" });
 	}
 }
