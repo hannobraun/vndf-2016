745629f1abc0ae87802137abee0a75a0856a2f67
Hanno Braun <mail@hannobraun.de>
2014-03-07 21:38:48 +0100

    Refactoring.

--- source/rust/client-ui/ui/renderer.rs
+++ source/rust/client-ui/ui/renderer.rs
@@ -1,9 +1,16 @@
+use collections::HashMap;
 use std::f64;
 
 use gl;
 use glfw;
 
+use common::vec::Vec2;
+
+use camera::Camera;
+use entities::Components;
+use texture::Texture;
 use ui::Window;
+use visual::Visual;
 
 
 pub struct Renderer;
@@ -38,4 +45,81 @@ impl Renderer {
 
 		~Renderer
 	}
+
+	pub fn render(&self,
+		window   : &Window,
+		camera   : &Camera,
+		positions: &Components<Vec2>,
+		visuals  : &Components<Visual>,
+		textures : &HashMap<~str, Texture>) {
+
+		gl::Clear(gl::COLOR_BUFFER_BIT);
+
+		gl::PushMatrix();
+
+		gl::Translated(0.0, 0.0, -500.0);
+		gl::Rotated(camera.v, 1.0, 0.0, 0.0);
+		gl::Rotated(camera.h, 0.0, 1.0, 0.0);
+
+		gl::Color4d(1.0, 1.0, 1.0, 1.0);
+
+		for (id, position) in positions.iter() {
+			let texture = textures.get(&visuals.get(id).texture);
+			draw_texture(position.x, position.y, texture);
+		}
+
+		gl::PopMatrix();
+		window.swap_buffers();
+	}
+}
+
+fn draw_texture(x: f64, y: f64, texture: &Texture) {
+	gl::BindTexture(
+		gl::TEXTURE_2D,
+		texture.name);
+
+	gl::PushMatrix();
+
+	gl::Translated(
+		x - texture.width as f64 / 2.0,
+		y - texture.height as f64 / 2.0,
+		0.0);
+
+	gl::Begin(gl::TRIANGLE_STRIP);
+	{
+		gl::TexCoord2d(
+			1.0,
+			0.0);
+		gl::Vertex3d(
+			texture.width as f64,
+			texture.height as f64,
+			0.0);
+
+		gl::TexCoord2d(
+			1.0,
+			1.0);
+		gl::Vertex3d(
+			texture.width as f64,
+			0.0,
+			0.0);
+
+		gl::TexCoord2d(
+			0.0,
+			0.0);
+		gl::Vertex3d(
+			0.0,
+			texture.height as f64,
+			0.0);
+
+		gl::TexCoord2d(
+			0.0,
+			1.0);
+		gl::Vertex3d(
+			0.0,
+			0.0,
+			0.0);
+	}
+	gl::End();
+
+	gl::PopMatrix();
 }
