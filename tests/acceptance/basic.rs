f98e2ee82cdcc50607dde4bc90af1b47027875c8
Hanno Braun <mail@hannobraun.de>
2014-03-08 22:30:08 +0100

    Refactoring.

--- tests/acceptance/basic.rs
+++ tests/acceptance/basic.rs
@@ -13,8 +13,8 @@ fn it_should_connect_and_receive_updates() {
 #[test]
 fn the_ship_should_move_on_a_straight_line() {
 	#[allow(unused_variable)];
-	let     core_service = GameService::start();
-	let mut client_core  = ClientCore::start(core_service.port);
+	let     game_service = GameService::start();
+	let mut client_core  = ClientCore::start(game_service.port);
 
 	let update1 = client_core.expect_update();
 	let update2 = client_core.expect_update();
