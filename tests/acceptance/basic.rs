e928169c274e8df7fb289ad963b2b4e0b3770f50
Hanno Braun <mail@hannobraun.de>
2014-03-08 17:16:03 +0100

    Refactoring.

--- tests/acceptance/basic.rs
+++ tests/acceptance/basic.rs
@@ -9,13 +9,9 @@ fn it_should_connect_and_receive_updates() {
 		"output/bin/vndf-client-core", [~"localhost"]);
 
 	let message = client_core.read_stdout_line();
-	assert_is_update(message);
+	let update = Update::from_str(message);
+	assert!(update != None);
 
 	core_service.kill();
 	client_core.kill();
-}
-
-fn assert_is_update(message: ~str) {
-	let update = Update::from_str(message);
-	assert!(update != None);
 }
