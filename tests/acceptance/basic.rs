9c85ad15ff7919e660685b7e905cc107eab30fb1
Hanno Braun <mail@hannobraun.de>
2014-03-08 17:15:14 +0100

    Refactoring.

---
+++ tests/acceptance/basic.rs
@@ -0,0 +1,21 @@
+use util::{Process, Update};
+
+
+#[test]
+fn it_should_connect_and_receive_updates() {
+	let mut core_service = Process::start(
+		"output/bin/vndf-core-service", []);
+	let mut client_core  = Process::start(
+		"output/bin/vndf-client-core", [~"localhost"]);
+
+	let message = client_core.read_stdout_line();
+	assert_is_update(message);
+
+	core_service.kill();
+	client_core.kill();
+}
+
+fn assert_is_update(message: ~str) {
+	let update = Update::from_str(message);
+	assert!(update != None);
+}
--- tests/acceptance/main.rs
+++ tests/acceptance/main.rs
@@ -1,26 +1,4 @@
 extern crate common;
 
-
-use util::{Process, Update};
-
-
+mod basic;
 mod util;
-
-#[test]
-fn it_should_connect_and_receive_updates() {
-	let mut core_service = Process::start(
-		"output/bin/vndf-core-service", []);
-	let mut client_core  = Process::start(
-		"output/bin/vndf-client-core", [~"localhost"]);
-
-	let message = client_core.read_stdout_line();
-	assert_is_update(message);
-
-	core_service.kill();
-	client_core.kill();
-}
-
-fn assert_is_update(message: ~str) {
-	let update = Update::from_str(message);
-	assert!(update != None);
-}
