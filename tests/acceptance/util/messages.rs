ef651aa157e862382cc4f959f51f89917a522f53
Hanno Braun <mail@hannobraun.de>
2014-03-08 19:15:08 +0100

    Improved error reporting in test framework.

--- tests/acceptance/util/messages.rs
+++ tests/acceptance/util/messages.rs
@@ -13,20 +13,23 @@ impl Update {
 	pub fn from_str(s: &str) -> Option<Update> {
 		let words: ~[&str] = s.words().collect();
 
-		assert!(words[0] == "UPDATE");
+		if words[0] == "UPDATE" {
+			let id: Option<uint> = from_str::from_str(words[1]);
+			let x : Option<f64>  = from_str::from_str(words[2]);
+			let y : Option<f64>  = from_str::from_str(words[3]);
+			let z : Option<f64>  = from_str::from_str(words[4]);
 
-		let id: Option<uint> = from_str::from_str(words[1]);
-		let x : Option<f64>  = from_str::from_str(words[2]);
-		let y : Option<f64>  = from_str::from_str(words[3]);
-		let z : Option<f64>  = from_str::from_str(words[4]);
-
-		Some(Update {
-			id : id.unwrap(),
-			pos: Vec3 {
-				x : x.unwrap(),
-				y : y.unwrap(),
-				z : z.unwrap()
-			}
-		})
+			Some(Update {
+				id : id.unwrap(),
+				pos: Vec3 {
+					x : x.unwrap(),
+					y : y.unwrap(),
+					z : z.unwrap()
+				}
+			})
+		}
+		else {
+			None
+		}
 	}
 }
