c53626e8a9b9be4b6b1b1b1908b914020a04dd36
Hanno Braun <mail@hannobraun.de>
2014-03-24 12:53:29 +0100

    Refactoring.

--- source/rust/game-service/protocol.rs
+++ source/rust/game-service/protocol.rs
@@ -1,5 +1,7 @@
 use std::libc;
 
+use net;
+
 
 pub fn send_update(
 	clientFD: libc::c_int,
@@ -8,11 +10,11 @@ pub fn send_update(
 	yPos    : f64,
 	zPos    : f64) -> libc::c_int {
 
-	::net::send_message(clientFD,
+	net::send_message(clientFD,
 		format!("UPDATE id: {:u}, pos: ({:f}, {:f} {:f})",
 			id, xPos, yPos, zPos))
 }
 
 pub fn send_remove(clientFD: libc::c_int, id: uint) -> libc::c_int {
-	::net::send_message(clientFD, format!("REMOVE id: {:u}", id))
+	net::send_message(clientFD, format!("REMOVE id: {:u}", id))
 }
