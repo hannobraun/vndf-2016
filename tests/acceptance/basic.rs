b99154d253f75d123c9ca4b279f260d5f7441de2
Hanno Braun <mail@hannobraun.de>
2014-03-24 16:17:05 +0100

    Refactoring.

--- tests/acceptance/basic.rs
+++ tests/acceptance/basic.rs
@@ -3,13 +3,13 @@ use control::{ClientCore, GameService};
 #[test]
 fn the_ship_should_move_on_a_straight_line() {
 	let     game_service = GameService::start();
-	let mut client_core  = ClientCore::start(game_service.port);
+	let mut client       = ClientCore::start(game_service.port);
 
-	client_core.ignore_message(); // self id
+	client.ignore_message(); // self id
 
-	let update1 = client_core.expect_update();
-	let update2 = client_core.expect_update();
-	let update3 = client_core.expect_update();
+	let update1 = client.expect_update();
+	let update2 = client.expect_update();
+	let update3 = client.expect_update();
 
 	let movement1 = update2.pos - update1.pos;
 	let movement2 = update3.pos - update2.pos;
