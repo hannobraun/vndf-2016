764567d66a74b088315c6674f8fbe4a61c994ad3
Hanno Braun <mail@hannobraun.de>
2014-04-04 17:36:20 +0000

    Draw glyphs with offset
    Taking the offset into account, text rendering looks great now!

--- source/rust/client-ui/ui/renderer.rs
+++ source/rust/client-ui/ui/renderer.rs
@@ -84,7 +84,7 @@ impl Renderer {
 			let glyph   = self.font.get(c);
 			let texture = self.textures.get(&glyph.texture_id);
 
-			draw_texture(position, texture);
+			draw_texture(position + glyph.offset, texture);
 
 			position = position + glyph.advance;
 		}
