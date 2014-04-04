20891aab9a973db982e35a7d7ec3ffb1bd830e81
Hanno Braun <mail@hannobraun.de>
2014-04-04 13:28:07 +0000

    Refactor

--- source/rust/client-ui/camera.rs
+++ source/rust/client-ui/camera.rs
@@ -1,17 +1,13 @@
 #[deriving(Show)]
 pub struct Camera {
 	x: f64,
-	y: f64,
-	h: f64,
-	v: f64
+	y: f64
 }
 
 impl Camera {
 	pub fn new() -> ~Camera {
 		~Camera {
 			x: 0.0,
-			y: 0.0,
-			v: 0.0,
-			h: 0.0 }
+			y: 0.0 }
 	}
 }
