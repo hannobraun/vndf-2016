0684a68b6b5efd0ae940c1b6c521ca5341d2ea46
Hanno Braun <mail@hannobraun.de>
2014-04-08 13:09:00 +0000

    Refactor
    Implemented input modul.

---
+++ source/rust/client-ui/ui/input.rs
@@ -0,0 +1,23 @@
+use glfw;
+
+use common::physics::Body;
+
+use ui::Window;
+
+
+pub fn apply_input(window: &Window, player: Option<&mut Body>) {
+	let angular_velocity = 0.1;
+	let mut attitude_change = 0.0;
+
+	if window.key_pressed(glfw::KeyLeft) {
+		attitude_change += angular_velocity;
+	}
+	if window.key_pressed(glfw::KeyRight) {
+		attitude_change -= angular_velocity;
+	}
+
+	match player {
+		Some(ship) => ship.attitude += attitude_change,
+		None       => ()
+	}
+}
--- source/rust/client-ui/ui/mod.rs
+++ source/rust/client-ui/ui/mod.rs
@@ -1,9 +1,11 @@
 pub use ui::font::Font;
+pub use ui::input::apply_input;
 pub use ui::renderer::Renderer;
 pub use ui::textures::{Texture, Textures};
 pub use ui::window::Window;
 
 mod font;
+mod input;
 mod renderer;
 mod textures;
 mod window;
