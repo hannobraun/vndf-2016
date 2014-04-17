fd02a8f6a3f3e3fe430574d89b8c1749ca89319b
Hanno Braun <mail@hannobraun.de>
2014-04-17 15:25:10 +0000

    Implement addition for radians

--- source/rust/common/physics/angle.rs
+++ source/rust/common/physics/angle.rs
@@ -1,1 +1,8 @@
 pub struct Radians(pub f64);
+
+impl Add<Radians, Radians> for Radians {
+	fn add(&self, &Radians(other): &Radians) -> Radians {
+		let &Radians(this) = self;
+		Radians(this + other)
+	}
+}
