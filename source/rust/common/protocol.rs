6b66114eb5a54bf13290c83bfc2ce3913415f392
Hanno Braun <mail@hannobraun.de>
2014-04-17 19:50:51 +0000

    Use JSON for network protocol
    This makes the protocol more inefficient, but makes the code MUCH less
    redundant. If more efficiency is needed in the future, we should use a
    solution based on libserialize.

--- source/rust/common/protocol.rs
+++ source/rust/common/protocol.rs
@@ -1,7 +1,12 @@
-use std::from_str;
+use serialize::{Decodable, Encodable};
+use serialize::json;
+use serialize::json::Encoder;
 use std::intrinsics::TypeId;
+use std::io::MemWriter;
+use std::io::Writer;
+use std::str;
 
-use physics::{Body, Radians, Vec2};
+use physics::Body;
 
 
 #[deriving(Decodable, Encodable, Show)]
@@ -15,26 +20,33 @@ pub enum Message {
 
 impl Message {
 	pub fn from_str(s: &str) -> Message {
-		let words: ~[&str] = s.words().collect();
+		let json_object = match json::from_str(s) {
+			Ok(object) => object,
+			Err(error) =>
+				fail!("Error decoding JSON object from \"{}\": {}", s, error)
+		};
 
-		match words[0] {
-			"SELF_ID" => SelfInfo(SelfInfo::from_words(words)),
-			"CREATE"  => Create(Create::from_words(words)),
-			"UPDATE"  => Update(Update::from_words(words)),
-			"REMOVE"  => Remove(Remove::from_words(words)),
-			_         => Invalid(s.to_owned())
+		let mut decoder = json::Decoder::new(json_object);
+
+		match Decodable::decode(&mut decoder) {
+			Ok(message) => message,
+			Err(error)  => fail!("error decoding JSON object ({})", error)
 		}
 	}
 
-	pub fn to_str(self) -> ~str {
-		match self {
-			SelfInfo(self_info) => self_info.to_str(),
-			Create(create)      => create.to_str(),
-			Update(update)      => update.to_str(),
-			Remove(remove)      => remove.to_str(),
-
-			_ => fail!("unexpected message ({})", self)
+	pub fn to_str(&self) -> ~str {
+		let mut m = MemWriter::new();
+		{
+			let mut encoder = Encoder::new(&mut m as &mut Writer);
+			match self.encode(&mut encoder) {
+				Ok(()) => (),
+				Err(e) => fail!("json encoding error: {}", e)
+			};
 		}
+
+		str::from_utf8(m.get_ref())
+			.expect("expected UTF-8 string")
+			.to_owned()
 	}
 
 	pub fn type_id(&self) -> TypeId {
@@ -54,48 +66,11 @@ pub struct SelfInfo {
 	pub id: uint
 }
 
-impl SelfInfo {
-	pub fn from_words(words: ~[&str]) -> SelfInfo {
-		let id: Option<uint> = from_str::from_str(words[1]);
-
-		SelfInfo {
-			id: id.unwrap()
-		}
-	}
-
-	fn to_str(&self) -> ~str {
-		format!(
-			"SELF_ID {}",
-			self.id)
-	}
-}
-
-
 #[deriving(Decodable, Encodable, Show)]
 pub struct Create {
 	pub id  : uint,
 	pub kind: ~str
 }
-
-impl Create {
-	pub fn from_words(words: ~[&str]) -> Create {
-		let id  : Option<uint> = from_str::from_str(words[1]);
-		let kind               = words[2].to_owned();
-
-		Create {
-			id  : id.unwrap(),
-			kind: kind
-		}
-	}
-
-	fn to_str(&self) -> ~str {
-		format!(
-			"CREATE {} {}",
-			self.id,
-			self.kind)
-	}
-}
-
 
 #[deriving(Decodable, Encodable, Eq, Show)]
 pub struct Update {
@@ -103,58 +78,7 @@ pub struct Update {
 	pub body: Body
 }
 
-impl Update {
-	pub fn from_words(words: ~[&str]) -> Update {
-		let id: Option<uint> = from_str::from_str(words[1]);
-		let x : Option<f64>  = from_str::from_str(words[2]);
-		let y : Option<f64>  = from_str::from_str(words[3]);
-
-		Update {
-			id  : id.unwrap(),
-			body: Body {
-				position: Vec2 {
-					x: x.unwrap(),
-					y: y.unwrap()
-				},
-				velocity: Vec2 {
-					x: 0.0,
-					y: 0.0
-				},
-				attitude: Radians(0.0)
-			}
-		}
-	}
-
-	fn to_str(&self) -> ~str {
-		format!(
-			"UPDATE {} {} {} {} {} {}",
-			self.id,
-			self.body.position.x,
-			self.body.position.y,
-			self.body.velocity.x,
-			self.body.velocity.y,
-			self.body.attitude)
-	}
-}
-
-
 #[deriving(Decodable, Encodable, Show)]
 pub struct Remove {
 	pub id: uint
-}
-
-impl Remove {
-	pub fn from_words(words: ~[&str]) -> Remove {
-		let id: Option<uint> = from_str::from_str(words[1]);
-
-		Remove {
-			id: id.unwrap()
-		}
-	}
-
-	fn to_str(&self) -> ~str {
-		format!(
-			"REMOVE {}",
-			self.id)
-	}
 }
