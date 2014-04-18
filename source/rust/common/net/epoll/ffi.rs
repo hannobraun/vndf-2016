dc903434fdc1f75ab8f68fa4594e05a542c036c5
Hanno Braun <mail@hannobraun.de>
2014-04-18 12:14:34 +0000

    Add epoll event flags

--- source/rust/common/net/epoll/ffi.rs
+++ source/rust/common/net/epoll/ffi.rs
@@ -12,7 +12,15 @@ pub static EPOLL_CTL_ADD: c_int = 1;
 pub static EPOLL_CTL_DEL: c_int = 2;
 pub static EPOLL_CTL_MOD: c_int = 3;
 
-pub static EPOLLIN: u32 = 1;
+// event flags
+pub static EPOLLIN     : u32 = 0x001;
+pub static EPOLLOUT    : u32 = 0x004;
+pub static EPOLLRDHUP  : u32 = 0x2000;
+pub static EPOLLPRI    : u32 = 0x002;
+pub static EPOLLERR    : u32 = 0x008;
+pub static EPOLLHUP    : u32 = 0x010;
+pub static EPOLLET     : u32 = 1 << 31;
+pub static EPOLLONESHOT: u32 = 1 << 30;
 
 
 
