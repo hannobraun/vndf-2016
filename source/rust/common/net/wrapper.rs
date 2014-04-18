86a546042536a8fca5e5afa82f063e85815e156f
Hanno Braun <mail@hannobraun.de>
2014-04-18 10:50:33 +0000

    Refactor

--- source/rust/common/net/wrapper.rs
+++ source/rust/common/net/wrapper.rs
@@ -1,5 +1,7 @@
 use libc;
 use libc::c_int;
+use std::c_str::CString;
+use std::os;
 
 use net::ffi;
 
@@ -17,10 +19,19 @@ pub fn register_accept(pollerFD: c_int, serverFD: c_int) {
 			&event);
 
 		if status != 0 {
-			"Error registering server socket with epoll".to_c_str().with_ref(|c_message| {
-				libc::perror(c_message);
-			});
-			fail!();
+			fail!(
+				"Error registering server socket with epoll: {}",
+				last_error());
 		}
+	}
+}
+
+fn last_error() -> ~str {
+	unsafe {
+		let c_error = libc::strerror(os::errno() as i32);
+		CString::new(c_error, false)
+			.as_str()
+			.expect("failed to convert C error message into Rust string")
+			.to_owned()
 	}
 }
