f0b595a55bc9070a138d1fd6f57ded405c757439
Hanno Braun <mail@hannobraun.de>
2014-04-18 14:57:26 +0000

    Refactor

--- source/rust/common/net/connection.rs
+++ source/rust/common/net/connection.rs
@@ -44,10 +44,7 @@ impl Connection {
 			});
 
 			if status != 0 {
-				"Error getting address info".to_c_str().with_ref(|c_message| {
-					libc::perror(c_message);
-				});
-				libc::exit(1);
+				fail!("Error getting address info: {}", last_error());
 			}
 
 			let socketFD = ffi::socket(
