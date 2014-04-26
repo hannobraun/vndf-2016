2f01349382f4f2a287c921fe74381575da0190e2
Hanno Braun <mail@hannobraun.de>
2014-04-26 06:03:36 +0000

    Add frame time command-line argument
    It isn't used yet.

--- source/rust/game-service/args.rs
+++ source/rust/game-service/args.rs
@@ -7,19 +7,22 @@ use std::os;
 
 
 pub struct Args {
-	pub port: ~str
+	pub port      : ~str,
+	pub frame_time: u32
 }
 
 
 pub fn parse() -> Option<Args> {
 	let mut args = Args {
-		port: ~"34481"
+		port      : ~"34481",
+		frame_time: 1000
 	};
 
 	let args_as_strs = os::args();
 
 	let options = [
-		optopt("p", "port", "port to listen on", args.port)
+		optopt("p", "port", "port to listen on", args.port),
+		optopt("f", "frame-time", "frame time in ms", args.frame_time.to_str())
 	];
 
 	let usage = usage(format!("{} [OPTIONS]", args_as_strs[0]), options);
@@ -37,6 +40,18 @@ pub fn parse() -> Option<Args> {
 	match matches.opt_str("p") {
 		Some(port) => args.port = port,
 		None       => ()
+	}
+
+	match matches.opt_str("f") {
+		Some(frame_time_as_str) => match from_str(frame_time_as_str) {
+			Some(frame_time) => args.frame_time = frame_time,
+			None             => {
+				print!("{}\n", "Frame time must be a number");
+				print!("{}", usage);
+			}
+		},
+
+		None => ()
 	}
 
 	Some(args)
