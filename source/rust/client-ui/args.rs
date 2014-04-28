de5afe06c64fd2a40c51849f2ccfc5863b272255
Hanno Braun <mail@hannobraun.de>
2014-04-28 15:02:46 +0000

    Add command-line flag for headless mode

--- source/rust/client-ui/args.rs
+++ source/rust/client-ui/args.rs
@@ -1,5 +1,6 @@
 use getopts::{
 	getopts,
+	optflag,
 	optopt,
 	usage
 };
@@ -7,22 +8,26 @@ use std::os;
 
 
 pub struct Args {
-	pub address: ~str,
-	pub port   : ~str
+	pub address : ~str,
+	pub port    : ~str,
+	pub headless: bool
 }
 
 
 pub fn parse() -> Option<Args> {
 	let mut args = Args {
-		address: ~"localhost",
-		port   : ~"34481"
+		address : ~"localhost",
+		port    : ~"34481",
+		headless: false
 	};
 
 	let args_as_strs = os::args();
 
 	let options = [
 		optopt("a", "address", "address of the server", args.address),
-		optopt("p", "port"   , "port to connect to"   , args.port)
+		optopt("p", "port"   , "port to connect to"   , args.port),
+
+		optflag("h", "headless", "start in headless mode")
 	];
 
 	let usage = usage(format!("{} [OPTIONS]", args_as_strs[0]), options);
@@ -46,6 +51,8 @@ pub fn parse() -> Option<Args> {
 		Some(port) => args.port = port,
 		None       => ()
 	}
+
+	args.headless = matches.opt_present("h");
 
 	Some(args)
 }
