79ac2f3b83b967d982539ec5e43f602afb974fe8
Hanno Braun <mail@hannobraun.de>
2014-04-29 20:44:19 +0000

    Make Frame JSON-encodable

--- source/rust/libcommon/headless/frame.rs
+++ source/rust/libcommon/headless/frame.rs
@@ -1,6 +1,29 @@
+use serialize::Encodable;
+use serialize::json::Encoder;
+use std::io::MemWriter;
+use std::str;
+
 use physics::Body;
 
 
+#[deriving(Encodable)]
 pub struct Frame {
 	pub ships: ~[Body]
+}
+
+impl Frame {
+	pub fn to_json(&self) -> ~str {
+		let mut m = MemWriter::new();
+		{
+			let mut encoder = Encoder::new(&mut m as &mut Writer);
+			match self.encode(&mut encoder) {
+				Ok(()) => (),
+				Err(e) => fail!("JSON encoding error: {}", e)
+			};
+		}
+
+		str::from_utf8(m.get_ref())
+			.expect("expected UTF-8 string")
+			.to_owned()
+	}
 }
