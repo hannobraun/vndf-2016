8e911707c4cd3b38a2b4fb0fa40dbbd7c0c4df4d
Hanno Braun <mail@hannobraun.de>
2014-04-29 20:50:21 +0000

    Make Frame decodable from JSON

--- source/rust/libcommon/headless/frame.rs
+++ source/rust/libcommon/headless/frame.rs
@@ -1,17 +1,39 @@
-use serialize::Encodable;
-use serialize::json::Encoder;
+use serialize::{
+	Decodable,
+	Encodable
+};
+use serialize::json;
+use serialize::json::{
+	Decoder,
+	Encoder
+};
 use std::io::MemWriter;
 use std::str;
 
 use physics::Body;
 
 
-#[deriving(Encodable)]
+#[deriving(Decodable, Encodable)]
 pub struct Frame {
 	pub ships: ~[Body]
 }
 
 impl Frame {
+	pub fn from_json(s: &str) -> Frame {
+		let json_object = match json::from_str(s) {
+			Ok(object) => object,
+			Err(error) =>
+				fail!("Error decoding JSON object from \"{}\": {}", s, error)
+		};
+
+		let mut decoder = Decoder::new(json_object);
+
+		match Decodable::decode(&mut decoder) {
+			Ok(frame)  => frame,
+			Err(error) => fail!("error decoding JSON object ({})", error)
+		}
+	}
+
 	pub fn to_json(&self) -> ~str {
 		let mut m = MemWriter::new();
 		{
