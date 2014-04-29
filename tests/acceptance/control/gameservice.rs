8ef8efaa074308f9ee7e5f6b44656e808f8d6ed3
Hanno Braun <mail@hannobraun.de>
2014-04-29 19:21:14 +0000

    Refactor

---
+++ tests/acceptance/control/gameservice.rs
@@ -0,0 +1,27 @@
+use rand;
+
+use util::Process;
+
+
+pub struct GameService {
+	pub port   : uint,
+	pub process: Process
+}
+
+impl GameService {
+	pub fn start() -> GameService {
+		let port = rand::random::<uint>() % 10000 + 40000;
+
+		let mut process = Process::start(
+			"output/bin/vndf-game-service",
+			[
+				~"--port", port.to_str(),
+				~"--frame-time", ~"10"]);
+		process.read_stdout_line(); // Make sure it's ready
+
+		GameService {
+			port   : port,
+			process: process
+		}
+	}
+}
--- tests/acceptance/control/mod.rs
+++ tests/acceptance/control/mod.rs
@@ -1,5 +1,4 @@
 use collections::HashSet;
-use rand;
 use std::intrinsics::TypeId;
 
 use common::physics::Radians;
@@ -13,28 +12,10 @@ use common::protocol::{
 use util::Process;
 
 
-pub struct GameService {
-	pub port   : uint,
-	pub process: Process
-}
+pub use self::gameservice::GameService;
 
-impl GameService {
-	pub fn start() -> GameService {
-		let port = rand::random::<uint>() % 10000 + 40000;
 
-		let mut process = Process::start(
-			"output/bin/vndf-game-service",
-			[
-				~"--port", port.to_str(),
-				~"--frame-time", ~"10"]);
-		process.read_stdout_line(); // Make sure it's ready
-
-		GameService {
-			port   : port,
-			process: process
-		}
-	}
-}
+mod gameservice;
 
 
 pub struct ClientCore {
