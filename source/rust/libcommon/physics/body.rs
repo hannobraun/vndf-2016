4b2931bc1b3ada2d5af198ba0c2533c059623262
Hanno Braun <mail@hannobraun.de>
2014-04-30 14:59:38 +0000

    Derive Clone for Body

--- source/rust/libcommon/physics/body.rs
+++ source/rust/libcommon/physics/body.rs
@@ -1,7 +1,7 @@
 use physics::Radians;
 use physics::Vec2;
 
-#[deriving(Decodable, Encodable, Eq, Show)]
+#[deriving(Clone, Decodable, Encodable, Eq, Show)]
 pub struct Body {
 	pub position: Vec2,
 	pub velocity: Vec2,
