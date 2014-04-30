210bbb9ef741bc77ff513d7fdafbe1d339025b39
Hanno Braun <mail@hannobraun.de>
2014-04-30 12:33:37 +0000

    Make Input JSON-encodable

--- source/rust/libcommon/headless/input.rs
+++ source/rust/libcommon/headless/input.rs
@@ -1,7 +1,15 @@
+use json::to_json;
 use physics::Radians;
 
 
+#[deriving(Encodable)]
 pub struct Input {
 	pub attitude: Radians,
 	pub send    : bool
+}
+
+impl Input {
+	pub fn to_json(&self) -> ~str {
+		to_json(self)
+	}
 }
