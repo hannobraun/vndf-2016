907e7bd6e3b1070609be1fe62e87e636ce807179
Hanno Braun <mail@hannobraun.de>
2014-05-03 17:37:47 +0000

    Remove unused code

--- source/rust/libcommon/protocol.rs
+++ source/rust/libcommon/protocol.rs
@@ -8,23 +8,6 @@ use physics::{
 };
 
 
-#[deriving(Decodable, Encodable, Eq, Show)]
-pub enum Message {
-	Perception(Perception),
-	Command(Command),
-	Invalid(~str)
-}
-
-impl Message {
-	pub fn from_str(s: &str) -> Result<Message, ~str> {
-		from_json(s)
-	}
-
-	pub fn to_str(&self) -> ~str {
-		to_json(self)
-	}
-}
-
 #[deriving(Clone, Decodable, Encodable, Eq, Show)]
 pub struct Perception {
 	pub self_id: uint,
