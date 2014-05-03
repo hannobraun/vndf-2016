c0c03df5a4eee001df1f218fdd106fc04ef6b400
Hanno Braun <mail@hannobraun.de>
2014-05-03 14:58:40 +0000

    Rename module ui::input to inputhandler

--- source/rust/client/ui/mod.rs
+++ source/rust/client/ui/mod.rs
@@ -1,11 +1,11 @@
 pub use ui::font::Font;
-pub use ui::input::InputHandler;
+pub use ui::inputhandler::InputHandler;
 pub use ui::renderer::Renderer;
 pub use ui::textures::{Texture, Textures};
 pub use ui::window::Window;
 
 mod font;
-mod input;
+mod inputhandler;
 mod renderer;
 mod textures;
 mod window;
