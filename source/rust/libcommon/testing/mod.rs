9cec730c37ab60e0665ae37eda8b3d50e88fd442
Hanno Braun <mail@hannobraun.de>
2014-05-17 11:05:46 +0000

    Create sub-mod for remote-control functionality

--- source/rust/libcommon/testing/mod.rs
+++ source/rust/libcommon/testing/mod.rs
@@ -1,11 +1,13 @@
 pub use self::generic::process::Process;
-pub use self::specific::client::Client;
-pub use self::specific::gameservice::GameService;
+pub use self::specific::rc::client::Client;
+pub use self::specific::rc::gameservice::GameService;
 
 mod generic {
 	pub mod process;
 }
 mod specific {
-	pub mod client;
-	pub mod gameservice;
+	pub mod rc {
+		pub mod client;
+		pub mod gameservice;
+	}
 }
