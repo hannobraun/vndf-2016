23d784f7850b2cab2aa1df459a1df2b08fbaa52d
Hanno Braun <mail@hannobraun.de>
2014-05-22 11:22:32 +0000

    Add argument for actions sending period
    The argument isn't used yet.
    The use case for such an argument are the tests: A one second period
    needlessly slows down the testing, so a shorter amount is good for
    performance.

--- source/rust/client/args.rs
+++ source/rust/client/args.rs
@@ -10,6 +10,7 @@ use std::os;
 pub struct Args {
 	pub address : StrBuf,
 	pub port    : StrBuf,
+	pub period  : u16,
 	pub headless: bool
 }
 
@@ -18,6 +19,7 @@ pub fn parse() -> Option<Args> {
 	let mut args = Args {
 		address : "localhost".to_strbuf(),
 		port    : "34481".to_strbuf(),
+		period  : 1000,
 		headless: false
 	};
 
@@ -28,8 +30,9 @@ pub fn parse() -> Option<Args> {
 		.collect();
 
 	let options = [
-		optopt("a", "address", "server address", args.address.as_slice()),
-		optopt("p", "port"   , "server port"   , args.port.as_slice()),
+		optopt("a", "address", "server address"     , args.address.as_slice()),
+		optopt("p", "port"   , "server port"        , args.port.as_slice()),
+		optopt("f", "period" , "action period in ms", args.period.to_str()),
 
 		optflag("h", "headless", "start in headless mode")
 	];
@@ -54,6 +57,14 @@ pub fn parse() -> Option<Args> {
 	match matches.opt_str("p") {
 		Some(port) => args.port = port,
 		None       => ()
+	}
+
+	match matches.opt_str("f") {
+		Some(period_str) => match from_str(period_str.as_slice()) {
+			Some(period) => args.period = period,
+			None         => return None
+		},
+		None => ()
 	}
 
 	args.headless = matches.opt_present("h");
