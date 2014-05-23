8f9eb18301aa24e419eecf96526b880f8fc3aea1
Hanno Braun <mail@hannobraun.de>
2014-05-23 16:44:21 +0000

    Remove camera acceptance test
    Two reasons:
    - This is a purely client-side feature. It really belongs in a component
      test.
    - I'm going to mess with the camera as part of my cleanup work. It's in
      the way.
    
    I'm going to replace it with a component test later, when things have
    settled down a bit.

--- tests/acceptance/basic.rs
+++ tests/acceptance/basic.rs
@@ -5,31 +5,6 @@ use common::testing::{
 
 
 #[test]
-fn the_camera_should_follow_the_ship() {
-	let     game_service = GameService::start();
-	let mut client       = Client::start(game_service.port);
-
-	let mut frame_1 = client.frame();
-	let mut frame_2 = client.frame();
-
-	while frame_1.ships.len() == 0 {
-		frame_1 = frame_2;
-		frame_2 = client.frame();
-	}
-
-	while frame_1.ships.get(0).position == frame_2.ships.get(0).position {
-		frame_2 = client.frame();
-	}
-
-	assert_eq!(
-		frame_1.ships.get(0).position,
-		frame_1.camera);
-	assert_eq!(
-		frame_2.ships.get(0).position,
-		frame_2.camera);
-}
-
-#[test]
 fn it_should_render_all_connected_clients() {
 	let     game_service = GameService::start();
 	let mut client_1     = Client::start(game_service.port);
