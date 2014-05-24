9aef51519a55ccb2409c875da89fbfa05dc73890
Hanno Braun <mail@hannobraun.de>
2014-05-24 12:53:22 +0000

    Refactor

--- source/rust/game-service/game/entities.rs
+++ source/rust/game-service/game/entities.rs
@@ -16,20 +16,29 @@ pub struct Entities {
 
 impl Entities {
 	pub fn create_ship(&mut self, id: ClientId) {
-		let velocity = Vec2(30.0, 10.0);
-		self.bodies.insert(id, Body {
-			position: Vec2::zero(),
-			velocity: velocity,
-			attitude: Radians::from_vec(velocity)
-		});
-
-		self.ships.insert(id, data::Ship {
-			missile_index: 0
-		});
+		Ship::create(id, &mut self.bodies, &mut self.ships);
 	}
 
 	pub fn destroy_ship(&mut self, id: ClientId) {
 		self.bodies.remove(&id);
 		self.ships.remove(&id);
+	}
+}
+
+
+struct Ship;
+
+impl Ship {
+	fn create(id: ClientId, bodies: &mut Components<Body>, ships: &mut Components<data::Ship>) {
+		let velocity = Vec2(30.0, 10.0);
+		bodies.insert(id, Body {
+			position: Vec2::zero(),
+			velocity: velocity,
+			attitude: Radians::from_vec(velocity)
+		});
+
+		ships.insert(id, data::Ship {
+			missile_index: 0
+		});
 	}
 }
