5f0d8ec5b8a928e02ffd1cb10598f1add9a0edfb
Hanno Braun <mail@hannobraun.de>
2014-05-27 10:53:44 +0000

    Move ECS infrastructure into its own module
    I'm about to move some common modules to libcommon, so they are going to
    need their own place.

---
+++ source/rust/libcommon/ecs/infra.rs
@@ -0,0 +1,29 @@
+use collections::HashMap;
+
+pub type EntityId      = u32;
+pub type Components<T> = HashMap<EntityId, T>;
+
+
+pub trait EntityTemplate2<A, C1, C2> {
+	fn create(&self,
+		id     : EntityId,
+		args   : A,
+		comps_1: &mut Components<C1>,
+		comps_2: &mut Components<C2>) {
+
+		let (c1, c2) = self.create_components(args);
+		comps_1.insert(id, c1);
+		comps_2.insert(id, c2);
+	}
+
+	fn destroy(&self,
+		id: EntityId, c1:
+		&mut Components<C1>,
+		c2: &mut Components<C2>) {
+
+		c1.remove(&id);
+		c2.remove(&id);
+	}
+
+	fn create_components(&self, args: A) -> (C1, C2);
+}
