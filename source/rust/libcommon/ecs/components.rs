cc77ccc9cc6f1431f9d07a2cdca938663884898e
Hanno Braun <mail@hannobraun.de>
2014-06-05 12:05:31 +0000

    Remove unused code

--- source/rust/libcommon/ecs/components.rs
+++ source/rust/libcommon/ecs/components.rs
@@ -1,18 +1,4 @@
 #[deriving(Clone, Decodable, Encodable, PartialEq, Show)]
-pub struct MissileKind {
-	dummy: bool // Required to derive Encodable
-}
-
-impl MissileKind {
-	pub fn new() -> MissileKind {
-		MissileKind {
-			dummy: false
-		}
-	}
-}
-
-
-#[deriving(Clone, Decodable, Encodable, PartialEq, Show)]
 pub enum Visual {
 	MissileVisual,
 	ShipVisual
