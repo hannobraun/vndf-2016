2d4f1a4d3d25604f91518f8747f475ca28a8c0ef
Hanno Braun <mail@hannobraun.de>
2014-06-18 14:16:21 +0200

    Implement entity updating in receiver function

--- source/rust/client/game/receiver.rs
+++ source/rust/client/game/receiver.rs
@@ -24,6 +24,14 @@ pub fn receive(world: &mut ClientWorld, perception: Perception) {
 			current_time
 		);
 	}
+
+	for entity in perception.updated.iter() {
+		*world.bodies.get_mut(&entity.id)  = entity.body.unwrap();
+		*world.visuals.get_mut(&entity.id) = entity.visual.unwrap();
+
+		world.interpolateds.get_mut(&entity.id).current      = entity.body;
+		world.interpolateds.get_mut(&entity.id).current_time = current_time;
+	}
 }
 
 
@@ -34,7 +42,10 @@ mod test {
 		SharedWorldEntity,
 		ShowAsMissile,
 	};
-	use common::physics::Body;
+	use common::physics::{
+		Body,
+		Vec2
+	};
 	use common::protocol::Perception;
 
 	use super::receive;
@@ -62,5 +73,34 @@ mod test {
 
 		assert_eq!(1, entities.len());
 		assert!(entities.get(0).id == 5);
+	}
+
+	#[test]
+	fn it_should_update_entities() {
+		let entity_id = 5;
+
+		let mut world = ClientWorld::new();
+		world.import_cliententity(entity_id, Body::default(), ShowAsMissile, 0);
+
+		let mut entity = SharedWorldEntity {
+			id    : entity_id,
+			visual: Some(ShowAsMissile),
+			body  : Some(Body::default()),
+		};
+		entity.body.get_mut_ref().position = Vec2(5.0, 8.0);
+
+		let perception = Perception {
+			self_id: 0,
+			added  : vec!(),
+			removed: vec!(),
+			updated: vec!(entity),
+		};
+
+		receive(&mut world, perception);
+
+		assert_eq!(
+			entity.body.unwrap(),
+			world.interpolateds.get(&entity_id).current.unwrap()
+		);
 	}
 }
