3afdbcc84913a5bc66ea6c51bb9049bda7da8274
Hanno Braun <mail@hannobraun.de>
2014-07-28 14:53:49 +0200

    Extract Action into a dedicated module

---
+++ source/rust/vndf/src/common/protocol/action.rs
@@ -0,0 +1,22 @@
+use common::json::{
+	from_json,
+	to_json
+};
+use physics::Radians;
+
+
+#[deriving(Decodable, Encodable, PartialEq, Show)]
+pub struct Action {
+	pub attitude: Radians,
+	pub missile : u64
+}
+
+impl Action {
+	pub fn from_string(s: &str) -> Result<Action, String> {
+		from_json(s)
+	}
+
+	pub fn to_string(&self) -> String {
+		to_json(self)
+	}
+}
--- source/rust/vndf/src/common/protocol/mod.rs
+++ source/rust/vndf/src/common/protocol/mod.rs
@@ -1,28 +1,6 @@
-use common::json::{
-	from_json,
-	to_json
-};
-use physics::Radians;
-
-
+pub use self::action::Action;
 pub use self::perception::Perception;
 
 
+mod action;
 mod perception;
-
-
-#[deriving(Decodable, Encodable, PartialEq, Show)]
-pub struct Action {
-	pub attitude: Radians,
-	pub missile : u64
-}
-
-impl Action {
-	pub fn from_string(s: &str) -> Result<Action, String> {
-		from_json(s)
-	}
-
-	pub fn to_string(&self) -> String {
-		to_json(self)
-	}
-}
