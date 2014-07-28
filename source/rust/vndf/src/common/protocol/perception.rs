0dfa368e05a72a999fd2888f07d58dd063dfd6b1
Hanno Braun <mail@hannobraun.de>
2014-07-28 14:48:51 +0200

    Extract Perception into dedicated module

---
+++ source/rust/vndf/src/common/protocol/perception.rs
@@ -0,0 +1,113 @@
+use serialize::{
+	Decodable,
+	Encodable,
+};
+use serialize::json::{
+	Decoder,
+	DecoderError,
+	Encoder,
+};
+use std::io::IoError;
+
+use common::json::{
+	from_json,
+	to_json
+};
+
+
+#[deriving(Clone, Decodable, Encodable, PartialEq, Show)]
+pub struct Perception<Id, T> {
+	pub self_id: Id,
+	pub added  : Vec<T>,
+	pub removed: Vec<T>,
+	pub updated: Vec<T>,
+}
+
+impl<
+	'a,
+	Id:
+		Ord +
+		Decodable<Decoder, DecoderError> +
+		Encodable<Encoder<'a>, IoError>,
+	T:
+		Decodable<Decoder, DecoderError> +
+		Encodable<Encoder<'a>, IoError>
+> Perception<Id, T> {
+	pub fn new(
+		get_id      : |&T| -> Id,
+		self_id     : Id,
+		mut previous: Vec<T>,
+		mut current : Vec<T>
+	) -> Perception<Id, T> {
+		previous.sort_by(|a, b| get_id(a).cmp(&get_id(b)));
+		current.sort_by(|a, b| get_id(a).cmp(&get_id(b)));
+
+		let mut previous = previous.move_iter();
+		let mut current  = current.move_iter();
+
+		let mut added   = Vec::new();
+		let mut removed = Vec::new();
+		let mut updated = Vec::new();
+
+		let mut p = previous.next();
+		let mut c = current.next();
+
+		while !(p.is_none() && c.is_none()) {
+			if p.is_none() {
+				added.push(c.take_unwrap());
+
+				c = current.next();
+
+				continue;
+			}
+			if c.is_none() {
+				removed.push(p.take_unwrap());
+
+				p = current.next();
+
+				continue;
+			}
+
+			let p_id = get_id(p.get_ref());
+			let c_id = get_id(c.get_ref());
+
+			if p_id == c_id {
+				updated.push(c.take_unwrap());
+
+				p = previous.next();
+				c = current.next();
+
+				continue;
+			}
+			if p_id < c_id {
+				removed.push(p.take_unwrap());
+
+				p = previous.next();
+
+				continue;
+			}
+			if p_id > c_id {
+				added.push(c.take_unwrap());
+
+				c = current.next();
+
+				continue;
+			}
+		}
+
+		Perception {
+			self_id: self_id,
+			added  : added,
+			removed: removed,
+			updated: updated,
+		}
+	}
+
+	pub fn from_string(s: &str) -> Result<Perception<Id, T>, String> {
+		from_json(s)
+	}
+
+	pub fn to_string(&self) -> String {
+		to_json(self)
+	}
+}
