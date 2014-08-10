018033a6a98bc4d44fa1987747aadbb9d715b44d
Hanno Braun <mail@hannobraun.de>
2014-08-10 14:41:06 +0200

    Remove unused crates

--- source/rust/vndf/src/lib.rs
+++ source/rust/vndf/src/lib.rs
@@ -7,11 +7,6 @@ extern crate libc;
 extern crate serialize;
 extern crate time;
 
-extern crate freetype;
-extern crate gl;
-extern crate glfw;
-extern crate stb_image;
-
 extern crate epoll;
 extern crate net;
 extern crate physics;
