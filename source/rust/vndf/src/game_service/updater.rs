9282753330362697adfee38b48a17acf8f5abb8a
Hanno Braun <mail@hannobraun.de>
2014-08-18 15:46:48 +0200

    Upgrade to latest Rust

--- source/rust/vndf/src/game_service/updater.rs
+++ source/rust/vndf/src/game_service/updater.rs
@@ -1,4 +1,5 @@
 use std::io::timer::sleep;
+use std::time::Duration;
 
 use game_service::events::{
 	GameEvent,
@@ -10,7 +11,7 @@ pub fn init(frame_time_in_ms: u64, game: Sender<GameEvent>) {
 	spawn(proc() {
 		loop {
 			game.send(Update(frame_time_in_ms as f64 / 1000.0));
-			sleep(frame_time_in_ms);
+			sleep(Duration::milliseconds(frame_time_in_ms as i32));
 		}
 	});
 }
