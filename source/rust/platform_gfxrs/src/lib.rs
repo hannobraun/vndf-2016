03576de6edf94b7f5f07f76b09fdda2a09400f69
Hanno Braun <mail@hannobraun.de>
2014-08-19 19:51:36 +0200

    Integrate Window

--- source/rust/platform_gfxrs/src/lib.rs
+++ source/rust/platform_gfxrs/src/lib.rs
@@ -22,6 +22,10 @@ use platform::{
 	Input,
 	Platform,
 };
+use window::Window;
+
+
+mod window;
 
 
 static VERTEX_SRC: gfx::ShaderSource = shaders! {
@@ -57,36 +61,25 @@ struct Vertex {
 
 
 struct DesktopPlatform {
-	glfw  : glfw::Glfw,
-	window: glfw::Window,
-	events: sync::comm::Receiver<(f64,glfw::WindowEvent)>,
-	device: device::gl::GlDevice,
+	window: Window,
 }
 
 impl Platform for DesktopPlatform {
 	fn input(&mut self) -> Result<Input, String> {
-		self.glfw.poll_events();
+		self.window.poll_events();
 
 		let mut input = Input::default();
 		input.exit = self.window.should_close();
-
-		for (_, event) in glfw::flush_messages(&self.events) {
-			match event {
-				glfw::KeyEvent(glfw::KeyEscape, _, glfw::Press, _) =>
-					input.exit = true,
-
-				_ => {},
-			}
-		}
 
 		Ok(input)
 	}
 
 	fn render(&mut self, frame: &Frame) {
-		let (width, height) = self.window.get_framebuffer_size();
-
-		let frame = gfx::Frame::new(width as u16, height as u16);
-		let mut list = self.device.create_draw_list();
+		let frame = gfx::Frame::new(
+			self.window.width as u16,
+			self.window.height as u16
+		);
+		let mut list = self.window.device.create_draw_list();
 
 		let state = gfx::DrawState::new();
 		let vertex_data = vec![
@@ -94,8 +87,8 @@ impl Platform for DesktopPlatform {
 			Vertex { pos: [ 0.5, -0.5 ], color: [0.0, 1.0, 0.0]  },
 			Vertex { pos: [ 0.0, 0.5 ], color: [0.0, 0.0, 1.0]  }
 		];
-		let mesh = self.device.create_mesh(vertex_data);
-		let program: gfx::shade::EmptyProgram = self.device.link_program(
+		let mesh = self.window.device.create_mesh(vertex_data);
+		let program: gfx::shade::EmptyProgram = self.window.device.link_program(
 			VERTEX_SRC.clone(), FRAGMENT_SRC.clone()).unwrap();
 
 		list.clear(
@@ -110,41 +103,18 @@ impl Platform for DesktopPlatform {
 		list.draw(&mesh, mesh.get_slice(), &frame, &program, &state)
 			.unwrap();
 
-		self.device.submit(list.as_slice());
+		self.window.device.submit(list.as_slice());
         self.window.swap_buffers();
 	}
 }
 
 
 pub fn init() -> Box<Platform> {
-	let width  = 800;
-	let height = 600;
-
-	let glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-
-	glfw.window_hint(glfw::ContextVersion(3, 2));
-	glfw.window_hint(glfw::OpenglForwardCompat(true));
-	glfw.window_hint(glfw::OpenglProfile(glfw::OpenGlCoreProfile));
-
-	let (window, events) = glfw
-		.create_window(
-			width,
-			height,
-			"Von Neumann Defense Force *EARLY PROTOTYPE*",
-			glfw::Windowed)
-		.expect("failed to create window");
-
-	window.make_current();
-	window.set_key_polling(true);
-
-	let device = gfx::GlDevice::new(|s| glfw.get_proc_address(s));
+	let window = Window::create(800, 600);
 
 	box
 		DesktopPlatform {
-			glfw  : glfw,
 			window: window,
-			events: events,
-			device: device,
 		}
 	as Box<Platform>
 }
