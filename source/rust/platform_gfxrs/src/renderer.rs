5125f7eb2ede8bf8e3ac0a434796681719fc8b53
Hanno Braun <mail@hannobraun.de>
2014-08-20 15:19:25 +0200

    Ignore color argument in shader

--- source/rust/platform_gfxrs/src/renderer.rs
+++ source/rust/platform_gfxrs/src/renderer.rs
@@ -21,10 +21,9 @@ static VERTEX_SRC: gfx::ShaderSource = shaders! {
 GLSL_150: b"
 	#version 150 core
 	in vec2 pos;
-	in vec3 color;
 	out vec4 v_Color;
 	void main() {
-		v_Color = vec4(color, 1.0);
+		v_Color = vec4(0.0, 0.0, 1.0, 1.0);
 		gl_Position = vec4(pos, 0.0, 1.0);
 	}
 "
