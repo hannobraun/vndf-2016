a5a752ec696d61eba0a342522eb3d99d6e3e674d
Hanno Braun <mail@hannobraun.de>
2014-09-09 18:43:38 +0200

    Refactor

--- source/rust/platform_gfxrs/src/renderer.rs
+++ source/rust/platform_gfxrs/src/renderer.rs
@@ -216,13 +216,13 @@ impl Renderer {
 	}
 
 	fn draw_craft(&mut self, body: &Body, camera: &Vec2, texture_id: &str) {
-		self.draw_texture(body, camera, texture_id);
+		self.draw_texture(&body.position, camera, texture_id);
 	}
 
-	fn draw_texture(&mut self, body: &Body, camera: &Vec2, id: &str) {
+	fn draw_texture(&mut self, position: &Vec2, camera: &Vec2, id: &str) {
 		let ref texture = self.textures[id.to_string()];
 
-		let Vec2(pos_x, pos_y) = body.position + texture.offset;
+		let Vec2(pos_x, pos_y) = position + texture.offset;
 		let &Vec2(camera_x, camera_y) = camera;
 
 		let params = TextureParams {
