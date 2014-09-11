17c79122ba6dab21c2f7114eeebd28a82061ca36
Hanno Braun <mail@hannobraun.de>
2014-09-11 14:52:35 +0200

    Make camera angles controllable via WASD
    This is not visible yet.

--- source/rust/platform_desktop/src/inputhandler.rs
+++ source/rust/platform_desktop/src/inputhandler.rs
@@ -3,7 +3,10 @@ use std::rc::Rc;
 
 use glfw;
 
-use physics::Radians;
+use physics::{
+	Degrees,
+	Radians,
+};
 use platform::Input;
 use window::Window;
 
@@ -11,7 +14,8 @@ use window::Window;
 pub struct InputHandler {
 	window   : Rc<Window>,
 	attitude : Radians,
-	missile  : u64
+	camera   : (Radians, Radians),
+	missile  : u64,
 }
 
 impl InputHandler {
@@ -19,7 +23,8 @@ impl InputHandler {
 		InputHandler {
 			window  : window,
 			attitude: Radians(0.0),
-			missile : 0
+			camera  : (Degrees(45.0).to_radians(), Degrees(45.0).to_radians()),
+			missile : 0,
 		}
 	}
 
@@ -48,10 +53,25 @@ impl InputHandler {
 			self.missile += 1;
 		}
 
+		let camera_speed = 1.0;
+		if self.window.key_pressed(glfw::KeyA) {
+			*self.camera.mut0() = self.camera.val0() - Radians(camera_speed);
+		}
+		if self.window.key_pressed(glfw::KeyD) {
+			*self.camera.mut0() = self.camera.val0() + Radians(camera_speed);
+		}
+		if self.window.key_pressed(glfw::KeyS) {
+			*self.camera.mut1() = self.camera.val1() - Radians(camera_speed);
+		}
+		if self.window.key_pressed(glfw::KeyW) {
+			*self.camera.mut1() = self.camera.val1() + Radians(camera_speed);
+		}
+
 		let mut input = Input::default();
 
 		input.exit     = self.window.should_close();
 		input.attitude = self.attitude;
+		input.camera   = self.camera;
 		input.missile  = self.missile;
 
 		input
