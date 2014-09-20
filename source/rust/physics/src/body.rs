bb9f27cf0b0a07f82185c1b94dc2bd11cc908ab7
Hanno Braun <mail@hannobraun.de>
2014-09-20 12:43:28 +0200

    Add force to Body
    It's currently unused.

--- source/rust/physics/src/body.rs
+++ source/rust/physics/src/body.rs
@@ -8,6 +8,7 @@ use cgmath::{
 pub struct Body {
 	pub position: Vector3<f64>,
 	pub velocity: Vector3<f64>,
+	pub force   : Vector3<f64>,
 	pub attitude: Quaternion<f64>,
 }
 
@@ -16,6 +17,7 @@ impl Body {
 		Body {
 			position: Vector3::zero(),
 			velocity: Vector3::zero(),
+			force   : Vector3::zero(),
 			attitude: Quaternion::zero(),
 		}
 	}
