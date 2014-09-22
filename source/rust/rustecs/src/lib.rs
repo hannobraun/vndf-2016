9fd2e9ee8356343eb8146ddc7c014fc0a0d35ac5
Hanno Braun <mail@hannobraun.de>
2014-09-22 17:44:30 +0200

    Remove unused trait

--- source/rust/rustecs/src/lib.rs
+++ source/rust/rustecs/src/lib.rs
@@ -9,11 +9,6 @@ pub type EntityId      = u32;
 pub type Components<T> = HashMap<EntityId, T>;
 
 
-pub trait Entity {
-	fn id(&self) -> EntityId;
-}
-
-
 pub fn components<T>() -> Components<T> {
 	HashMap::new()
 }
