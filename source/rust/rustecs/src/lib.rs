08aa9a98ad48939d8617ed1fd7a83eb9c63bf924
Hanno Braun <mail@hannobraun.de>
2014-09-22 17:39:17 +0200

    Remove unused trait

--- source/rust/rustecs/src/lib.rs
+++ source/rust/rustecs/src/lib.rs
@@ -13,16 +13,6 @@ pub trait Entity {
 	fn id(&self) -> EntityId;
 }
 
-pub trait World<E: Entity> {
-	fn new() -> Self;
-
-	fn from_entities(entities: Vec<E>) -> Self;
-	fn to_entities(&self) -> Vec<E>;
-
-	fn import_entity(&mut self, entity: E) -> bool;
-	fn destroy_entity(&mut self, id: EntityId) -> bool;
-}
-
 
 pub fn components<T>() -> Components<T> {
 	HashMap::new()
