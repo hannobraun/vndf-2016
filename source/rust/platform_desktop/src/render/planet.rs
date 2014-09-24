a3b879cbdc185feadadc5965dbd9cb6d8c411d4e
Hanno Braun <mail@hannobraun.de>
2014-09-24 18:24:33 +0200

    Modify planet to always face the camera

--- source/rust/platform_desktop/src/render/planet.rs
+++ source/rust/platform_desktop/src/render/planet.rs
@@ -1,8 +1,8 @@
 use cgmath::{
+	EuclideanVector,
 	FixedArray,
-	Matrix4,
-	Vector,
 	Vector2,
+	Vector3,
 };
 use gfx::{
 	mod,
@@ -24,12 +24,20 @@ static VERTEX_SHADER: gfx::ShaderSource = shaders! {
 	GLSL_150: b"
 		#version 150 core
 
+		uniform vec2 size;
 		uniform mat4 transform;
+		uniform vec3 camera_right_world;
+		uniform vec3 camera_up_world;
 
 		in vec3 position;
 
 		void main() {
-			gl_Position = transform * vec4(position, 1.0);
+			vec3 position2 =
+				vec3(0.0, 0.0, 0.0)
+				+ camera_right_world * position.x * size.x
+				+ camera_up_world * position.y * size.y;
+
+			gl_Position = transform * vec4(position2, 1.0);
 		}
 	"
 };
@@ -49,22 +57,26 @@ static FRAGMENT_SHADER: gfx::ShaderSource = shaders! {
 
 #[shader_param(Batch)]
 struct Params {
+	size     : [f32, ..2],
 	transform: [[f32, ..4], ..4],
+
+	camera_right_world: [f32, ..3],
+	camera_up_world   : [f32, ..3],
 }
 
 
 pub struct Planet {
 	batch : Batch,
-	offset: Vector2<f32>,
+	size  : Vector2<f32>,
 }
 
 impl Planet {
 	pub fn new(graphics: &mut Graphics, width: f32, height: f32) -> Planet {
 		let vertices = [
-			Vertex::new([   0.0,    0.0, 0.0 ], [ 0.0, 1.0 ]),
-			Vertex::new([ width,    0.0, 0.0 ], [ 1.0, 1.0 ]),
-			Vertex::new([   0.0, height, 0.0 ], [ 0.0, 0.0 ]),
-			Vertex::new([ width, height, 0.0 ], [ 1.0, 0.0 ]),
+			Vertex::new([ -0.5, -0.5, 0.0 ], [ 0.0, 1.0 ]),
+			Vertex::new([  0.5, -0.5, 0.0 ], [ 1.0, 1.0 ]),
+			Vertex::new([ -0.5,  0.5, 0.0 ], [ 0.0, 0.0 ]),
+			Vertex::new([  0.5,  0.5, 0.0 ], [ 1.0, 0.0 ]),
 		];
 
 		let mesh  = graphics.device.create_mesh(vertices);
@@ -88,7 +100,7 @@ impl Planet {
 
 		Planet {
 			batch : batch,
-			offset: Vector2::new(width, height).mul_s(-0.5),
+			size  : Vector2::new(width, height),
 		}
 	}
 
@@ -99,12 +111,31 @@ impl Planet {
 		projection: Transform,
 		camera    : &Camera,
 	) {
-		let transform = projection
-			.mul(&camera.to_transform())
-			.mul(&Matrix4::from_translation(&self.offset.extend(0.0)));
+		let view = camera.to_transform();
+
+		let camera_right_world =
+			Vector3::new(
+				view[0][0],
+				view[1][0],
+				view[2][0],
+			)
+			.normalize();
+		let camera_up_world =
+			Vector3::new(
+				view[0][1],
+				view[1][1],
+				view[2][1],
+			)
+			.normalize();
+
+		let transform = projection.mul(&view);
 
 		let params = Params {
+			size     : self.size.into_fixed(),
 			transform: transform.into_fixed(),
+
+			camera_right_world: camera_right_world.into_fixed(),
+			camera_up_world   : camera_up_world.into_fixed(),
 		};
 
 		graphics.draw(
