a015eb6791f45ce53a6547e7310863834c0547ed
Hanno Braun <mail@hannobraun.de>
2014-09-29 18:43:13 +0200

    Convert load_image into a method on Image

--- source/rust/platform_desktop/src/images.rs
+++ source/rust/platform_desktop/src/images.rs
@@ -5,12 +5,6 @@ use stb_image::image;
 
 pub type Images = HashMap<String, Image>;
 
-pub struct Image {
-	pub data  : Vec<u8>,
-	pub width : uint,
-	pub height: uint
-}
-
 
 pub fn load() -> Images {
 	let paths = vec!(
@@ -21,7 +15,7 @@ pub fn load() -> Images {
 	let mut images = HashMap::new();
 
 	for &path in paths.iter() {
-		let image = match load_image(path) {
+		let image = match Image::load(path) {
 			Ok(image)  => image,
 			Err(error) => fail!(error)
 		};
@@ -32,23 +26,32 @@ pub fn load() -> Images {
 	images
 }
 
-fn load_image(path: &str) -> Result<Image, String> {
-	match image::load(&Path::new(path)) {
-		image::ImageU8(image) => {
-			let width  = image.width;
-			let height = image.height;
 
-			Ok(Image {
-				data  : image.data,
-				width : width,
-				height: height
-			})
-		},
+pub struct Image {
+	pub data  : Vec<u8>,
+	pub width : uint,
+	pub height: uint
+}
 
-		image::ImageF32(_) =>
-			Err(format!("Unexpected image type: ImageF32")),
+impl Image {
+	fn load(path: &str) -> Result<Image, String> {
+		match image::load(&Path::new(path)) {
+			image::ImageU8(image) => {
+				let width  = image.width;
+				let height = image.height;
 
-		image::Error(message) =>
-			Err(message)
+				Ok(Image {
+					data  : image.data,
+					width : width,
+					height: height
+				})
+			},
+
+			image::ImageF32(_) =>
+				Err(format!("Unexpected image type: ImageF32")),
+
+			image::Error(message) =>
+				Err(message)
+		}
 	}
 }
