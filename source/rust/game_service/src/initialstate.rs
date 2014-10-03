b23f34cb320276c700ed80c32b7869b49bbb71c5
Hanno Braun <mail@hannobraun.de>
2014-10-03 16:01:52 +0200

    Define initial state as JSON

--- source/rust/game_service/src/initialstate.rs
+++ source/rust/game_service/src/initialstate.rs
@@ -1,24 +1,45 @@
-use cgmath::Vector3;
+use serialize::json;
 
 use ecs::World;
 use game::ecs::Planet;
 
 
 pub fn load(world: &mut World) {
-	let initial_state = InitialState {
-		planets: vec![
-			Planet {
-				position: Vector3::zero(),
-				radius  : 2576.0,
-				color   : Vector3::new(0.8, 0.68, 0.27),
-			},
-			Planet {
-				position: Vector3::new(0.0, 5000.0, 0.0),
-				radius  : 480.0,
-				color   : Vector3::new(0.5, 0.7, 0.7),
-			},
-		]
-	};
+	let initial_state_as_json = "
+		{
+			\"planets\": [
+				{
+					\"position\": {
+						\"x\": 0.0,
+						\"y\": 0.0,
+						\"z\": 0.0
+					},
+					\"radius\": 2576.0,
+					\"color\": {
+						\"x\": 0.8,
+						\"y\": 0.68,
+						\"z\": 0.27
+					}
+				},
+				{
+					\"position\": {
+						\"x\": 0.0,
+						\"y\": 5000.0,
+						\"z\": 0.0
+					},
+					\"radius\": 480.0,
+					\"color\": {
+						\"x\": 0.5,
+						\"y\": 0.7,
+						\"z\": 0.7
+					}
+				}
+			]
+		}
+	";
+
+	let initial_state: InitialState =
+		json::decode(initial_state_as_json).unwrap();
 
 	for planet in initial_state.planets.iter()  {
 		world.create_planet(
