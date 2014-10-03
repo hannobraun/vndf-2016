ca8090fb15bdc2a7f8a42e1c4cbc337b0c71f21c
Hanno Braun <mail@hannobraun.de>
2014-10-03 16:08:29 +0200

    Move initial state into external JSON file

--- source/rust/game_service/src/initialstate.rs
+++ source/rust/game_service/src/initialstate.rs
@@ -1,45 +1,18 @@
 use serialize::json;
+use std::io::File;
 
 use ecs::World;
 use game::ecs::Planet;
 
 
 pub fn load(world: &mut World) {
-	let initial_state_as_json = "
-		{
-			\"planets\": [
-				{
-					\"position\": {
-						\"x\": 0.0,
-						\"y\": 0.0,
-						\"z\": 0.0
-					},
-					\"radius\": 2576.0,
-					\"color\": {
-						\"x\": 0.8,
-						\"y\": 0.68,
-						\"z\": 0.27
-					}
-				},
-				{
-					\"position\": {
-						\"x\": 0.0,
-						\"y\": 5000.0,
-						\"z\": 0.0
-					},
-					\"radius\": 480.0,
-					\"color\": {
-						\"x\": 0.5,
-						\"y\": 0.7,
-						\"z\": 0.7
-					}
-				}
-			]
-		}
-	";
+	let initial_state_as_json =
+		File::open(&Path::new("initial-state.json"))
+			.read_to_string()
+			.unwrap();
 
 	let initial_state: InitialState =
-		json::decode(initial_state_as_json).unwrap();
+		json::decode(initial_state_as_json.as_slice()).unwrap();
 
 	for planet in initial_state.planets.iter()  {
 		world.create_planet(
