9ae576c90e0ad8f4f19f4422b5bafce9232e9b3d
Hanno Braun <mail@hannobraun.de>
2014-10-04 18:10:49 +0200

    Remove unused code

--- source/rust/game_service/src/ecs.rs
+++ source/rust/game_service/src/ecs.rs
@@ -69,15 +69,6 @@ world!(
 )
 
 
-pub fn destroy_ship(world: &mut World, client_id: ConnId) {
-	let id = match entity_id_from_client_id(world, client_id) {
-		Some(id) => id,
-		None     => return
-	};
-
-	world.destroy_entity(id);
-}
-
 pub fn entity_id_from_client_id(
 	world    : &World,
 	client_id: ConnId
