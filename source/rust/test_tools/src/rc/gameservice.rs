a94cf6de974f86de6a731a18ff6b485e4eb1a043
Hanno Braun <mail@hannobraun.de>
2014-10-04 11:56:24 +0200

    Fix tests
    The tests where failing after I introduced the loading of initial state
    from an external file. This solution is obviously a band-aid, but it
    won't be permanent. Soon, acceptance tests are going to generate their
    own initial state.

--- source/rust/test_tools/src/rc/gameservice.rs
+++ source/rust/test_tools/src/rc/gameservice.rs
@@ -1,3 +1,4 @@
+use std::os;
 use std::rand::random;
 
 use test_infra::Process;
@@ -12,11 +13,19 @@ impl GameService {
 	pub fn start() -> GameService {
 		let port = random::<u16>() % 10000 + 40000;
 
+		let mut initial_state = "".to_string();
+		for &(ref key, ref value) in os::env().iter() {
+			if *key == "INITIAL_STATE".to_string() {
+				initial_state = value.clone();
+			}
+		}
+
 		let mut process = Process::start(
 			"vndf-game-service",
 			[
-				"--port".to_string(), port.to_string(),
-				"--frame-time".to_string(), "10".to_string()
+				"--port".to_string()         , port.to_string(),
+				"--frame-time".to_string()   , "10".to_string(),
+				"--initial-state".to_string(), initial_state,
 			]
 		);
 		process.read_stdout_line(); // Make sure it's ready
