9cab37b6f403033f5f280f12455cab82d5f13347
Hanno Braun <mail@hannobraun.de>
2014-10-06 13:16:27 +0200

    Add mass to planet

--- source/rust/game/src/ecs.rs
+++ source/rust/game/src/ecs.rs
@@ -7,6 +7,7 @@ use physics::Body;
 pub struct Planet {
 	pub position: Vector3<f64>,
 	pub radius  : f64,
+	pub mass    : f64,
 	pub color   : Vector3<f32>,
 }
 
@@ -15,6 +16,7 @@ impl Planet {
 		Planet {
 			position: Vector3::zero(),
 			radius  : 1.0,
+			mass    : 1.0,
 			color   : Vector3::new(1.0, 1.0, 1.0),
 		}
 	}
@@ -26,6 +28,11 @@ impl Planet {
 
 	pub fn with_radius(mut self, radius: f64) -> Planet {
 		self.radius = radius;
+		self
+	}
+
+	pub fn with_mass(mut self, mass: f64) -> Planet {
+		self.mass = mass;
 		self
 	}
 }
