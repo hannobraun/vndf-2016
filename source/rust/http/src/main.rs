5481b8daa71d486a3207d1fe911c390d1ea21e80
Hanno Braun <mail@hannobraun.de>
2014-10-12 20:20:58 +0200

    Serve static files

--- source/rust/http/src/main.rs
+++ source/rust/http/src/main.rs
@@ -1,22 +1,16 @@
 extern crate iron;
+extern crate static_file;
 
 
 use std::io::net::ip::Ipv4Addr;
 
-use iron::{
-	status,
-	Iron,
-	IronResult,
-	Request,
-	Response,
-};
-
-
-fn hello_world(_: &mut Request) -> IronResult<Response> {
-	Ok(Response::with(status::Ok, "Hello, world!"))
-}
+use iron::Iron;
+use static_file::Static;
 
 
 fn main() {
-	Iron::new(hello_world).listen(Ipv4Addr(127, 0, 0, 1), 3000);
+	Iron::new(
+		Static::new(Path::new("/home/hanno/Projects/vndf/source/http/public"))
+	)
+	.listen(Ipv4Addr(127, 0, 0, 1), 3000);
 }
