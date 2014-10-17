ccd166fcba3cf74e358ef123bb8305f0648e4c73
Hanno Braun <mail@hannobraun.de>
2014-10-17 15:02:25 +0200

    Add no-op cache layer to http
    The cache doesn't do anything right now and just always returns the
    response from the inner handler.

--- source/rust/http/src/main.rs
+++ source/rust/http/src/main.rs
@@ -4,13 +4,42 @@ extern crate static_file;
 
 use std::io::net::ip::Ipv4Addr;
 
-use iron::Iron;
+use iron::{
+	Handler,
+	Iron,
+	IronResult,
+	Request,
+	Response,
+};
 use static_file::Static;
+
+
+struct Cache<H: Handler> {
+	cached_handler: H,
+}
+
+impl<H: Handler> Cache<H> {
+	pub fn new(cached_handler: H) -> Cache<H> {
+		Cache {
+			cached_handler: cached_handler,
+		}
+	}
+}
+
+impl<H: Handler> Handler for Cache<H> {
+	fn call(&self, request: &mut Request) -> IronResult<Response> {
+		self.cached_handler.call(request)
+	}
+}
 
 
 fn main() {
 	Iron::new(
-		Static::new(Path::new("/home/hanno/Projects/vndf/source/http/public"))
+		Cache::new(
+			Static::new(
+				Path::new("/home/hanno/Projects/vndf/source/http/public")
+			)
+		)
 	)
 	.listen(Ipv4Addr(127, 0, 0, 1), 3000);
 
