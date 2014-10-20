f94b596543182e53231828ad8a73e0b30c40cafb
Hanno Braun <mail@hannobraun.de>
2014-10-20 11:26:20 +0200

    Replace caching code with `StaticWithCache`
    This is a new handler I wrote for the static-file middleware. Currently
    this depends on my own fork. The following pull requests are pending:
    - https://github.com/iron/static-file/pull/47
    - https://github.com/iron/static-file/pull/49

--- source/rust/http/src/main.rs
+++ source/rust/http/src/main.rs
@@ -6,50 +6,14 @@ extern crate static_file;
 
 use std::io::net::ip::Ipv4Addr;
 
-use iron::{
-	Handler,
-	Iron,
-	IronResult,
-	Request,
-	Response,
-};
-use static_file::Static;
-
-
-struct Cache<H: Handler> {
-	cached_handler: H,
-}
-
-impl<H: Handler> Cache<H> {
-	pub fn new(cached_handler: H) -> Cache<H> {
-		Cache {
-			cached_handler: cached_handler,
-		}
-	}
-}
-
-impl<H: Handler> Handler for Cache<H> {
-	fn call(&self, request: &mut Request) -> IronResult<Response> {
-		match self.cached_handler.call(request) {
-			Ok(mut response) => {
-				response.headers.cache_control =
-					Some("public, max-age=31536000".to_string());
-				response.headers.last_modified = Some(time::now_utc());
-
-				Ok(response)
-			},
-			Err(error) => Err(error),
-		}
-	}
-}
+use iron::Iron;
+use static_file::StaticWithCache;
 
 
 fn main() {
 	Iron::new(
-		Cache::new(
-			Static::new(
-				Path::new("/home/hanno/Projects/vndf/source/http/public")
-			)
+		StaticWithCache::new(
+			Path::new("/home/hanno/Projects/vndf/source/http/public")
 		)
 	)
 	.listen(Ipv4Addr(127, 0, 0, 1), 3000);
