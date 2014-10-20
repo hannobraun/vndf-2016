701e6fd5691e207a11852b7eee43c390d2c36f12
Hanno Braun <mail@hannobraun.de>
2014-10-20 12:01:37 +0200

    Move handler into dedicated module

--- source/rust/rocks/src/main.rs
+++ source/rust/rocks/src/main.rs
@@ -12,36 +12,12 @@ use getopts::{
 	getopts,
 	optopt,
 };
+use iron::Iron;
 
-use iron::{
-	Handler,
-	Iron,
-	IronResult,
-	Request,
-	Response,
-};
-use static_file::StaticWithCache;
+use handler::RocksHandler;
 
 
-struct RocksHandler {
-	static_handler: StaticWithCache,
-}
-
-impl RocksHandler {
-	pub fn new(root_path: Path) -> RocksHandler {
-		let public_path = root_path.join("public");
-
-		RocksHandler {
-			static_handler: StaticWithCache::new(public_path),
-		}
-	}
-}
-
-impl Handler for RocksHandler {
-	fn call(&self, request: &mut Request) -> IronResult<Response> {
-		self.static_handler.call(request)
-	}
-}
+mod handler;
 
 
 fn main() {
