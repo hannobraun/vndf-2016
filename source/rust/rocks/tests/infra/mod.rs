5f1a6549e2bcb26344e60b370916c13f47861edf
Hanno Braun <mail@hannobraun.de>
2014-10-21 19:06:10 +0200

    Add acceptance test infrastructure for Rust
    This commit adds the file, but that isn't included anywhere yet. I have
    a test that is using it, but that fails right now due to problems I
    wasn't able to figure out yet.

---
+++ source/rust/rocks/tests/infra/mod.rs
@@ -0,0 +1,50 @@
+use std::io::net::ip::Port;
+
+use acceptance::{
+	random_port,
+	Process,
+	Tree,
+};
+use hyper::client::{
+	Request,
+	Response,
+};
+use hyper::Url;
+
+
+pub struct Rocks {
+	pub port: Port,
+
+	_process: Process,
+}
+
+impl Rocks {
+	pub fn start(tree: Tree) -> Rocks {
+		let port = random_port(8000, 9000);
+
+		let mut process = Process::start("rocks", [
+			format!("--port={}", port).as_slice(),
+			format!("--root={}", tree.root().display()).as_slice(),
+		]);
+
+		process.read_stdout_line(); // Make sure it's ready
+
+		Rocks {
+			port    : port,
+			_process: process,
+		}
+	}
+
+	pub fn get(&self, path: &str) -> Response {
+		let url = format!("http://localhost:{}{}", self.port, path);
+
+		print!("url: {}\n", url);
+
+		Request::get(Url::parse(url.as_slice()).unwrap())
+			.unwrap_or_else(|e| fail!("get failed: {}", e))
+			.start()
+			.unwrap_or_else(|e| fail!("start failed: {}", e))
+			.send()
+			.unwrap_or_else(|e| fail!("send failed: {}", e))
+	}
+}
