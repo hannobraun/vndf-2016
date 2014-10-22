328f3346df773e7a7e178944595318fa5a812dd9
Hanno Braun <mail@hannobraun.de>
2014-10-22 20:36:41 +0200

    Convert run_plugin into a function

--- source/rust/rocks/src/handler.rs
+++ source/rust/rocks/src/handler.rs
@@ -27,54 +27,6 @@ impl RocksHandler {
 			source_path   : root_path.join("source"),
 		}
 	}
-
-	fn run_plugin(&self, request: &Request) -> Option<IronResult<Response>> {
-		let requested_path =
-			self.source_path.join_many(request.url.path.as_slice());
-
-		let path_candidates = [
-			requested_path.with_filename(
-				format!("{}.response", requested_path.filename_str().unwrap()),
-			),
-			requested_path.join(".response"),
-		];
-
-		let mut source_file = None;
-		for candidate in path_candidates.iter() {
-			if candidate.exists() {
-				source_file = Some(candidate);
-			}
-		}
-
-		let source_file = match source_file {
-			Some(path) => path,
-			None       => return None,
-		};
-
-		let data =
-			Parser::new(
-				File::open(source_file).read_to_string().unwrap().as_slice()
-			)
-			.parse()
-			.unwrap();
-
-		let code     = data["code".to_string()].as_integer().unwrap();
-		let location = data["location".to_string()].as_str().unwrap();
-
-		let mut url = request.url.clone();
-		url.path = location
-			.split('/')
-			.filter(|&s| s != "")
-			.map(|s| s.to_string())
-			.collect();
-
-		let response = Response::redirect(
-			FromPrimitive::from_i64(code).unwrap(),
-			url,
-		);
-
-		Some(Ok(response))
-	}
 }
 
 impl Handler for RocksHandler {
@@ -83,7 +35,7 @@ impl Handler for RocksHandler {
 			Ok(response) => {
 				match response.status {
 					Some(status::NotFound) => {
-						match self.run_plugin(request) {
+						match run_plugin(&self.source_path, request) {
 							Some(result) => result,
 							None         => Ok(response),
 						}
@@ -96,4 +48,56 @@ impl Handler for RocksHandler {
 		}
 
 	}
+}
+
+
+fn run_plugin(
+	source_path: &Path,
+	request    : &Request
+) -> Option<IronResult<Response>> {
+	let requested_path =
+		source_path.join_many(request.url.path.as_slice());
+
+	let path_candidates = [
+		requested_path.with_filename(
+			format!("{}.response", requested_path.filename_str().unwrap()),
+		),
+		requested_path.join(".response"),
+	];
+
+	let mut source_file = None;
+	for candidate in path_candidates.iter() {
+		if candidate.exists() {
+			source_file = Some(candidate);
+		}
+	}
+
+	let source_file = match source_file {
+		Some(path) => path,
+		None       => return None,
+	};
+
+	let data =
+		Parser::new(
+			File::open(source_file).read_to_string().unwrap().as_slice()
+		)
+		.parse()
+		.unwrap();
+
+	let code     = data["code".to_string()].as_integer().unwrap();
+	let location = data["location".to_string()].as_str().unwrap();
+
+	let mut url = request.url.clone();
+	url.path = location
+		.split('/')
+		.filter(|&s| s != "")
+		.map(|s| s.to_string())
+		.collect();
+
+	let response = Response::redirect(
+		FromPrimitive::from_i64(code).unwrap(),
+		url,
+	);
+
+	Some(Ok(response))
 }
