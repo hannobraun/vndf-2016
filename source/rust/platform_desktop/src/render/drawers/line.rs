069557111197c1564a353b725e2e3613b887f6e3
Hanno Braun <mail@hannobraun.de>
2014-11-21 14:06:07 +0100

    Replace LineDrawer with Drawer

--- source/rust/platform_desktop/src/render/drawers/line.rs
+++ source/rust/platform_desktop/src/render/drawers/line.rs
@@ -4,10 +4,7 @@ use cgmath::{
 };
 use gfx::{
 	mod,
-	DeviceHelper,
 	DrawState,
-	Frame,
-	ToSlice,
 };
 
 use render::{
@@ -17,7 +14,10 @@ use render::{
 	Vertex,
 };
 
-use super::Draw;
+use super::{
+	Draw,
+	Drawer,
+};
 
 
 #[shader_param(Batch)]
@@ -25,51 +25,6 @@ struct Params {
 	center   : [f32, ..3],
 	position : [f32, ..3],
 	transform: [[f32, ..4], ..4],
-}
-
-
-pub struct LineDrawer {
-	pub batch: Batch,
-}
-
-impl LineDrawer {
-	pub fn new(graphics: &mut Graphics, draw_state: &DrawState) -> LineDrawer {
-		let vertices = [
-			Vertex::new([ 0.0, 0.0, 0.0 ], [ 0.0, 0.0 ]),
-			Vertex::new([ 0.0, 0.0, 1.0 ], [ 0.0, 0.0 ]),
-		];
-
-		let mesh  = graphics.device.create_mesh(vertices);
-		let slice = mesh.to_slice(gfx::Line);
-
-		let program = graphics.device
-			.link_program(
-				shaders::vertex::LINE.clone(),
-				shaders::fragment::SIMPLE.clone()
-			)
-			.unwrap_or_else(|error| panic!("error linking program: {}", error));
-
-		let batch = graphics
-			.make_batch(
-				&program,
-				&mesh,
-				slice,
-				draw_state,
-			)
-			.unwrap();
-
-		LineDrawer {
-			batch: batch,
-		}
-	}
-
-	pub fn draw(&self, graphics: &mut Graphics, frame: &Frame, line: &Line) {
-		graphics.draw(
-			&self.batch,
-			&line.to_params(),
-			frame
-		);
-	}
 }
 
 
@@ -87,4 +42,26 @@ impl Draw<Params> for Line {
 			transform: self.transform.into_fixed(),
 		}
 	}
+}
+
+
+pub type LineDrawer = Drawer<_ParamsLink, Params>;
+
+pub fn new_drawer(
+	graphics: &mut Graphics,
+	draw_state: &DrawState
+) -> LineDrawer {
+	let vertices = [
+		Vertex::new([ 0.0, 0.0, 0.0 ], [ 0.0, 0.0 ]),
+		Vertex::new([ 0.0, 0.0, 1.0 ], [ 0.0, 0.0 ]),
+	];
+
+	Drawer::new(
+		graphics,
+		draw_state,
+		vertices,
+		gfx::Line,
+		shaders::vertex::LINE.clone(),
+		shaders::fragment::SIMPLE.clone(),
+	)
 }
