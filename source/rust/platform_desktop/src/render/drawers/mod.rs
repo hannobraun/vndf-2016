5f3090541d573769d1ebee49171000928b8082bc
Hanno Braun <mail@hannobraun.de>
2014-11-21 10:09:21 +0100

    Rename nav_disc_drawer module

--- source/rust/platform_desktop/src/render/drawers/mod.rs
+++ source/rust/platform_desktop/src/render/drawers/mod.rs
@@ -8,7 +8,7 @@ use render::Graphics;
 use self::base::BaseDrawer;
 use self::billboard::BillboardDrawer;
 use self::line::LineDrawer;
-use self::nav_disc_drawer::NavDiscDrawer;
+use self::nav_disc::NavDiscDrawer;
 use self::planet_drawer::PlanetDrawer;
 use self::projected_course::ProjectedCourseDrawer;
 
@@ -16,7 +16,7 @@ use self::projected_course::ProjectedCourseDrawer;
 pub use self::base::Base;
 pub use self::billboard::Billboard;
 pub use self::line::Line;
-pub use self::nav_disc_drawer::NavDisc;
+pub use self::nav_disc::NavDisc;
 pub use self::planet_drawer::Planet;
 pub use self::projected_course::ProjectedCourse;
 
@@ -24,7 +24,7 @@ pub use self::projected_course::ProjectedCourse;
 mod base;
 mod billboard;
 mod line;
-mod nav_disc_drawer;
+mod nav_disc;
 mod planet_drawer;
 mod projected_course;
 
