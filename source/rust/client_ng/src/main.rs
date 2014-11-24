640d56f49015b66568fbeb939b88b7e2b083c8c1
Hanno Braun <mail@hannobraun.de>
2014-11-24 18:32:36 +0100

    Improve formatting

--- source/rust/client_ng/src/main.rs
+++ source/rust/client_ng/src/main.rs
@@ -22,12 +22,12 @@ mod termios;
 
 fn main() {
 	let mut termios = ffi::termios {
-		c_iflag: 0,
-		c_oflag: 0,
-		c_cflag: 0,
-		c_lflag: 0,
-		c_line: 0,
-		c_cc: [0, ..NCCS],
+		c_iflag : 0,
+		c_oflag : 0,
+		c_cflag : 0,
+		c_lflag : 0,
+		c_line  : 0,
+		c_cc    : [0, ..NCCS],
 		c_ispeed: 0,
 		c_ospeed: 0,
 	};
