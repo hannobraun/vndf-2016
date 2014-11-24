290a07ff6400180d4fdc490ba2ac1f0cfb059a9d
Hanno Braun <mail@hannobraun.de>
2014-11-24 18:38:30 +0100

    Make use of termios wrapper

--- source/rust/client_ng/src/main.rs
+++ source/rust/client_ng/src/main.rs
@@ -4,15 +4,13 @@ extern crate libc;
 use std::io::timer::sleep;
 use std::time::Duration;
 
+use termios::Termios;
 use termios::ffi::{
-	mod,
 	ECHO,
 	FAILURE,
 	ICANON,
-	NCCS,
 	SUCCESS,
 	TCSANOW,
-	tcgetattr,
 	tcsetattr,
 };
 
@@ -21,25 +19,10 @@ mod termios;
 
 
 fn main() {
-	let mut termios = ffi::termios {
-		c_iflag : 0,
-		c_oflag : 0,
-		c_cflag : 0,
-		c_lflag : 0,
-		c_line  : 0,
-		c_cc    : [0, ..NCCS],
-		c_ispeed: 0,
-		c_ospeed: 0,
-	};
+	let mut termios = Termios::get(libc::STDIN_FILENO);
 
-	match unsafe { tcgetattr(libc::STDIN_FILENO, &mut termios) } {
-		FAILURE => panic!("Error getting term attributes"),
-		SUCCESS => (),
-		_       => unreachable!(),
-	}
-
-	termios.c_lflag &= !ICANON & !ECHO; // disable line buffering and echo
-	match unsafe { tcsetattr(libc::STDIN_FILENO, TCSANOW, &termios) } {
+	termios.termios.c_lflag &= !ICANON & !ECHO; // disable line buffering and echo
+	match unsafe { tcsetattr(libc::STDIN_FILENO, TCSANOW, &termios.termios) } {
 		FAILURE => panic!("Error setting term attributes"),
 		SUCCESS => (),
 		_       => unreachable!(),
