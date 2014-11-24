787a295aadafbee13b36239969f37d593cd2b2fd
Hanno Braun <mail@hannobraun.de>
2014-11-24 18:28:51 +0100

    Disable line buffering and echoing for stdin

--- source/rust/client_ng/src/main.rs
+++ source/rust/client_ng/src/main.rs
@@ -1,8 +1,49 @@
+extern crate libc;
+
+
 use std::io::timer::sleep;
 use std::time::Duration;
 
+use termios::{
+	ECHO,
+	FAILURE,
+	ICANON,
+	NCCS,
+	SUCCESS,
+	TCSANOW,
+	tcgetattr,
+	tcsetattr,
+};
+
+
+mod termios;
+
 
 fn main() {
+	let mut termios = termios::termios {
+		c_iflag: 0,
+		c_oflag: 0,
+		c_cflag: 0,
+		c_lflag: 0,
+		c_line: 0,
+		c_cc: [0, ..NCCS],
+		c_ispeed: 0,
+		c_ospeed: 0,
+	};
+
+	match unsafe { tcgetattr(libc::STDIN_FILENO, &mut termios) } {
+		FAILURE => panic!("Error getting term attributes"),
+		SUCCESS => (),
+		_       => unreachable!(),
+	}
+
+	termios.c_lflag &= !ICANON & !ECHO; // disable line buffering and echo
+	match unsafe { tcsetattr(libc::STDIN_FILENO, TCSANOW, &termios) } {
+		FAILURE => panic!("Error setting term attributes"),
+		SUCCESS => (),
+		_       => unreachable!(),
+	}
+
 	let mut i = 0u8;
 
 	print!("\n");
