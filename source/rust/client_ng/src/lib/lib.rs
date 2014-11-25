2bd6925e1ee58df7a2924d9ae85f3b4e0614c727
Hanno Braun <mail@hannobraun.de>
2014-11-25 10:11:46 +0100

    Implement decoding/encoding from/to JSON for Frame

--- source/rust/client_ng/src/lib/lib.rs
+++ source/rust/client_ng/src/lib/lib.rs
@@ -1,3 +1,23 @@
+extern crate serialize;
+
+
+use serialize::json::{
+	mod,
+	DecodeResult,
+};
+
+
+#[deriving(Decodable, Encodable)]
 pub struct Frame {
 	pub broadcasts: Vec<String>,
+}
+
+impl Frame {
+	pub fn from_json(json: &str) -> DecodeResult<Frame> {
+		json::decode(json)
+	}
+
+	pub fn to_json(&self) -> String {
+		json::encode(self)
+	}
 }
