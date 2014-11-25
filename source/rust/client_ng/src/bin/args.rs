f9028a7d17e327e03afa95f7f25cbe1189b2ff6b
Hanno Braun <mail@hannobraun.de>
2014-11-25 12:40:13 +0100

    Add support for server port argument

--- source/rust/client_ng/src/bin/args.rs
+++ source/rust/client_ng/src/bin/args.rs
@@ -1,25 +1,35 @@
 use getopts::{
 	getopts,
 	optflag,
+	optopt,
 };
+use std::io::net::ip::Port;
 
 
 pub struct Args {
 	pub headless: bool,
+	pub port    : Port,
 }
 
 impl Args {
 	pub fn parse(args: &[String]) -> Args {
 		let opts = &[
 			optflag("h", "headless", "enable headless mode"),
+			optopt("p", "server-port", "server port to connect to", "34481"),
 		];
 		let matches = match getopts(args.tail(), opts) {
 			Ok(matches) => matches,
 			Err(error)  => panic!("Error parsing arguments: {}", error),
 		};
 
+		let port = match matches.opt_str("p") {
+			Some(port) => from_str(port.as_slice()).unwrap(),
+			None       => 34481,
+		};
+
 		Args {
 			headless: matches.opt_present("h"),
+			port    : port,
 		}
 	}
 }
