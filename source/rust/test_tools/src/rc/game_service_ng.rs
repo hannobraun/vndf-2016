db43bbae4c9f16198bd975800d7d158c907f75cd
Hanno Braun <mail@hannobraun.de>
2014-11-25 12:10:41 +0100

    Implement rc for game_service_ng

--- source/rust/test_tools/src/rc/game_service_ng.rs
+++ source/rust/test_tools/src/rc/game_service_ng.rs
@@ -1,14 +1,35 @@
 use std::io::net::ip::Port;
 
+use acceptance::{
+	random_port,
+	Process,
+};
 
-pub struct GameService;
+
+pub struct GameService {
+	port    : Port,
+	_process: Process,
+}
 
 impl GameService {
 	pub fn start() -> GameService {
-		GameService
+		let port = random_port(40000, 50000);
+
+		let mut process = Process::start(
+			"vndf-game-service-ng",
+			&[
+				format!("{}", port).as_slice(),
+			]
+		);
+		process.read_stdout_line(); // Make sure it's ready
+
+		GameService {
+			port    : port,
+			_process: process,
+		}
 	}
 
 	pub fn port(&self) -> Port {
-		0
+		self.port
 	}
 }
