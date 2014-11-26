2ff1fc2ca8565a687db03e375c2fc373a4c993a6
Hanno Braun <mail@hannobraun.de>
2014-11-26 17:31:29 +0100

    Add Perception type

--- source/rust/protocol_ng/src/lib.rs
+++ source/rust/protocol_ng/src/lib.rs
@@ -22,3 +22,19 @@ impl Action {
 		json::encode(self)
 	}
 }
+
+
+#[deriving(Decodable, Encodable, Show)]
+pub struct Perception {
+	pub broadcasts: Vec<String>,
+}
+
+impl Perception {
+	pub fn from_json(json: &str) -> DecodeResult<Perception> {
+		json::decode(json)
+	}
+
+	pub fn to_json(&self) -> String {
+		json::encode(self)
+	}
+}
