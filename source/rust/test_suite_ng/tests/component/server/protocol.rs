a3812e1ca55da863b35ec479af1bcee68179cca7
Hanno Braun <mail@hannobraun.de>
2014-11-27 15:20:41 +0100

    Add test for action receipt confirmation
    It works thanks to stubbed out implementations.

---
+++ source/rust/test_suite_ng/tests/component/server/protocol.rs
@@ -0,0 +1,24 @@
+use protocol_ng::{
+	Action,
+	Step,
+};
+use test_tools_ng::{
+	GameService,
+	MockClient,
+};
+
+
+#[test]
+fn it_should_confirm_received_actions() {
+	let game_service = GameService::start();
+	let client       = MockClient::start(game_service.port());
+
+	let seq = 512;
+	client.send_action(Action {
+		seq  : seq,
+		steps: vec![Step::Login],
+	});
+
+	let perception = client.expect_perception().unwrap();
+	assert_eq!(seq, perception.last_action);
+}
--- source/rust/test_suite_ng/tests/main.rs
+++ source/rust/test_suite_ng/tests/main.rs
@@ -2,12 +2,18 @@
 
 
 extern crate client_ng;
+extern crate protocol_ng;
 extern crate test_tools_ng;
 
 
 mod unit {
 	mod client {
 		mod server;
+	}
+}
+mod component {
+	mod server {
+		mod protocol;
 	}
 }
 mod acceptance {
