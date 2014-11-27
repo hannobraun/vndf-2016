d4e138c4b1e8b8e21969b8ed12378916edad76bd
Hanno Braun <mail@hannobraun.de>
2014-11-27 15:06:39 +0100

    Add type Action
    Action will be the thing that's actually send in a UDP message.

--- source/rust/protocol_ng/src/lib.rs
+++ source/rust/protocol_ng/src/lib.rs
@@ -8,6 +8,22 @@ use serialize::json::{
 
 
 #[deriving(Decodable, Encodable, Show)]
+pub struct Action {
+	pub steps: Vec<Step>,
+}
+
+impl Action {
+	pub fn from_json(json: &str) -> DecodeResult<Action> {
+		json::decode(json)
+	}
+
+	pub fn to_json(&self) -> String {
+		json::encode(self)
+	}
+}
+
+
+#[deriving(Decodable, Encodable, Show)]
 pub enum Step {
 	Login,
 	Broadcast(String),
