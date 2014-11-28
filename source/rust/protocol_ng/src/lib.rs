4b5bb3529839a30ed9cd29ad449a49b83e5f1bf9
Hanno Braun <mail@hannobraun.de>
2014-11-28 10:10:27 +0100

    Derive PartialEq for protocol types

--- source/rust/protocol_ng/src/lib.rs
+++ source/rust/protocol_ng/src/lib.rs
@@ -7,7 +7,7 @@ use serialize::json::{
 };
 
 
-#[deriving(Clone, Decodable, Encodable, Show)]
+#[deriving(Clone, Decodable, Encodable, PartialEq, Show)]
 pub struct Action {
 	pub seq  : u64,
 	pub steps: Vec<Step>,
@@ -24,14 +24,14 @@ impl Action {
 }
 
 
-#[deriving(Clone, Decodable, Encodable, Show)]
+#[deriving(Clone, Decodable, Encodable, PartialEq, Show)]
 pub enum Step {
 	Login,
 	Broadcast(String),
 }
 
 
-#[deriving(Clone, Decodable, Encodable, Show)]
+#[deriving(Clone, Decodable, Encodable, PartialEq, Show)]
 pub struct Perception {
 	pub last_action: u64,
 	pub broadcasts : Vec<String>,
