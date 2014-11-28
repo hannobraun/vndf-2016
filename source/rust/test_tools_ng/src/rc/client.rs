9f4492b5d313c3579f5688e755f3775baeebc341
Hanno Braun <mail@hannobraun.de>
2014-11-28 17:05:23 +0100

    Add method for broadcast command to rc client

--- source/rust/test_tools_ng/src/rc/client.rs
+++ source/rust/test_tools_ng/src/rc/client.rs
@@ -29,6 +29,14 @@ impl Client {
 		self.process.write_stdin_line(command);
 	}
 
+	pub fn broadcast(&mut self, broadcast: &str) {
+		// TODO(83305336): This is just the command argument, what's missing is
+		//                 the command. Once we need more than one command, this
+		//                 should be something like "broadcast ...", instead of
+		//                 only "...".
+		self.command(broadcast);
+	}
+
 	pub fn frame(&mut self) -> Frame {
 		let line = self.process.read_stdout_line();
 		match Frame::from_json(line.as_slice()) {
