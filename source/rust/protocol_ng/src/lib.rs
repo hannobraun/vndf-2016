52f570ee2682e6c4ebbea2ec8dc9bf0911c9537a
Hanno Braun <mail@hannobraun.de>
2014-11-29 20:52:12 +0100

    Replace JSON with custom encoding for perceptions

--- source/rust/protocol_ng/src/lib.rs
+++ source/rust/protocol_ng/src/lib.rs
@@ -18,14 +18,18 @@ mod action;
 mod perception;
 
 
-pub struct Encoder;
+pub struct Encoder {
+	buffer: [u8, ..512],
+}
 
 impl Encoder {
 	pub fn new() -> Encoder {
-		Encoder
+		Encoder {
+			buffer: [0, ..512],
+		}
 	}
 
 	pub fn perception(&mut self, last_action: u64) -> PerceptionEnc {
-		PerceptionEnc::new(last_action)
+		PerceptionEnc::new(&mut self.buffer, last_action)
 	}
 }
--- source/rust/protocol_ng/src/perception.rs
+++ source/rust/protocol_ng/src/perception.rs
@@ -1,9 +1,9 @@
-use serialize::json;
 use std::io::{
-	BufWriter,
+	BufReader,
 	IoResult,
 };
-use std::str;
+
+use self::buf_writer::BufWriter;
 
 
 #[deriving(Clone, Decodable, Encodable, PartialEq, Show)]
@@ -14,50 +14,242 @@ pub struct Perception {
 
 impl Perception {
 	pub fn decode(buffer: &[u8]) -> Option<Perception> {
-		// TODO: Replace unwrap with proper error handling.
-		match json::decode(str::from_utf8(buffer).unwrap()) {
-			Ok(perception) => Some(perception),
-			Err(_)         => None,
+		// TODO: Properly handle all errors.
+		let mut reader = BufReader::new(buffer);
+
+		let message = match reader.read_to_string() {
+			Ok(message) =>
+				message,
+			Err(error) => {
+				print!("Error converting message to string: {}\n", error);
+				return None;
+			},
+		};
+
+		let mut lines: Vec<&str> = message.split('\n').collect();
+
+		let header = match lines.remove(0) {
+			Some(header) =>
+				header,
+			None => {
+				print!("Header line is missing\n");
+				return None;
+			},
+		};
+
+		let last_action = match from_str(header) {
+			Some(last_action) =>
+				last_action,
+			None => {
+				print!("Header is not a number\n");
+				return None;
+			},
+		};
+
+		let mut broadcasts = Vec::new();
+		for line in lines.iter() {
+			let mut splits: Vec<String> = line.splitn(1, ' ')
+				.map(|s| s.to_string())
+				.collect();
+
+			if splits.len() != 2 && splits[0].as_slice() != "UPDATE" {
+				continue;
+			}
+
+			let broadcast = match splits.pop() {
+				Some(broadcast) =>
+					broadcast,
+				None => {
+					print!("Invalid line, broadcast missing: {}\n", line);
+					return None;
+				},
+			};
+
+			broadcasts.push(broadcast);
 		}
+
+		Some(Perception {
+			last_action: last_action,
+			broadcasts : broadcasts,
+		})
 	}
 }
 
 
-pub struct PerceptionEnc {
-	last_action: u64,
-	broadcasts : Vec<String>,
+pub struct PerceptionEnc<'r> {
+	writer: BufWriter<'r>,
 }
 
-impl PerceptionEnc {
-	pub fn new(last_action: u64) -> PerceptionEnc {
+impl<'r> PerceptionEnc<'r> {
+	pub fn new(buffer: &mut [u8], last_action: u64) -> PerceptionEnc {
+		let mut writer = BufWriter::new(buffer);
+
+		match write!(&mut writer, "{}\n", last_action) {
+			Ok(()) =>
+				(),
+			Err(error) =>
+				panic!("Error writing perception header: {}", error),
+		}
+
 		PerceptionEnc {
-			last_action: last_action,
-			broadcasts : Vec::new(),
+			writer: writer,
 		}
 	}
 
 	pub fn update(&mut self, broadcast: &str) -> bool {
-		self.broadcasts.push(broadcast.to_string());
+		let mut update = [0, ..512];
+
+		let len = {
+			let mut writer = BufWriter::new(&mut update);
+			match write!(&mut writer, "UPDATE {}\n", broadcast) {
+				Ok(())  => (),
+				Err(_)  => return false,
+			}
+
+			writer.tell().unwrap_or_else(|_|
+				panic!(
+					"I/O operation on BufWriter that cannot possibly fail \
+					still managed to fail somehow."
+				)
+			)
+		};
+		let update = update[.. len as uint];
+
+		match self.writer.write(update) {
+			Ok(()) => (),
+			Err(_) => return false,
+		}
+
 		true
 	}
 
 	pub fn encode(self, buffer: &mut [u8]) -> IoResult<&[u8]> {
 		let len = {
-			let perception = Perception {
-				last_action: self.last_action,
-				broadcasts : self.broadcasts,
-			};
-			let string = json::encode(&perception);
-			let bytes  = string.as_bytes();
-
 			let mut writer = BufWriter::new(buffer);
-
-			match writer.write(bytes) {
-				Ok(())     => bytes.len(),
+			match writer.write(self.writer.into_slice()) {
+				Ok(())     => (),
 				Err(error) => return Err(error),
-			}
+			};
+
+			writer.tell().unwrap_or_else(|_|
+				panic!(
+					"I/O operation on BufWriter that cannot possibly fail \
+					still managed to fail somehow."
+				)
+			)
 		};
 
-		Ok(buffer[.. len])
+		Ok(buffer[.. len as uint])
+	}
+}
+
+
+mod buf_writer {
+	// This is code from the Rust standard library. I copied itbecause I needed
+	// the BufWriter::into_slice method that I implemented here.
+
+	// TODO: Send PR to Rust project.
+
+
+	use std::io::{
+		mod,
+		IoError,
+		IoResult,
+		SeekStyle,
+	};
+	use std::slice;
+
+
+	fn combine(seek: SeekStyle, cur: uint, end: uint, offset: i64) -> IoResult<u64> {
+		// compute offset as signed and clamp to prevent overflow
+		let pos = match seek {
+			io::SeekSet => 0,
+			io::SeekEnd => end,
+			io::SeekCur => cur,
+		} as i64;
+
+		if offset + pos < 0 {
+			Err(IoError {
+				kind: io::InvalidInput,
+				desc: "invalid seek to a negative offset",
+				detail: None
+			})
+		} else {
+			Ok((offset + pos) as u64)
+		}
+	}
+
+
+	/// Writes to a fixed-size byte slice
+	///
+	/// If a write will not fit in the buffer, it returns an error and does not
+	/// write any data.
+	///
+	/// # Example
+	///
+	/// ```rust
+	/// # #![allow(unused_must_use)]
+	/// use std::io::BufWriter;
+	///
+	/// let mut buf = [0, ..4];
+	/// {
+	///     let mut w = BufWriter::new(&mut buf);
+	///     w.write(&[0, 1, 2]);
+	/// }
+	/// assert!(buf == [0, 1, 2, 0]);
+	/// ```
+	pub struct BufWriter<'a> {
+		buf: &'a mut [u8],
+		pos: uint
+	}
+
+	impl<'a> BufWriter<'a> {
+		/// Creates a new `BufWriter` which will wrap the specified buffer. The
+		/// writer initially starts at position 0.
+		#[inline]
+		pub fn new<'a>(buf: &'a mut [u8]) -> BufWriter<'a> {
+			BufWriter {
+				buf: buf,
+				pos: 0
+			}
+		}
+
+		/// Consumes the `BufWriter`, returning the slice it was originally
+		/// created with.
+		#[inline]
+		pub fn into_slice(self) -> &'a mut [u8] {
+			self.buf
+		}
+	}
+
+	impl<'a> Writer for BufWriter<'a> {
+		#[inline]
+		fn write(&mut self, buf: &[u8]) -> IoResult<()> {
+			// return an error if the entire write does not fit in the buffer
+			let cap = if self.pos >= self.buf.len() { 0 } else { self.buf.len() - self.pos };
+			if buf.len() > cap {
+				return Err(IoError {
+					kind: io::OtherIoError,
+					desc: "Trying to write past end of buffer",
+					detail: None
+				})
+			}
+
+			slice::bytes::copy_memory(self.buf[mut self.pos..], buf);
+			self.pos += buf.len();
+			Ok(())
+		}
+	}
+
+	impl<'a> Seek for BufWriter<'a> {
+		#[inline]
+		fn tell(&self) -> IoResult<u64> { Ok(self.pos as u64) }
+
+		#[inline]
+		fn seek(&mut self, pos: i64, style: SeekStyle) -> IoResult<()> {
+			let new = try!(combine(style, self.pos, self.buf.len(), pos));
+			self.pos = new as uint;
+			Ok(())
+		}
 	}
 }
