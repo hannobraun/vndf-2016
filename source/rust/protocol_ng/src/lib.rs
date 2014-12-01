eda82c2f2fcea726447fb51aaf47ce245512201e
Hanno Braun <mail@hannobraun.de>
2014-12-01 20:57:36 +0100

    Inline Step into parent module

--- source/rust/protocol_ng/src/lib.rs
+++ source/rust/protocol_ng/src/lib.rs
@@ -6,9 +6,45 @@ extern crate serialize;
 extern crate acpe;
 
 
-pub use action::Step;
+use serialize::Encodable;
+use serialize::json;
+use std::io::IoResult;
+
+use acpe::protocol::{
+	MessagePart,
+};
+
+
 pub use perception::Percept;
 
 
-mod action;
 mod perception;
+
+
+#[deriving(Clone, Decodable, Encodable, PartialEq, Show)]
+pub enum Step {
+	Login,
+	Broadcast(String),
+}
+
+impl MessagePart for Step {
+	fn write<W: Writer>(&self, writer: &mut W) -> IoResult<()> {
+		try!(self.encode(&mut json::Encoder::new(writer)));
+		try!(writer.write_char('\n'));
+
+		Ok(())
+	}
+
+	fn read(line: &str) -> Result<Step, String> {
+		match json::decode(line) {
+			Ok(part) =>
+				Ok(part),
+			Err(error) =>
+				Err(format!(
+					"Error decoding step. \
+					Error: {}; Step: {}",
+					error, line,
+				)),
+		}
+	}
+}
