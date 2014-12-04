51dbd5e4a1ebea5f50bea3fc8051e6bdaf4ebdb8
Hanno Braun <mail@hannobraun.de>
2014-12-04 12:46:13 +0100

    Make test more precise

--- source/rust/test_suite_ng/tests/acceptance/basic.rs
+++ source/rust/test_suite_ng/tests/acceptance/basic.rs
@@ -26,8 +26,8 @@ fn it_should_send_broadcasts_to_all_clients() {
 	client_1.broadcast(message_1.as_slice());
 	client_2.broadcast(message_2.as_slice());
 
-	let frame_1 = client_1.wait_until(|frame| frame.broadcasts.len() >= 2);
-	let frame_2 = client_2.wait_until(|frame| frame.broadcasts.len() >= 2);
+	let frame_1 = client_1.wait_until(|frame| frame.broadcasts.len() == 2);
+	let frame_2 = client_2.wait_until(|frame| frame.broadcasts.len() == 2);
 	assert!(contains(&frame_1, (&frame_1.self_id, &message_1)));
 	assert!(contains(&frame_1, (&frame_2.self_id, &message_2)));
 	assert!(contains(&frame_2, (&frame_1.self_id, &message_1)));
