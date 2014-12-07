15dc369c1c09cc582f0fa090c67333025b45533b
Hanno Braun <mail@hannobraun.de>
2014-12-07 15:20:54 +0100

    Write to buffer before rendering to screen

--- source/rust/client_ng/src/bin/output/screen.rs
+++ source/rust/client_ng/src/bin/output/screen.rs
@@ -1,53 +1,108 @@
 use libc;
 use std::io::{
 	stdout,
+	IoError,
+	IoErrorKind,
 	IoResult,
 	LineBufferedWriter,
 };
 use std::io::stdio::StdWriter;
+use std::str::from_utf8;
 
 use termios::Termios;
 
 
 pub struct Screen {
 	stdout: LineBufferedWriter<StdWriter>,
+	buffer: ScreenBuffer,
 }
 
 impl Screen {
-	pub fn new(_width: u16, _height: u16) -> Screen {
+	pub fn new(width: u16, height: u16) -> Screen {
 		let mut termios = Termios::get(libc::STDIN_FILENO);
 		termios.echo(false);
 		termios.canonical_input(false);
 		termios.set(libc::STDIN_FILENO);
 
+		let width  = width  as uint;
+		let height = height as uint;
+
+		let buffer = Vec::from_fn(height, |_| Vec::from_elem(width, ' '));
+
 		Screen {
 			stdout: stdout(),
+			buffer: buffer,
 		}
 	}
 
 	pub fn clear(&mut self) -> IoResult<()> {
-		try!(write!(&mut self.stdout, "\x1b[2J")); // clear screen
+		for line in self.buffer.iter_mut() {
+			for c in line.iter_mut() {
+				*c = ' ';
+			}
+		}
+
 		Ok(())
 	}
 
 	/// Origin is in upper-left corner.
-	pub fn buffer(
-		&mut self,
-		x: u16,
-		y: u16,
-	) -> &mut LineBufferedWriter<StdWriter> {
-		// TODO: Improve error handling
-		(write!(
-			&mut self.stdout,
-			"\x1b[{};{}H",
-			y + 1, x + 1
-		)).unwrap(); // set cursor
-
-		&mut self.stdout
+	pub fn buffer(&mut self, x: u16, y: u16) -> BufferWriter {
+		BufferWriter {
+			buffer: &mut self.buffer,
+			x     : x,
+			y     : y,
+		}
 	}
 
 	pub fn submit(&mut self) -> IoResult<()> {
+		try!(write!(&mut self.stdout, "\x1b[2J")); // clear screen
+		try!(write!(&mut self.stdout, "\x1b[H")); // reset cursor
+
+		for line in self.buffer.iter() {
+			for &c in line.iter() {
+				try!(self.stdout.write_char(c));
+			}
+			try!(self.stdout.write_char('\n'));
+		}
+
 		try!(self.stdout.flush());
+		Ok(())
+	}
+}
+
+
+type ScreenBuffer = Vec<Vec<char>>;
+
+
+struct BufferWriter<'r> {
+	buffer: &'r mut ScreenBuffer,
+	x     : u16,
+	y     : u16,
+}
+
+impl<'r> Writer for BufferWriter<'r> {
+	fn write(&mut self, buf: &[u8]) -> IoResult<()> {
+		let s = match from_utf8(buf) {
+			Some(s) =>
+				s,
+			None =>
+				return Err(IoError {
+					kind  : IoErrorKind::OtherIoError,
+					desc  : "Tried to write invalid UTF-8",
+					detail: None,
+				})
+
+		};
+
+		for c in s.chars() {
+			// TODO: Check bounds
+			let x = self.x as uint;
+			let y = self.y as uint;
+			self.buffer[y][x] = c;
+
+			self.x += 1;
+		}
+
 		Ok(())
 	}
 }
