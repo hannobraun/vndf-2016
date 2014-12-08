99453d2de76f262d7bbf8c2ec1805a5629dca593
Hanno Braun <mail@hannobraun.de>
2014-12-08 13:11:07 +0100

    Add a TODO

--- source/rust/test_suite_ng/tests/unit/acpe/socket.rs
+++ source/rust/test_suite_ng/tests/unit/acpe/socket.rs
@@ -3,6 +3,10 @@ use acpe::network::Socket;
 
 #[test]
 fn it_should_not_block_forever_on_receive() {
+	// TODO: Use random_port using acceptance::random_port. This is complicated
+	//       by the fact that the acceptance name is shadowed by the module for
+	//       acceptance tests. Maybe it's finally time to think about a real
+	//       name for acceptance-rs?
 	Socket::new(34481);
 
 	// Socket is dropped immediately, but its task won't notice if it blocks on
