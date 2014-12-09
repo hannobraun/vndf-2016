b855c29c951d3762f841ebd2af5369500f78dc75
Hanno Braun <mail@hannobraun.de>
2014-12-09 13:38:07 +0100

    Remove finished TODO

--- source/rust/client_ng/src/bin/output/screen.rs
+++ source/rust/client_ng/src/bin/output/screen.rs
@@ -42,7 +42,6 @@ pub struct Screen {
 }
 
 // TODO: Add color support
-// TODO: Add support for bold text
 impl Screen {
 	pub fn new(width: u16, height: u16) -> IoResult<Screen> {
 		let mut termios = Termios::get(libc::STDIN_FILENO);
