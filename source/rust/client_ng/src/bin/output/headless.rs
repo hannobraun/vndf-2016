f257cbc744e61222ffa5de07af35ca39caf6989f
Hanno Braun <mail@hannobraun.de>
2014-12-09 19:13:37 +0100

    Move HeadlessRenderer to dedicated module

---
+++ source/rust/client_ng/src/bin/output/headless.rs
@@ -0,0 +1,21 @@
+use std::io::IoResult;
+
+use client::output::Frame;
+
+use super::Render;
+
+
+pub struct HeadlessRenderer;
+
+impl HeadlessRenderer {
+	pub fn new() -> IoResult<HeadlessRenderer> {
+		Ok(HeadlessRenderer)
+	}
+}
+
+impl Render for HeadlessRenderer {
+	fn render(&mut self, frame: &Frame) -> IoResult<()> {
+		print!("{}\n", frame.to_json());
+		Ok(())
+	}
+}
--- source/rust/client_ng/src/bin/output/mod.rs
+++ source/rust/client_ng/src/bin/output/mod.rs
@@ -3,30 +3,16 @@ use std::io::IoResult;
 use client::output::Frame;
 
 
+pub use self::headless::HeadlessRenderer;
 pub use self::renderer::Renderer;
 
 
 mod color;
+mod headless;
 mod renderer;
 mod screen;
 
 
 pub trait Render {
 	fn render(&mut self, frame: &Frame) -> IoResult<()>;
-}
-
-
-pub struct HeadlessRenderer;
-
-impl HeadlessRenderer {
-	pub fn new() -> IoResult<HeadlessRenderer> {
-		Ok(HeadlessRenderer)
-	}
-}
-
-impl Render for HeadlessRenderer {
-	fn render(&mut self, frame: &Frame) -> IoResult<()> {
-		print!("{}\n", frame.to_json());
-		Ok(())
-	}
 }
