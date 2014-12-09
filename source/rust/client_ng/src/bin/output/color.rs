1d84a05830e216b4134abda5cd2ba0ad8b6b9ce4
Hanno Braun <mail@hannobraun.de>
2014-12-09 13:35:59 +0100

    Add method that returns color code

--- source/rust/client_ng/src/bin/output/color.rs
+++ source/rust/client_ng/src/bin/output/color.rs
@@ -14,4 +14,17 @@ impl Color {
 	pub fn default() -> Color {
 		Color::White
 	}
+
+	pub fn foreground_code(&self) -> u16 {
+		match *self {
+			Color::Black   => 30,
+			Color::Red     => 31,
+			Color::Green   => 32,
+			Color::Yellow  => 33,
+			Color::Blue    => 34,
+			Color::Magenta => 35,
+			Color::Cyan    => 36,
+			Color::White   => 37,
+		}
+	}
 }
