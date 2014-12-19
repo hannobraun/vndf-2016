45917c69c244315fea6be1d9f788186690190132
Hanno Braun <mail@hannobraun.de>
2014-12-19 14:23:22 +0100

    Refactor

--- source/rust/client_ng/src/bin/main.rs
+++ source/rust/client_ng/src/bin/main.rs
@@ -81,7 +81,9 @@ fn run<R: Render>(args: Args, mut renderer: R) {
 	action_assembler.add_step(Step::Login);
 
 	loop {
-		for result in input_reader.input().commands.into_iter() {
+		let input = input_reader.input();
+
+		for result in input.commands.into_iter() {
 			match result {
 				Ok(command) => {
 					let mut reset_status = true;
