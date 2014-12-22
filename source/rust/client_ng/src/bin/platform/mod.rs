a30d73e51954197784c9e5bce545886e20a4cae0
Hanno Braun <mail@hannobraun.de>
2014-12-22 16:09:25 +0100

    Define PlatformIo trait

--- source/rust/client_ng/src/bin/platform/mod.rs
+++ source/rust/client_ng/src/bin/platform/mod.rs
@@ -1,2 +1,10 @@
+use self::input::ReadInput;
+use self::render::Render;
+
+
 pub mod input;
 pub mod render;
+
+
+// TODO: Merge ReadInput and Render into PlatformIo
+pub trait PlatformIo: ReadInput + Render {}
