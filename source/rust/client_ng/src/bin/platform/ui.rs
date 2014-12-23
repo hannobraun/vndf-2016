dd253f2af4c460cd94724f44da2b9bd9d1b94af0
Hanno Braun <mail@hannobraun.de>
2014-12-23 14:10:44 +0100

    Return Input from Ui::process_input

--- source/rust/client_ng/src/bin/platform/ui.rs
+++ source/rust/client_ng/src/bin/platform/ui.rs
@@ -1,3 +1,6 @@
+use client::platform::Input;
+
+
 pub struct Ui {
 	pub input_active: bool,
 	pub input_text  : String,
@@ -11,7 +14,7 @@ impl Ui {
 		}
 	}
 
-	pub fn process_input(&mut self, chars: &[char]) {
+	pub fn process_input(&mut self, chars: &[char]) -> Input {
 		for &c in chars.iter() {
 			if c == '\n' {
 				self.input_active = !self.input_active;
@@ -27,5 +30,15 @@ impl Ui {
 				self.input_text.push(c);
 			}
 		}
+
+		let mut input = Input::new();
+		input.broadcast = if !self.input_active {
+			Some(self.input_text.clone())
+		}
+		else {
+			None
+		};
+
+		input
 	}
 }
