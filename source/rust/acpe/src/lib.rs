f1867e02ffdf7040631c30ce966b323ccc0e27c8
Hanno Braun <mail@hannobraun.de>
2014-12-30 11:23:57 +0100

    Add constants for directives
    There are going to be different kinds of directives in messages. Those
    are the definitions for them.

--- source/rust/acpe/src/lib.rs
+++ source/rust/acpe/src/lib.rs
@@ -4,7 +4,11 @@
 extern crate "rustc-serialize" as rustc_serialize;
 
 
-pub use root::MAX_PACKET_SIZE;
+pub use root::{
+	DESTROY,
+	MAX_PACKET_SIZE,
+	UPDATE,
+};
 
 
 pub mod network;
@@ -13,4 +17,7 @@ pub mod protocol;
 
 mod root {
 	pub const MAX_PACKET_SIZE: uint = 512;
+
+	pub const DESTROY: &'static str = "DESTROY";
+	pub const UPDATE : &'static str = "UPDATE";
 }
