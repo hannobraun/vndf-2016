be2675552a73743432c05493c2a412c35f076efb
Hanno Braun <mail@hannobraun.de>
2015-01-02 11:57:26 +0100

    Derive Default for PerceptionHeader

--- source/rust/acpe/src/protocol/perception.rs
+++ source/rust/acpe/src/protocol/perception.rs
@@ -7,7 +7,7 @@ use super::{
 pub type Perception<Id, Entity> = Message<PerceptionHeader, Id, Entity>;
 
 
-#[deriving(Clone, PartialEq, RustcDecodable, RustcEncodable, Show)]
+#[deriving(Clone, Default, PartialEq, RustcDecodable, RustcEncodable, Show)]
 pub struct PerceptionHeader {
 	pub confirm_action: Seq,
 	pub self_id       : Option<String>,
