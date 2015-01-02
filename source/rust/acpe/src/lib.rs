fc12df58acb56803493602eb8489620a229d27d1
Hanno Braun <mail@hannobraun.de>
2015-01-02 21:28:13 +0100

    Remove unused code

--- source/rust/acpe/src/lib.rs
+++ source/rust/acpe/src/lib.rs
@@ -6,7 +6,6 @@ extern crate "rustc-serialize" as rustc_serialize;
 
 pub use root::{
 	DESTROY,
-	HEADER,
 	MAX_PACKET_SIZE,
 	UPDATE,
 };
@@ -19,7 +18,6 @@ pub mod protocol;
 mod root {
 	pub const MAX_PACKET_SIZE: uint = 512;
 
-	pub const HEADER : &'static str = "HEADER";
 	pub const DESTROY: &'static str = "DESTROY";
 	pub const UPDATE : &'static str = "UPDATE";
 }
