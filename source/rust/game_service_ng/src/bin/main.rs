2161b1a8e0829764125a0cbfcdfe84187eae7494
Hanno Braun <mail@hannobraun.de>
2015-01-03 16:51:04 +0100

    Move sending code into sender

--- source/rust/game_service_ng/src/bin/main.rs
+++ source/rust/game_service_ng/src/bin/main.rs
@@ -8,22 +8,14 @@ extern crate game_service;
 
 
 use std::collections::HashMap;
-use std::io::net::ip::SocketAddr;
 use std::io::timer::sleep;
 use std::os;
 use std::time::Duration;
 
-use acpe::protocol::{
-	Encoder,
-	PerceptionHeader,
-};
 use time::precise_time_s;
 
 use args::Args;
-use common::protocol::{
-	Broadcast,
-	Percept,
-};
+use common::protocol::Broadcast;
 use game_service::Socket;
 use network::{
 	Receiver,
@@ -75,61 +67,8 @@ fn main() {
 			)
 			.collect();
 
-		for (&address, client) in clients.iter() {
-			let header = PerceptionHeader {
-				confirm_action: client.last_action,
-				self_id       : Some(client.id.clone()),
-			};
-			// TODO(85373160): It's not necessary to keep resending all the
-			//                 broadcasts every frame. The client should confirm
-			//                 the last sent perception and the server should
-			//                 only send what has changed. This requires a list
-			//                 of destroyed entities in Perception.
-			let mut broadcasts = broadcasts.clone();
-
-			let mut needs_to_send_perception = true;
-			while needs_to_send_perception {
-				send_perception(
-					&mut sender.encoder,
-					&header,
-					&mut broadcasts,
-					&mut socket,
-					address,
-				);
-
-				needs_to_send_perception = broadcasts.len() > 0;
-			}
-		}
+		sender.send(&mut socket, &mut clients, &broadcasts);
 
 		sleep(Duration::milliseconds(20));
 	}
-}
-
-
-fn send_perception(
-	encoder    : &mut Encoder,
-	header     : &PerceptionHeader,
-	broadcasts : &mut Vec<Broadcast>,
-	socket     : &mut Socket,
-	address    : SocketAddr,
-) {
-	let mut perception = encoder.message(header);
-	loop {
-		let broadcast = match broadcasts.pop() {
-			Some(broadcast) => broadcast,
-			None            => break,
-		};
-
-		let could_add = perception.update(
-			&broadcast.sender,
-			&Percept::Broadcast(broadcast.clone())
-		);
-		if !could_add {
-			broadcasts.push(broadcast);
-			break;
-		}
-	}
-
-	let message = perception.encode();
-	socket.send(message, address);
 }
--- source/rust/game_service_ng/src/bin/network/sender.rs
+++ source/rust/game_service_ng/src/bin/network/sender.rs
@@ -1,4 +1,17 @@
-use acpe::protocol::Encoder;
+use std::io::net::ip::SocketAddr;
+
+use acpe::protocol::{
+	Encoder,
+	PerceptionHeader,
+};
+
+use common::protocol::{
+	Broadcast,
+	Percept,
+};
+use game_service::Socket;
+
+use super::Clients;
 
 
 pub struct Sender {
@@ -11,4 +24,66 @@ impl Sender {
 			encoder: Encoder::new(),
 		}
 	}
+
+	pub fn send(
+		&mut self,
+		socket    : &mut Socket,
+		clients   : &mut Clients,
+		broadcasts: &Vec<Broadcast>,
+	) {
+		for (&address, client) in clients.iter() {
+			let header = PerceptionHeader {
+				confirm_action: client.last_action,
+				self_id       : Some(client.id.clone()),
+			};
+			// TODO(85373160): It's not necessary to keep resending all the
+			//                 broadcasts every frame. The client should confirm
+			//                 the last sent perception and the server should
+			//                 only send what has changed. This requires a list
+			//                 of destroyed entities in Perception.
+			let mut broadcasts = broadcasts.clone();
+
+			let mut needs_to_send_perception = true;
+			while needs_to_send_perception {
+				send_perception(
+					&mut self.encoder,
+					&header,
+					&mut broadcasts,
+					socket,
+					address,
+				);
+
+				needs_to_send_perception = broadcasts.len() > 0;
+			}
+		}
+	}
+}
+
+
+fn send_perception(
+	encoder    : &mut Encoder,
+	header     : &PerceptionHeader,
+	broadcasts : &mut Vec<Broadcast>,
+	socket     : &mut Socket,
+	address    : SocketAddr,
+) {
+	let mut perception = encoder.message(header);
+	loop {
+		let broadcast = match broadcasts.pop() {
+			Some(broadcast) => broadcast,
+			None            => break,
+		};
+
+		let could_add = perception.update(
+			&broadcast.sender,
+			&Percept::Broadcast(broadcast.clone())
+		);
+		if !could_add {
+			broadcasts.push(broadcast);
+			break;
+		}
+	}
+
+	let message = perception.encode();
+	socket.send(message, address);
 }
