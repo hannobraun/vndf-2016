2a45ae0247a145485538b988b5f1251dc8ea8dfd
Hanno Braun <mail@hannobraun.de>
2015-01-03 16:21:55 +0100

    Move receiving code to receiver module

--- source/rust/game_service_ng/src/bin/main.rs
+++ source/rust/game_service_ng/src/bin/main.rs
@@ -11,7 +11,6 @@ use std::collections::HashMap;
 use std::io::net::ip::SocketAddr;
 use std::io::timer::sleep;
 use std::os;
-use std::rand::random;
 use std::time::Duration;
 
 use acpe::protocol::{
@@ -24,13 +23,9 @@ use args::Args;
 use common::protocol::{
 	Broadcast,
 	Percept,
-	Step,
 };
 use game_service::Socket;
-use network::{
-	Client,
-	Receiver,
-};
+use network::Receiver;
 
 
 mod args;
@@ -49,58 +44,7 @@ fn main() {
 	print!("Listening on port {}\n", args.port);
 
 	loop {
-		socket.receive(&mut receiver.received);
-
-		for result in receiver.received.drain() {
-			match result {
-				Ok((action, address)) => {
-					for (_, step) in action.update.into_iter() {
-						match step {
-							Step::Login => {
-								clients.insert(address, Client {
-									id           : generate_id(),
-									last_action  : action.header.id,
-									last_active_s: precise_time_s(),
-									broadcast    : None,
-								});
-							},
-							Step::Broadcast(broadcast) => {
-								match clients.get_mut(&address) {
-									Some(client) =>
-										client.broadcast = Some(broadcast),
-									None =>
-										continue, // invalid, ignore
-								}
-							},
-							Step::StopBroadcast => {
-								match clients.get_mut(&address) {
-									Some(client) =>
-										client.broadcast = None,
-									None =>
-										continue, // invalid, ignore
-								}
-							},
-						}
-					}
-
-					match clients.get_mut(&address) {
-						Some(client) => {
-							client.last_action   = action.header.id;
-							client.last_active_s = precise_time_s();
-						},
-						None =>
-							continue, // invalid, ignore
-					}
-				},
-				Err((error, address)) => {
-					print!(
-						"Error receiving message from {}: {}\n",
-						address, error
-					);
-					clients.remove(&address);
-				},
-			}
-		}
+		receiver.receive(&mut socket, &mut clients);
 
 		let now_s = precise_time_s();
 		clients = clients
@@ -158,39 +102,6 @@ fn main() {
 	}
 }
 
-
-// TODO: The generated id should be guaranteed to be unique.
-fn generate_id() -> String {
-	fn random_char(min: char, max: char) -> char {
-		let min = min as u8;
-		let max = max as u8;
-
-		((random::<u8>() % (max + 1 - min)) + min) as char
-	}
-	fn random_letter() -> char {
-		random_char('A', 'Z')
-	}
-	fn random_letter_or_number() -> char {
-		if random() {
-			random_letter()
-		}
-		else {
-			random_char('0', '9')
-		}
-	}
-
-	let mut id = String::new();
-
-	for _ in range(0u8, 3) {
-		id.push(random_letter());
-	}
-	id.push('-');
-	for _ in range(0u8, 5) {
-		id.push(random_letter_or_number());
-	}
-
-	id
-}
 
 fn send_perception(
 	encoder    : &mut Encoder,
--- source/rust/game_service_ng/src/bin/network/receiver.rs
+++ source/rust/game_service_ng/src/bin/network/receiver.rs
@@ -1,4 +1,17 @@
-use game_service::ReceiveResult;
+use std::rand::random;
+
+use time::precise_time_s;
+
+use common::protocol::Step;
+use game_service::{
+	ReceiveResult,
+	Socket,
+};
+
+use super::{
+	Client,
+	Clients,
+};
 
 
 pub struct Receiver {
@@ -11,4 +24,93 @@ impl Receiver {
 			received: Vec::new(),
 		}
 	}
+
+	pub fn receive(&mut self, socket: &mut Socket, clients: &mut Clients) {
+		socket.receive(&mut self.received);
+
+		for result in self.received.drain() {
+			match result {
+				Ok((action, address)) => {
+					for (_, step) in action.update.into_iter() {
+						match step {
+							Step::Login => {
+								clients.insert(address, Client {
+									id           : generate_id(),
+									last_action  : action.header.id,
+									last_active_s: precise_time_s(),
+									broadcast    : None,
+								});
+							},
+							Step::Broadcast(broadcast) => {
+								match clients.get_mut(&address) {
+									Some(client) =>
+										client.broadcast = Some(broadcast),
+									None =>
+										continue, // invalid, ignore
+								}
+							},
+							Step::StopBroadcast => {
+								match clients.get_mut(&address) {
+									Some(client) =>
+										client.broadcast = None,
+									None =>
+										continue, // invalid, ignore
+								}
+							},
+						}
+					}
+
+					match clients.get_mut(&address) {
+						Some(client) => {
+							client.last_action   = action.header.id;
+							client.last_active_s = precise_time_s();
+						},
+						None =>
+							continue, // invalid, ignore
+					}
+				},
+				Err((error, address)) => {
+					print!(
+						"Error receiving message from {}: {}\n",
+						address, error
+					);
+					clients.remove(&address);
+				},
+			}
+		}
+	}
+}
+
+
+// TODO: The generated id should be guaranteed to be unique.
+fn generate_id() -> String {
+	fn random_char(min: char, max: char) -> char {
+		let min = min as u8;
+		let max = max as u8;
+
+		((random::<u8>() % (max + 1 - min)) + min) as char
+	}
+	fn random_letter() -> char {
+		random_char('A', 'Z')
+	}
+	fn random_letter_or_number() -> char {
+		if random() {
+			random_letter()
+		}
+		else {
+			random_char('0', '9')
+		}
+	}
+
+	let mut id = String::new();
+
+	for _ in range(0u8, 3) {
+		id.push(random_letter());
+	}
+	id.push('-');
+	for _ in range(0u8, 5) {
+		id.push(random_letter_or_number());
+	}
+
+	id
 }
