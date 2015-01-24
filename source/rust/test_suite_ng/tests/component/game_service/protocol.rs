89e3effbc2dd8f13ffeb5f56299edb578b28e07a
Hanno Braun <mail@hannobraun.de>
2015-01-24 19:24:20 +0100

    Remove some tests
    Those will no longer be valid after the networking revamp and are
    getting in the way during the rewrite.

--- source/rust/test_suite_ng/tests/component/game_service/protocol.rs
+++ source/rust/test_suite_ng/tests/component/game_service/protocol.rs
@@ -1,27 +1,9 @@
-use std::collections::HashSet;
-
-use acpe::MAX_PACKET_SIZE;
-use time::precise_time_s;
-
 use common::game::Broadcast;
 use common::protocol::Percept;
 use test_suite::{
 	GameService,
 	MockClient,
 };
-
-
-#[test]
-fn it_should_confirm_received_actions() {
-	let     game_service = GameService::start();
-	let mut client       = MockClient::start(game_service.port());
-
-	let seq = 512;
-	client.login(seq);
-
-	let perception = client.expect_perception().unwrap();
-	assert_eq!(seq, perception.header.confirm_action);
-}
 
 
 #[test]
@@ -52,57 +34,6 @@ fn it_should_disconnect_clients_sending_invalid_data() {
 }
 
 #[test]
-fn it_should_distribute_large_payloads_over_multiple_packets() {
-	let     game_service = GameService::start();
-	let mut client       = MockClient::start(game_service.port());
-
-	client.login(0);
-
-	// Create enough clients to overflow the maximum packet size.
-	let     broadcast_text = "Broadcast from client";
-	let mut other_clients  = Vec::new();
-	let mut broadcasts     = HashSet::new();
-	for i in range(0, MAX_PACKET_SIZE / broadcast_text.len() + 1) {
-		let mut client    = MockClient::start(game_service.port());
-		let     broadcast = format!("{} {}", broadcast_text, i);
-
-		client.login(0);
-		client.broadcast(1, broadcast.clone());
-
-		other_clients.push(client);
-		broadcasts.insert(broadcast);
-	}
-
-	// Receive perceptions until all broadcasts have been seen.
-	let mut perceptions = Vec::new();
-	let     start_s     = precise_time_s();
-	while broadcasts.len() > 0 {
-		if precise_time_s() - start_s > 0.5 {
-			panic!("Not all broadcasts arrived.");
-		}
-
-		match client.expect_perception() {
-			Some(perception) => {
-				for &(_, ref percept) in perception.update_items() {
-					match *percept {
-						Percept::Broadcast(ref broadcast) => {
-							broadcasts.remove(&broadcast.message);
-						},
-					}
-				}
-				perceptions.push(perception);
-			},
-
-			None => (),
-		}
-	}
-
-	for perception in perceptions.into_iter() {
-		assert!(perception.encode().len() <= MAX_PACKET_SIZE);
-	}
-}
-
-#[test]
 fn it_should_ignore_clients_that_havent_logged_in() {
 	let     game_service = GameService::start();
 	let mut client_1     = MockClient::start(game_service.port());
@@ -113,38 +44,6 @@ fn it_should_ignore_clients_that_havent_logged_in() {
 		"I haven't logged in, but send this anyway.".to_string()
 	);
 
-	let message = "This is a broadcast.".to_string();
-	client_2.login(0);
-	client_2.broadcast(1, message.clone());
-
-	client_2.wait_until(|perception|
-		if let &Some(ref perception) = perception {
-			let sender = perception.header.self_id.as_ref().unwrap().clone();
-			perception.update_items().any(|&(ref id, ref entity)| {
-				let percept = Percept::Broadcast(Broadcast {
-					sender : sender.clone(),
-					message: message.clone(),
-				});
-
-				id == &sender && entity == &percept
-			})
-		}
-		else {
-			false
-		}
-	);
-}
-
-#[test]
-fn it_should_ignore_client_sending_empty_action_before_login() {
-	let     game_service = GameService::start();
-	let mut client_1     = MockClient::start(game_service.port());
-	let mut client_2     = MockClient::start(game_service.port());
-
-	// Sending anything before logging in is invalid.
-	client_1.send_action(0, Vec::new());
-
-	// The game service should just ignore it and keep working.
 	let message = "This is a broadcast.".to_string();
 	client_2.login(0);
 	client_2.broadcast(1, message.clone());
