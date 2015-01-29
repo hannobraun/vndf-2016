6c8f9874e6b5d62b9468b93191cfa087e48322f5
Hanno Braun <mail@hannobraun.de>
2015-01-29 11:26:49 +0100

    Refactor

--- source/rust/client/src/bin/render/mod.rs
+++ source/rust/client/src/bin/render/mod.rs
@@ -1,13 +1,13 @@
-pub use self::buffer::ScreenBuffer;
-pub use self::color::Color;
-pub use self::screen::Screen;
-pub use self::util::Section;
-
-
 mod buffer;
 mod color;
 mod screen;
 mod util;
+
+
+pub use self::buffer::ScreenBuffer;
+pub use self::color::Color;
+pub use self::screen::Screen;
+pub use self::util::Section;
 
 
 pub type Pos = u16;
