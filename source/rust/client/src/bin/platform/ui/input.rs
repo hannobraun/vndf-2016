5834d2d87814c9441a4db5d05c635c670d0008e8
Hanno Braun <mail@hannobraun.de>
2015-01-29 10:54:21 +0100

    Add input processor for TextField

--- source/rust/client/src/bin/platform/ui/input.rs
+++ source/rust/client/src/bin/platform/ui/input.rs
@@ -1,3 +1,20 @@
+use super::TextField;
+
+
 pub trait ProcessInput<T> {
 	fn process_char(&mut self, element: &mut T, c: char);
+}
+
+
+pub struct TextFieldProcessor;
+
+impl ProcessInput<TextField> for TextFieldProcessor {
+	fn process_char(&mut self, element: &mut TextField, c: char) {
+		if c == '\x7f' { // Backspace
+			element.text.pop();
+		}
+		else {
+			element.text.push(c);
+		}
+	}
 }
