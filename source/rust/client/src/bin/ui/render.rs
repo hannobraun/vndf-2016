9b18bc8868a73f9a3d69e6511310bcb8e2811aa2
Hanno Braun <mail@hannobraun.de>
2015-01-30 14:51:53 +0100

    Refactor

--- source/rust/client/src/bin/ui/render.rs
+++ source/rust/client/src/bin/ui/render.rs
@@ -23,7 +23,7 @@ use super::data::{
 pub trait Render {
 	type Args;
 
-	fn render(&self, b: &mut ScreenBuffer, x: Pos, y: Pos, data: &Self::Args)
+	fn render(&self, b: &mut ScreenBuffer, x: Pos, y: Pos, args: &Self::Args)
 		-> IoResult<()>;
 }
 
