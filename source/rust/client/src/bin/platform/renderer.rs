069dcfd03c9ea153a9ca8c403cd381e547805608
Hanno Braun <mail@hannobraun.de>
2015-01-30 14:48:36 +0100

    Refactor

--- source/rust/client/src/bin/platform/renderer.rs
+++ source/rust/client/src/bin/platform/renderer.rs
@@ -44,7 +44,7 @@ impl Renderer {
 		})
 	}
 
-	pub fn render(&mut self, frame: &Frame, ui: &mut Ui) -> IoResult<()> {
+	pub fn render(&mut self, frame: &Frame, ui: &Ui) -> IoResult<()> {
 		let mut y = 0;
 
 		self.screen.cursor(None);
@@ -60,7 +60,7 @@ impl Renderer {
 	fn render_comm(
 		&mut self,
 		frame: &Frame,
-		ui   : &mut Ui,
+		ui   : &Ui,
 		y    : &mut Pos
 	) -> IoResult<()> {
 		self.comm.buffer.clear();
