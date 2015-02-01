16cc1200fe0dc2907ba14706edf67ed81309ec1f
Hanno Braun <mail@hannobraun.de>
2015-02-01 15:43:36 +0100

    Define enum for input events

--- source/rust/client/src/lib/platform.rs
+++ source/rust/client/src/lib/platform.rs
@@ -31,6 +31,26 @@ impl Input {
 }
 
 
+#[derive(Clone, Debug, RustcDecodable, RustcEncodable, Eq, PartialEq)]
+pub enum InputEvent {
+	StartBroadcast(String),
+	StopBroadcast,
+}
+
+impl InputEvent {
+	pub fn from_json(json: &str) -> DecodeResult<InputEvent> {
+		json::decode(json)
+	}
+
+	pub fn to_json(&self) -> String {
+		match json::encode(self) {
+			Ok(encoded) => encoded,
+			Err(error)  => panic!("Encoding error: {}", error)
+		}
+	}
+}
+
+
 #[derive(Debug, RustcDecodable, RustcEncodable)]
 pub struct Frame {
 	pub self_id   : String,
