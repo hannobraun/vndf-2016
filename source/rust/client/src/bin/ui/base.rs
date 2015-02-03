303d1cd38a6ddecb89e137fd2bbc8bfdf75e65ab
Hanno Braun <mail@hannobraun.de>
2015-02-03 14:54:08 +0100

    Add enum for input events

--- source/rust/client/src/bin/ui/base.rs
+++ source/rust/client/src/bin/ui/base.rs
@@ -1,3 +1,12 @@
+pub enum InputEvent {
+	Char(char),
+	CursorLeft,
+	CursorRight,
+	CursorUp,
+	CursorDown,
+}
+
+
 pub trait ProcessInput {
 	fn process_char(&mut self, c: char);
 	fn process_cursor(&mut self, direction: Direction);
