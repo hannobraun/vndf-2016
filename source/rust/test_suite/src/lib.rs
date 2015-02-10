8f38ff4cf199bbbfcb06601a7657cbf8683d1f5c
Hanno Braun <mail@hannobraun.de>
2015-02-10 14:58:18 +0100

    Remove unused exports

--- source/rust/test_suite/src/lib.rs
+++ source/rust/test_suite/src/lib.rs
@@ -10,11 +10,5 @@ extern crate common;
 extern crate game_service;
 
 
-pub use self::mock::Client as MockClient;
-pub use self::mock::GameService as MockGameService;
-pub use self::rc::Client;
-pub use self::rc::GameService;
-
-
 pub mod mock;
 pub mod rc;
