0603b368e32ae5a47346aa21cc90b7e0bac57105
Hanno Braun <mail@hannobraun.de>
2015-02-23 16:31:10 +0100

    Extract border drawing into dedicated function

--- source/rust/client/src/bin/render/util.rs
+++ source/rust/client/src/bin/render/util.rs
@@ -37,31 +37,44 @@ impl Section {
 			screen.cursor(Some(cursor));
 		}
 
-		let mut c = C::new();
-
-		c.c = '┏';
-		try!(screen.buffer().set(x +              0, y +               0, c));
-		c.c = '┓';
-		try!(screen.buffer().set(x + self.width - 1, y +               0, c));
-		c.c = '┗';
-		try!(screen.buffer().set(x +              0, y + self.height - 1, c));
-		c.c = '┛';
-		try!(screen.buffer().set(x + self.width - 1, y + self.height - 1, c));
-
-		c.c = '━';
-		for rel_x in range(1, self.width - 1) {
-			for &rel_y in [0, self.height - 1].iter() {
-				try!(screen.buffer().set(x + rel_x, y + rel_y, c));
-			}
-		}
-
-		c.c = '┃';
-		for &rel_x in [0, self.width - 1].iter() {
-			for rel_y in range(1, self.height - 1) {
-				try!(screen.buffer().set(x + rel_x, y + rel_y, c));
-			}
-		}
+		try!(border(screen.buffer(), x, y, self.width, self.height));
 
 		Ok(())
 	}
+}
+
+
+pub fn border(
+	buffer: &mut ScreenBuffer,
+	x     : Pos,
+	y     : Pos,
+	width : Pos,
+	height: Pos
+) -> IoResult<()> {
+	let mut c = C::new();
+
+	c.c = '┏';
+	try!(buffer.set(x +         0, y +          0, c));
+	c.c = '┓';
+	try!(buffer.set(x + width - 1, y +          0, c));
+	c.c = '┗';
+	try!(buffer.set(x +         0, y + height - 1, c));
+	c.c = '┛';
+	try!(buffer.set(x + width - 1, y + height - 1, c));
+
+	c.c = '━';
+	for rel_x in range(1, width - 1) {
+		for &rel_y in [0, height - 1].iter() {
+			try!(buffer.set(x + rel_x, y + rel_y, c));
+		}
+	}
+
+	c.c = '┃';
+	for &rel_x in [0, width - 1].iter() {
+		for rel_y in range(1, height - 1) {
+			try!(buffer.set(x + rel_x, y + rel_y, c));
+		}
+	}
+
+	Ok(())
 }
