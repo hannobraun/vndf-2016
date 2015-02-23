b57361802da3bc1c9ecbeb6173a5c84f0b05b8e0
Hanno Braun <mail@hannobraun.de>
2015-02-23 16:43:19 +0100

    Re-export draw_border

--- source/rust/client/src/bin/render/mod.rs
+++ source/rust/client/src/bin/render/mod.rs
@@ -8,6 +8,7 @@ pub use self::buffer::C;
 pub use self::buffer::ScreenBuffer;
 pub use self::color::Color;
 pub use self::screen::Screen;
+pub use self::util::draw_border;
 pub use self::util::Section;
 
 
