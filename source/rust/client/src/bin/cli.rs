53fbf691be39f281d60195415b1b8ed181532597
Hanno Braun <mail@hannobraun.de>
2015-02-28 12:53:24 +0100

    Extend CLI to echo any input

--- source/rust/client/src/bin/cli.rs
+++ source/rust/client/src/bin/cli.rs
@@ -1,4 +1,13 @@
-use std::old_io::IoResult;
+use std::old_io::{
+	stdin,
+	IoResult,
+};
+use std::sync::mpsc::{
+	channel,
+	Receiver,
+	TryRecvError,
+};
+use std::thread;
 use std::vec::Drain;
 
 use client::interface::{
@@ -9,17 +18,52 @@ use client::interface::{
 
 pub struct Cli {
 	events: Vec<InputEvent>,
+	lines : Receiver<String>,
 }
 
 impl Cli {
 	pub fn new() -> Cli {
+		let (sender, receiver) = channel();
+
+		thread::spawn(move || {
+			let mut stdin = stdin();
+
+			loop {
+				match stdin.read_line() {
+					Ok(line) =>
+						match sender.send(line) {
+							Ok(()) =>
+								(),
+							Err(error) =>
+								panic!("Error sending line: {:?}", error),
+						},
+					Err(error) =>
+						panic!("Error reading line: {:?}", error),
+				}
+			}
+		});
+
 		Cli {
 			events: Vec::new(),
+			lines : receiver,
 		}
 	}
 
 	pub fn update(&mut self, _: &Frame) -> IoResult<Drain<InputEvent>> {
-		// TODO: Implement
+		// TODO: Read lines and display information or generate input events as
+		//       appropriate
+		loop {
+			match self.lines.try_recv() {
+				Ok(line) => print!("{}", line),
+
+				Err(error) => match error {
+					TryRecvError::Empty =>
+						break,
+					TryRecvError::Disconnected =>
+						panic!("Channel disconnected"),
+				}
+			}
+		}
 		Ok(self.events.drain())
 	}
 }
