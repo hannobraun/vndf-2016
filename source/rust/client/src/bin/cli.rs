0e7a1c0435b5698bb4aba68df46db7b13eef4661
Hanno Braun <mail@hannobraun.de>
2015-03-02 12:37:56 +0100

    Display cursor

--- source/rust/client/src/bin/cli.rs
+++ source/rust/client/src/bin/cli.rs
@@ -128,6 +128,9 @@ impl Cli {
 				.writer(0, self.height - 1)
 				.write_str(self.input_buffer.as_slice())
 		);
+		self.screen.cursor(
+			Some((self.input_buffer.len() as u16, self.height -1))
+		);
 
 		try!(self.screen.submit());
 
