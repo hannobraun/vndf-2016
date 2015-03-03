dd3ab131f699aa17d52672dee1820469de792d1e
Hanno Braun <mail@hannobraun.de>
2015-03-03 13:40:42 +0100

    Draw a quad

--- source/rust/client/src/bin/render/renderer.rs
+++ source/rust/client/src/bin/render/renderer.rs
@@ -1,23 +1,80 @@
-use gfx;
+use gfx::{
+	self,
+	DeviceExt,
+	ToSlice,
+};
 use gfx_device_gl::{
 	GlDevice,
 	GlResources,
 };
 
 
+#[vertex_format]
+#[derive(Copy)]
+struct Vertex {
+	pos: [f32; 2],
+}
+
+
+static VERTEX_SRC: &'static [u8] = b"
+	#version 120
+
+	attribute vec2 pos;
+
+	void main() {
+		gl_Position = vec4(pos, 0.0, 1.0);
+	}
+";
+
+static FRAGMENT_SRC: &'static [u8] = b"
+	#version 120
+
+	void main() {
+		gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+	}
+";
+
+
 pub struct Renderer {
 	graphics: gfx::Graphics<GlDevice>,
 	frame   : gfx::Frame<GlResources>,
+	batch   : gfx::batch::RefBatch<Option<GlResources>>,
 }
 
 impl Renderer {
-	pub fn new(device: GlDevice, width: u32, height: u32) -> Renderer {
-		let graphics = gfx::Graphics::new(device);
-		let frame    = gfx::Frame::new(width as u16, height as u16);
+	pub fn new(mut device: GlDevice, width: u32, height: u32) -> Renderer {
+		let program = match device.link_program(VERTEX_SRC, FRAGMENT_SRC) {
+			Ok(program) => program,
+			Err(error)  => panic!("Error linking program: {:?}", error),
+		};
+
+		let mesh = device.create_mesh(&[
+			Vertex { pos: [ -0.5,  0.5 ] },
+			Vertex { pos: [ -0.5, -0.5 ] },
+			Vertex { pos: [  0.5,  0.5 ] },
+			Vertex { pos: [  0.5, -0.5 ] },
+		]);
+
+		let mut graphics = gfx::Graphics::new(device);
+		let     frame    = gfx::Frame::new(width as u16, height as u16);
+
+		let slice = mesh.to_slice(gfx::PrimitiveType::TriangleStrip);
+
+		let batch = graphics.make_batch(
+			&program,
+			&mesh,
+			slice,
+			&gfx::DrawState::new(),
+		);
+		let batch = match batch {
+			Ok(batch)  => batch,
+			Err(error) => panic!("Error making batch: {:?}", error),
+		};
 
 		Renderer {
 			graphics: graphics,
 			frame   : frame,
+			batch   : batch,
 		}
 	}
 
@@ -31,6 +88,11 @@ impl Renderer {
 			gfx::COLOR,
 			&self.frame,
 		);
+
+		match self.graphics.draw(&self.batch, &None, &self.frame) {
+			Ok(())     => (),
+			Err(error) => panic!("Error drawing graphics: {:?}", error),
+		}
 
 		self.graphics.end_frame();
 	}
