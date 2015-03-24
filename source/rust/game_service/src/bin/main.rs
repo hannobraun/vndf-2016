61d2cea92c6642b903f2c1dcbc3b0471c21af794
Hanno Braun <mail@hannobraun.de>
2015-03-24 11:43:19 +0100

    Tag TODO

--- source/rust/game_service/src/bin/main.rs
+++ source/rust/game_service/src/bin/main.rs
@@ -194,7 +194,7 @@ fn main() {
 }
 
 
-// TODO(85374284): The generated id should be guaranteed to be unique.
+// TODO(bFokG9mr): The generated id should be guaranteed to be unique.
 fn generate_id() -> String {
 	fn random_char(min: char, max: char) -> char {
 		let min = min as u8;
