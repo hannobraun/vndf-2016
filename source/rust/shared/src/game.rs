c130ae39b1c7860613629c8c161f06e0000ade2a
Hanno Braun <mail@hannobraun.de>
2015-06-15 18:06:09 +0200

    Define struct for player ships

--- source/rust/shared/src/game.rs
+++ source/rust/shared/src/game.rs
@@ -1,5 +1,15 @@
+use nalgebra::Vec2;
+
+
 #[derive(Clone, Debug, Eq, PartialEq, RustcDecodable, RustcEncodable)]
 pub struct Broadcast {
 	pub sender : String,
 	pub message: String,
+}
+
+
+#[derive(Clone, Debug, PartialEq, RustcDecodable, RustcEncodable)]
+pub struct Ship {
+	pub position: Vec2<f64>,
+	pub velocity: Vec2<f64>,
 }
