ac4913328f0576d19864538711fc3d0873d59102
Hanno Braun <mail@hannobraun.de>
2015-06-15 14:19:20 +0200

    Implement Event::is_important

--- source/rust/common/src/protocol.rs
+++ source/rust/common/src/protocol.rs
@@ -5,6 +5,23 @@ pub mod client {
 		Privileged(event::Privileged),
 	}
 
+	impl Event {
+		/// Returns whether the event should be considered important or not.
+		/// This is currently only used to determine the log level of the event.
+		pub fn is_important(&self) -> bool {
+			use self::event::Public::*;
+			use self::event::Privileged::*;
+
+			match *self {
+				Event::Public(Login)                   => true,
+				Event::Privileged(Heartbeat)           => false,
+				Event::Privileged(StartBroadcast(_))   => true,
+				Event::Privileged(StopBroadcast)       => true,
+				Event::Privileged(ScheduleManeuver(_)) => true,
+			}
+		}
+	}
+
 
 	pub mod event {
 		#[derive(Debug, PartialEq, RustcDecodable, RustcEncodable)]
