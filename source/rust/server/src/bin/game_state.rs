13385ba961a17314825dd59c8a558fb14b677f9b
Hanno Braun <mail@hannobraun.de>
2015-06-15 19:01:46 +0200

    Add support for ships to GameState

--- source/rust/server/src/bin/game_state.rs
+++ source/rust/server/src/bin/game_state.rs
@@ -1,18 +1,26 @@
 use std::collections::HashMap;
-use std::collections::hash_map::Values;
+use std::collections::hash_map::{
+	IterMut,
+	Values,
+};
 use std::net::SocketAddr;
 
-use shared::game::Broadcast;
+use shared::game::{
+	Broadcast,
+	Ship,
+};
 
 
 pub struct GameState {
 	broadcasts: HashMap<SocketAddr, Broadcast>,
+	ships     : HashMap<SocketAddr, Ship>,
 }
 
 impl GameState {
 	pub fn new() -> GameState {
 		GameState {
 			broadcasts: HashMap::new(),
+			ships     : HashMap::new(),
 		}
 	}
 
@@ -26,5 +34,18 @@ impl GameState {
 
 	pub fn broadcasts(&mut self) -> Values<SocketAddr, Broadcast> {
 		self.broadcasts.values()
+	}
+
+	pub fn create_ship(&mut self, id: SocketAddr, ship: Ship) {
+		self.ships.insert(id, ship);
+	}
+
+	pub fn ship(&mut self, id: &SocketAddr) -> &mut Ship {
+		self.ships.get_mut(id)
+			.unwrap_or_else(|| panic!("Ship not found: {}", id))
+	}
+
+	pub fn ships(&mut self) -> IterMut<SocketAddr, Ship> {
+		self.ships.iter_mut()
 	}
 }
