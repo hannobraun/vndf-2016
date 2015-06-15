9ba421e44c2aac339a070dc3ee7dd87f6355cc87
Hanno Braun <mail@hannobraun.de>
2015-06-15 14:47:30 +0200

    Move event handling code into dedicated module

--- source/rust/server/src/bin/event_handler.rs
+++ source/rust/server/src/bin/event_handler.rs
@@ -1,6 +1,20 @@
+use std::collections::HashMap;
 use std::net::SocketAddr;
 
+use nalgebra::{
+	Rot2,
+	Rotate,
+	Vec1,
+	Vec2,
+};
+use rand::random;
+use time::precise_time_s;
+
+use clients::Client;
+use common::game::Broadcast;
 use common::protocol::client;
+use common::protocol::server::Event as ServerEvent;
+use server::network::Network;
 
 
 pub struct EventHandler {
@@ -21,4 +35,133 @@ impl EventHandler {
 			self.incoming.push((address, event));
 		}
 	}
+
+	pub fn handle(
+		&mut self,
+		clients        : &mut HashMap<SocketAddr, Client>,
+		broadcasts     : &mut HashMap<SocketAddr, Broadcast>,
+		outgoing_events: &mut Vec<ServerEvent>,
+		network        : &mut Network,
+	) {
+		for (address, event) in self.incoming.drain(..) {
+			let now = precise_time_s();
+
+			let log_message = format!(
+				"Event: {:?} (address: {}; time: {})\n",
+				event, address, now,
+			);
+			if event.is_important() {
+				info!("{}", log_message);
+			}
+			else {
+				debug!("{}", log_message);
+			}
+
+			match event {
+				client::Event::Public(event) => {
+					match event {
+						client::event::Public::Login => {
+							if clients.contains_key(&address) {
+								debug!("Ignoring Login: {}\n", address);
+							}
+							else {
+								let client = Client {
+									id           : generate_id(),
+									last_active_s: precise_time_s(),
+									position     : Vec2::new(0.0, 0.0),
+									velocity     : Vec2::new(1.0, 0.0),
+								};
+
+								let login = ServerEvent::SelfId(client.id.clone());
+								network.send(
+									Some(address).into_iter(),
+									&[login],
+								);
+
+								clients.insert(address, client);
+							}
+						}
+					}
+				},
+
+				client::Event::Privileged(event) => {
+					let client = match clients.get_mut(&address) {
+						Some(client) =>
+							client,
+						None => {
+							debug!(
+								"Ignoring event: {:?} ({})\n",
+								event, address,
+							);
+							continue;
+						},
+					};
+
+					client.last_active_s = now;
+
+					match event {
+						client::event::Privileged::Heartbeat => {
+							// Nothing to do here, really, as the the time of
+							// last activity for the client has already been
+							// updated.
+						},
+						client::event::Privileged::StartBroadcast(message) => {
+							broadcasts.insert(
+								address,
+								Broadcast {
+									sender : client.id.clone(),
+									message: message,
+								}
+							);
+						},
+						client::event::Privileged::StopBroadcast => {
+							broadcasts.remove(&address);
+							outgoing_events.push(
+								ServerEvent::StopBroadcast(client.id.clone())
+							);
+						},
+						client::event::Privileged::ScheduleManeuver(angle) => {
+							let rotation = Rot2::new(Vec1::new(angle as f64));
+							let new_velocity = rotation.rotate(&Vec2::new(1.0, 0.0));
+
+							client.velocity = new_velocity;
+						},
+					}
+				}
+			}
+		}
+	}
+}
+
+
+fn generate_id() -> String {
+	fn random_char(min: char, max: char) -> char {
+		let min = min as u8;
+		let max = max as u8;
+
+		((random::<u8>() % (max + 1 - min)) + min) as char
+	}
+	fn random_letter() -> char {
+		random_char('A', 'Z')
+	}
+	fn random_letter_or_number() -> char {
+		if random() {
+			random_letter()
+		}
+		else {
+			random_char('0', '9')
+		}
+	}
+
+	let mut id = String::new();
+
+	for _ in (0u8 .. 3) {
+		id.push(random_letter());
+	}
+	id.push('-');
+	for _ in (0u8 .. 5) {
+		id.push(random_letter_or_number());
+	}
+
+	id
 }
--- source/rust/server/src/bin/main.rs
+++ source/rust/server/src/bin/main.rs
@@ -22,20 +22,10 @@ use std::collections::HashMap;
 use std::env;
 use std::thread::sleep_ms;
 
-use nalgebra::{
-	Rot2,
-	Rotate,
-	Vec1,
-	Vec2,
-};
-use rand::random;
 use time::precise_time_s;
 
 use args::Args;
 
-use clients::Client;
-use common::game::Broadcast;
-use common::protocol::client;
 use common::protocol::server::Event as ServerEvent;
 use event_handler::EventHandler;
 use server::network::Network;
@@ -61,94 +51,12 @@ fn main() {
 		trace!("Start server main loop iteration");
 
 		event_handler.receive(network.receive());
-
-		for (address, event) in event_handler.incoming.drain(..) {
-			let now = precise_time_s();
-
-			let log_message = format!(
-				"Event: {:?} (address: {}; time: {})\n",
-				event, address, now,
-			);
-			if event.is_important() {
-				info!("{}", log_message);
-			}
-			else {
-				debug!("{}", log_message);
-			}
-
-			match event {
-				client::Event::Public(event) => {
-					match event {
-						client::event::Public::Login => {
-							if clients.contains_key(&address) {
-								debug!("Ignoring Login: {}\n", address);
-							}
-							else {
-								let client = Client {
-									id           : generate_id(),
-									last_active_s: precise_time_s(),
-									position     : Vec2::new(0.0, 0.0),
-									velocity     : Vec2::new(1.0, 0.0),
-								};
-
-								let login = ServerEvent::SelfId(client.id.clone());
-								network.send(
-									Some(address).into_iter(),
-									&[login],
-								);
-
-								clients.insert(address, client);
-							}
-						}
-					}
-				},
-
-				client::Event::Privileged(event) => {
-					let client = match clients.get_mut(&address) {
-						Some(client) =>
-							client,
-						None => {
-							debug!(
-								"Ignoring event: {:?} ({})\n",
-								event, address,
-							);
-							continue;
-						},
-					};
-
-					client.last_active_s = now;
-
-					match event {
-						client::event::Privileged::Heartbeat => {
-							// Nothing to do here, really, as the the time of
-							// last activity for the client has already been
-							// updated.
-						},
-						client::event::Privileged::StartBroadcast(message) => {
-							broadcasts.insert(
-								address,
-								Broadcast {
-									sender : client.id.clone(),
-									message: message,
-								}
-							);
-						},
-						client::event::Privileged::StopBroadcast => {
-							broadcasts.remove(&address);
-							outgoing_events.push(
-								ServerEvent::StopBroadcast(client.id.clone())
-							);
-						},
-						client::event::Privileged::ScheduleManeuver(angle) => {
-							let rotation = Rot2::new(Vec1::new(angle as f64));
-							let new_velocity = rotation.rotate(&Vec2::new(1.0, 0.0));
-
-							client.velocity = new_velocity;
-						},
-					}
-				}
-			}
-		}
+		event_handler.handle(
+			&mut clients,
+			&mut broadcasts,
+			&mut outgoing_events,
+			&mut network,
+		);
 
 		let     now_s     = precise_time_s();
 		let mut to_remove = Vec::new();
@@ -211,37 +119,4 @@ fn main() {
 		//                 broadcasts can be handled immediately.
 		sleep_ms(args.sleep_ms);
 	}
-}
-
-
-fn generate_id() -> String {
-	fn random_char(min: char, max: char) -> char {
-		let min = min as u8;
-		let max = max as u8;
-
-		((random::<u8>() % (max + 1 - min)) + min) as char
-	}
-	fn random_letter() -> char {
-		random_char('A', 'Z')
-	}
-	fn random_letter_or_number() -> char {
-		if random() {
-			random_letter()
-		}
-		else {
-			random_char('0', '9')
-		}
-	}
-
-	let mut id = String::new();
-
-	for _ in (0u8 .. 3) {
-		id.push(random_letter());
-	}
-	id.push('-');
-	for _ in (0u8 .. 5) {
-		id.push(random_letter_or_number());
-	}
-
-	id
 }
