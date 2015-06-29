d007c68a24aafacf54799305cd610cfb4bed0490
Hanno Braun <mail@hannobraun.de>
2015-06-29 16:23:14 +0200

    Improve wording in TODO

--- source/rust/client/src/bin/cli.rs
+++ source/rust/client/src/bin/cli.rs
@@ -78,8 +78,8 @@ impl Cli {
 					);
 				},
 
-				// TODO: Those aren't really related to the CLI. Feels wrong to
-				//       handle them here.
+				// TODO: Those aren't really related to the CLI. It feels wrong
+				//       to handle them here.
 				KeyboardInput(Pressed, _, Some(VirtualKeyCode::Escape)) =>
 					events.push(InputEvent::Quit),
 				Closed =>
