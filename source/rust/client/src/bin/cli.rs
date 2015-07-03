e80d02df2d3cd9f930a2f913dabc12c4c7d4fa49
Hanno Braun <mail@hannobraun.de>
2015-07-03 10:26:28 +0200

    Convert TODO into comment

--- source/rust/client/src/bin/cli.rs
+++ source/rust/client/src/bin/cli.rs
@@ -78,8 +78,8 @@ impl Cli {
 					);
 				},
 
-				// TODO: Those aren't really related to the CLI. It feels wrong
-				//       to handle them here.
+				// Those events aren't really related to the CLI. It feels wrong
+				// to handle them here.
 				KeyboardInput(Pressed, _, Some(VirtualKeyCode::Escape)) =>
 					events.push(InputEvent::Quit),
 				Closed =>
