98409857f743a5fc4f13fbf8057f4c65f811af47
Hanno Braun <mail@hannobraun.de>
2015-07-17 15:09:09 +0200

    Reorder code

--- source/rust/shared/src/game.rs
+++ source/rust/shared/src/game.rs
@@ -4,15 +4,14 @@ use nalgebra::Vec2;
 pub type EntityId = u64;
 
 
-#[derive(Clone, Debug, Eq, PartialEq, RustcDecodable, RustcEncodable)]
-pub struct Broadcast {
-	pub sender : EntityId,
-	pub message: String,
-}
-
-
 #[derive(Clone, Copy, Debug, PartialEq, RustcDecodable, RustcEncodable)]
 pub struct Body {
 	pub position: Vec2<f64>,
 	pub velocity: Vec2<f64>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, RustcDecodable, RustcEncodable)]
+pub struct Broadcast {
+	pub sender : EntityId,
+	pub message: String,
 }
