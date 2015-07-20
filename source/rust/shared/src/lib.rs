733bc7ca5809707ffe869e525f208986b73b7cb2
Hanno Braun <mail@hannobraun.de>
2015-07-20 17:37:52 +0200

    Refactor

--- source/rust/shared/src/lib.rs
+++ source/rust/shared/src/lib.rs
@@ -3,18 +3,15 @@
 
 extern crate freetype;
 extern crate getopts;
-#[macro_use]
-extern crate gfx;
+#[macro_use] extern crate gfx;
 extern crate gfx_device_gl;
 extern crate glutin;
 extern crate libc;
-#[macro_use]
-extern crate log;
+#[macro_use] extern crate log;
 extern crate nalgebra;
 extern crate rand;
 extern crate rustc_serialize;
-#[macro_use]
-extern crate scan_fmt;
+#[macro_use] extern crate scan_fmt;
 extern crate time;
 
 
