5792648ef839b552d394e9aac53864997f673581
viperscape <chrisfgill@gmail.com>
2015-08-08 08:36:10 -0400

    consolidate render text into a method

--- source/rust/src/client/render/renderer.rs
+++ source/rust/src/client/render/renderer.rs
@@ -3,6 +3,7 @@ use std::fmt::Write;
 use nalgebra::{
 	cast,
 	Ortho3,
+	Vec2,
 };
 
 use client::interface::Frame;
@@ -70,35 +71,38 @@ impl Renderer {
 			self.ship_drawer.draw(&mut self.graphics, &cast(ship.position));
 
 			// draw ship id
-			let vsid: Vec<char> = ship_id.to_string().chars().collect();
-			let glyph_offset = 9;
-			let pos_offset = (glyph_offset * vsid.len())
-							  as f64 / 2.0; //centers ship id
-			for (x, c) in vsid.iter().enumerate() {
-				self.glyph_drawer.draw_at(
-					(ship.position[0] - pos_offset + ((x * glyph_offset) as f64)),
-					(ship.position[1]+20.0),
-					*c,
-					&mut self.graphics,
-				);
-			}
+			self.render_text(&ship_id.to_string(),
+								 [ship.position[0],ship.position[1]+20.0],
+								 true);
 
 			// draw ship broadcast
 			if let Some(ship_comm) = frame.broadcasts.get(&ship_id) {
-				let glyph_offset = 9;
-				let pos_offset = (glyph_offset * ship_comm.len())
-								  as f64 / 2.0; //centers ship id
-				for (x, c) in ship_comm.chars().enumerate() {
-					self.glyph_drawer.draw_at(
-						(ship.position[0] - pos_offset + ((x * glyph_offset) as f64)),
-						(ship.position[1]-40.0),
-						c,
-						&mut self.graphics,
-						);
-				}
+				self.render_text(ship_comm,
+								 [ship.position[0],ship.position[1]-40.0],
+								 true);
 			}
 		}
 
 		self.graphics.flush();
+	}
+
+	// NOTE: glyph size offset is currently hardcoded to 9px
+	fn render_text (&mut self, text: &String, pos: [f64;2], center: bool) {
+		let glyph_offset = 9;
+		let mut pos_offset: f64;
+		if center {
+			pos_offset = (glyph_offset * text.len())
+				as f64 / 2.0;
+		}
+		else { pos_offset = 0.0; }
+		
+		for (x, c) in text.chars().enumerate() {
+			self.glyph_drawer.draw_at(
+				(pos[0] - pos_offset + ((x * glyph_offset) as f64)),
+				pos[1],
+				c,
+				&mut self.graphics,
+				);
+		}
 	}
 }
