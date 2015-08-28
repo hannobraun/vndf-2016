505528157fa8faf9e4045c04cb61fb6c6eedf106
Hanno Braun <mail@hannobraun.de>
2015-08-28 17:08:16 +0200

    ide: Pass command-line arguments

--- source/rust/ide/src/vndf.rs
+++ source/rust/ide/src/vndf.rs
@@ -1,3 +1,29 @@
+use std::env;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+
+    let _program = args.next();
+    let command  = args.next();
+
+    let command = match command {
+        Some(ref command) =>
+            command.as_ref(),
+        None => {
+            print!("You need to provide an argument.\n");
+            return;
+        }
+    };
+
+    match command {
+        // TODO: Execute client
+        "client" => print!("client\n"),
+        // TODO: Execute server
+        "server" => print!("server\n"),
+        // TODO: Execute tests
+        "test"   => print!("test\n"),
+
+        _ => print!("Unknown command: {}\n", command),
+    }
 }
