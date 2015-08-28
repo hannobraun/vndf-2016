d8f9d02b70d61c2a24a680ae5e48622ff109059d
Hanno Braun <mail@hannobraun.de>
2015-08-28 18:00:51 +0200

    Build and run client from IDE

--- source/rust/ide/src/vndf.rs
+++ source/rust/ide/src/vndf.rs
@@ -1,4 +1,8 @@
 use std::env;
+use std::process::{
+    Command,
+    Stdio,
+};
 
 
 fn main() {
@@ -17,13 +21,46 @@ fn main() {
     };
 
     match command {
-        // TODO: Execute client
-        "client" => print!("client\n"),
+        "client" => run_client(args),
         // TODO: Execute server
         "server" => print!("server\n"),
         // TODO: Execute tests
         "test"   => print!("test\n"),
 
         _ => print!("Unknown command: {}\n", command),
+    }
+}
+
+
+fn run_client<I>(args: I) where I: Iterator<Item=String> {
+    let status =
+        Command::new("cargo")
+            .args(&["build", "--bin", "vndf-client"])
+            // TODO: Read path from configuration file
+            .current_dir("source/rust/vndf")
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .status()
+            .unwrap_or_else(|e| panic!("Error running client build: {}", e));
+
+    if !status.success() {
+        panic!("Client build exited with status {}", status);
+    }
+
+    // TODO: Read path from configuration file
+    let mut command = Command::new("output/cargo/debug/vndf-client");
+    for arg in args {
+        command.arg(arg);
+    }
+
+    let status =
+        command
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .status()
+            .unwrap_or_else(|e| panic!("Error running client: {}", e));
+
+    if !status.success() {
+        panic!("Client exited with status {}", status);
     }
 }
