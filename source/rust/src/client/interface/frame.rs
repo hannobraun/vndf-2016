100a0354668b77d05c7cc3db9ae080e22bb7ce44
viperscape <chrisfgill@gmail.com>
2015-08-28 11:54:47 -0400

    format with spaces

--- source/rust/src/client/interface/frame.rs
+++ source/rust/src/client/interface/frame.rs
@@ -1,81 +1,81 @@
 use std::collections::{HashMap,BTreeMap};
 
 use rustc_serialize::json::{
-	self,
-	DecodeResult,
+    self,
+    DecodeResult,
 };
 
 use shared::game::{
-	Body,
-	EntityId,
+    Body,
+    EntityId,
 };
 
 
 #[derive(Clone, Debug, RustcDecodable, RustcEncodable)]
 pub struct Frame {
-	pub ship_id    : Option<EntityId>,
-	pub game_time_s: Option<f64>,
-	pub message    : Message,
-	pub broadcasts : HashMap<EntityId, String>,
-	pub ships      : BTreeMap<EntityId, Body>,
+    pub ship_id    : Option<EntityId>,
+    pub game_time_s: Option<f64>,
+    pub message    : Message,
+    pub broadcasts : HashMap<EntityId, String>,
+    pub ships      : BTreeMap<EntityId, Body>,
 }
 
 impl Frame {
-	pub fn new() -> Frame {
-		Frame {
-			ship_id    : None,
-			game_time_s: None,
-			message    : Message::None,
-			broadcasts : HashMap::new(),
-			ships      : BTreeMap::new(),
-		}
-	}
+    pub fn new() -> Frame {
+        Frame {
+            ship_id    : None,
+            game_time_s: None,
+            message    : Message::None,
+            broadcasts : HashMap::new(),
+            ships      : BTreeMap::new(),
+        }
+    }
 
-	pub fn from_json(json: &str) -> DecodeResult<Frame> {
-		json::decode(json)
-	}
+    pub fn from_json(json: &str) -> DecodeResult<Frame> {
+        json::decode(json)
+    }
 
-	pub fn to_json(&self) -> String {
-		match json::encode(self) {
-			Ok(encoded) => encoded,
-			Err(error)  => panic!("Encoding error: {}", error)
-		}
-	}
+    pub fn to_json(&self) -> String {
+        match json::encode(self) {
+            Ok(encoded) => encoded,
+            Err(error)  => panic!("Encoding error: {}", error)
+        }
+    }
 }
 
 
 #[derive(Clone, Debug, RustcDecodable, RustcEncodable, Eq, PartialEq)]
 pub enum Message {
-	Notice(String),
-	Error(String),
-	None,
+    Notice(String),
+    Error(String),
+    None,
 }
 
 impl Message {
-	pub fn is_notice(&self) -> bool {
-		if let &Message::Notice(_) = self {
-			true
-		}
-		else {
-			false
-		}
-	}
+    pub fn is_notice(&self) -> bool {
+        if let &Message::Notice(_) = self {
+            true
+        }
+        else {
+            false
+        }
+    }
 
-	pub fn is_error(&self) -> bool {
-		if let &Message::Error(_) = self {
-			true
-		}
-		else {
-			false
-		}
-	}
+    pub fn is_error(&self) -> bool {
+    if let &Message::Error(_) = self {
+        true
+    }
+    else {
+        false
+    }
+}
 
-	pub fn is_none(&self) -> bool {
-		if let &Message::None = self {
-			true
-		}
-		else {
-			false
-		}
-	}
+pub fn is_none(&self) -> bool {
+if let &Message::None = self {
+    true
+}
+else {
+    false
+}
+        }
 }
