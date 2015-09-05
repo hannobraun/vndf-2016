e8d56925c9f86027cf04a09ec7fef5ca86902ad3
viperscape <chrisfgill@gmail.com>
2015-09-05 12:14:05 -0400

    vndf_path environment var example

--- setup.bat
+++ setup.bat
@@ -3,6 +3,7 @@ cd source/rust/ide
 cargo build
 @echo off
 (echo IDE built. Add it to your path by executing the following command:
+ echo setx VNDF_PATH "%vndf_path%"
  echo setx PATH "%%PATH%%;%vndf_path%\output\cargo\debug")
 cd ..
 cd ..
