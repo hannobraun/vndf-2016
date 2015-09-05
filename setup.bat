15481d3eab9e2ff41b4aa02c3061f0ee53d212a7
viperscape <chrisfgill@gmail.com>
2015-09-05 12:09:04 -0400

    fix for windows setup

--- setup.bat
+++ setup.bat
@@ -1,4 +1,9 @@
-cd source\rust\ide
+SET vndf_path=%CD%
+cd source/rust/ide
 cargo build
-echo "IDE built. Add it to your path by executing the following command:" &&
-echo "set PATH=%PATH%:output\rust\debug"
+@echo off
+(echo IDE built. Add it to your path by executing the following command:
+ echo setx PATH "%%PATH%%;%vndf_path%\output\cargo\debug")
+cd ..
+cd ..
+cd ..
