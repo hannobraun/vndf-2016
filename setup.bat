8babfc576c9f0b3d1231d7b29e773b3efd4a037c
viperscape <chrisfgill@gmail.com>
2015-09-07 11:44:06 -0400

    remove custom env var, as we're not using it

--- setup.bat
+++ setup.bat
@@ -3,7 +3,6 @@ cd source/rust/ide
 cargo build
 @echo off
 (echo IDE built. Add it to your path by executing the following command:
- echo setx VNDF_PATH "%vndf_path%"
  echo setx PATH "%%PATH%%;%vndf_path%\output\cargo\debug")
 cd ..
 cd ..
