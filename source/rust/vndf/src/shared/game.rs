03fc632608c4687691e25a5b9cd388e7c511e3ea
viperscape <chrisfgill@gmail.com>
2015-09-08 18:34:59 -0400

    format with spaces

--- source/rust/vndf/src/shared/game.rs
+++ source/rust/vndf/src/shared/game.rs
@@ -6,19 +6,20 @@ pub type EntityId = u64;
 
 #[derive(Clone, Copy, Debug, PartialEq, RustcDecodable, RustcEncodable)]
 pub struct Body {
-	pub position: Vec2<f64>,
-	pub velocity: Vec2<f64>,
+    pub position: Vec2<f64>,
+    pub velocity: Vec2<f64>,
+    
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, RustcDecodable, RustcEncodable)]
 pub struct Broadcast {
-	pub sender : EntityId,
-	pub message: String,
+    pub sender : EntityId,
+    pub message: String,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, RustcDecodable, RustcEncodable)]
 pub struct ManeuverData {
-	pub start_s   : f64,
-	pub duration_s: f64,
-	pub angle     : f64,
+    pub start_s   : f64,
+    pub duration_s: f64,
+    pub angle     : f64,
 }
