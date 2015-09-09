888c302f2166453bb049f0c9897bd2fb9103b349
viperscape <chrisfgill@gmail.com>
2015-09-08 20:03:44 -0400

    format with spaces

--- source/rust/vndf/src/client/graphics/draw/shapes.rs
+++ source/rust/vndf/src/client/graphics/draw/shapes.rs
@@ -6,114 +6,114 @@ use gfx_device_gl as gl;
 use nalgebra::Mat4;
 
 use client::graphics::base::{
-	Batch,
-	Graphics,
-	color,
+    Batch,
+    Graphics,
+    color,
 };
 
 
 static VERTEX_SRC: &'static [u8] = b"
-	#version 120
+        #version 120
 
-	attribute vec2 pos;
+        attribute vec2 pos;
 
-	uniform mat4  transform;
-	uniform float size;
-	uniform vec3  color;
+        uniform mat4  transform;
+        uniform float size;
+        uniform vec3  color;
 
-	varying vec3 v_color;
+        varying vec3 v_color;
 
-	void main() {
-		gl_Position = transform * vec4(pos * size, 0.0, 1.0);
-		v_color = color;
-	}
+        void main() {
+                gl_Position = transform * vec4(pos * size, 0.0, 1.0);
+                v_color = color;
+        }
 ";
 
 static FRAGMENT_SRC: &'static [u8] = b"
-	#version 120
+        #version 120
 
-	varying vec3 v_color;
+        varying vec3 v_color;
 
-	void main() {
-		gl_FragColor = vec4(v_color, 1.0);
-	}
+        void main() {
+                gl_FragColor = vec4(v_color, 1.0);
+        }
 ";
 
 
 gfx_vertex!(Vertex {
-	pos@ pos: [f32; 2],
+    pos@ pos: [f32; 2],
 });
 
 
 gfx_parameters!(Params {
-	transform@ transform: [[f32; 4]; 4],
-	size     @ size     : f32,
-	color	 @ color    : [f32; 3],
+    transform@ transform: [[f32; 4]; 4],
+    size     @ size     : f32,
+    color        @ color    : [f32; 3],
 });
 
 
 pub struct ShapeDrawer {
-	batch: Batch<Params<gl::Resources>>,
+    batch: Batch<Params<gl::Resources>>,
 }
 
 impl ShapeDrawer {
-	pub fn new(
-		graphics : &mut Graphics,
-		primitive: gfx::PrimitiveType,
-		vertices : &[Vertex]
-	) -> ShapeDrawer {
-		let batch = Batch::new(
-			graphics,
-			VERTEX_SRC, FRAGMENT_SRC,
-			primitive,
-			vertices,
-		);
+    pub fn new(
+        graphics : &mut Graphics,
+        primitive: gfx::PrimitiveType,
+        vertices : &[Vertex]
+            ) -> ShapeDrawer {
+        let batch = Batch::new(
+            graphics,
+            VERTEX_SRC, FRAGMENT_SRC,
+            primitive,
+            vertices,
+            );
 
-		ShapeDrawer {
-			batch: batch,
-		}
-	}
+        ShapeDrawer {
+            batch: batch,
+        }
+    }
 
-	pub fn ship(graphics: &mut Graphics) -> ShapeDrawer {
-		ShapeDrawer::new(
-			graphics,
-			gfx::PrimitiveType::TriangleStrip,
-			&[
-				Vertex { pos: [ -0.5, -0.5 ] },
-				Vertex { pos: [  0.5, -0.5 ] },
-				Vertex { pos: [  0.0,  0.5 ] },
-			],
-		)
-	}
+    pub fn ship(graphics: &mut Graphics) -> ShapeDrawer {
+        ShapeDrawer::new(
+            graphics,
+            gfx::PrimitiveType::TriangleStrip,
+            &[
+                Vertex { pos: [ -0.5, -0.5 ] },
+                Vertex { pos: [  0.5, -0.5 ] },
+                Vertex { pos: [  0.0,  0.5 ] },
+                ],
+            )
+    }
 
-	pub fn line(graphics: &mut Graphics) -> ShapeDrawer {
-		ShapeDrawer::new(
-			graphics,
-			gfx::PrimitiveType::LineStrip,
-			&[
-				Vertex { pos: [ 0.0, 0.0 ] },
-				Vertex { pos: [ 1.0, 0.0 ] },
-			],
-		)
-	}
+    pub fn line(graphics: &mut Graphics) -> ShapeDrawer {
+        ShapeDrawer::new(
+            graphics,
+            gfx::PrimitiveType::LineStrip,
+            &[
+                Vertex { pos: [ 0.0, 0.0 ] },
+                Vertex { pos: [ 1.0, 0.0 ] },
+                ],
+            )
+    }
 
-	pub fn draw(
-		&mut self,
-		size     : f32,
-		color    : color::Color,
-		transform: Mat4<f32>,
-		graphics : &mut Graphics,
-	) {
-		let params = Params {
-			transform: *transform.as_array(),
-			size     : size,
-			color    : color,
-			_r       : PhantomData,
-		};
+    pub fn draw(
+        &mut self,
+        size     : f32,
+        color    : color::Color,
+        transform: Mat4<f32>,
+        graphics : &mut Graphics,
+        ) {
+        let params = Params {
+            transform: *transform.as_array(),
+            size     : size,
+            color    : color,
+            _r       : PhantomData,
+        };
 
-		graphics.draw(
-			&self.batch,
-			&params,
-		);
-	}
+        graphics.draw(
+            &self.batch,
+            &params,
+            );
+    }
 }
