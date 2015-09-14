7b262b6382b21711078b66a124f2f49b81e4b521
Hanno Braun <mail@hannobraun.de>
2015-09-14 15:39:04 +0200

    Fix indentation
    @viperscape, I think there's something wrong with your editor settings.
    I've found multiple files that had mixed spaces/tabs indentation.

--- source/rust/vndf/src/client/graphics/draw/planet.rs
+++ source/rust/vndf/src/client/graphics/draw/planet.rs
@@ -17,61 +17,62 @@ pub struct PlanetDrawer {
 
 impl PlanetDrawer {
     pub fn new(graphics: &mut Graphics,
-	       font_size     : f32,
+               font_size     : f32,
                scaling_factor: f32,)
                -> PlanetDrawer {
         PlanetDrawer {
-	    scaling_factor: scaling_factor,
-            symbol_drawer: ShapeDrawer::planet(graphics),
-	    glyph_drawer: GlyphDrawer::new(graphics, font_size as u32),
-	}
+            scaling_factor: scaling_factor,
+                symbol_drawer: ShapeDrawer::planet(graphics),
+            glyph_drawer: GlyphDrawer::new(graphics, font_size as u32),
+        }
     }
 
     pub fn draw(&mut self,
                 frame: &Frame,
-		zoom: f32,
+                zoom: f32,
                 transforms: &Transforms,
                 graphics: &mut Graphics,) {
         for (id,planet) in &frame.planets {
             let transform = transforms.symbol_to_screen(cast(planet.body.position));
-	    let scale = planet.attr.size * self.scaling_factor;
-	    
-	    // draw selection behind planet
-	    if frame.select_ids.contains(id) {
+            let scale = planet.attr.size * self.scaling_factor;
+
+            // draw selection behind planet
+            if frame.select_ids.contains(id) {
                 self.symbol_drawer.draw(
-		    (scale/zoom) + 3.0, //this needs tweaking
-		    color::Colors::white(),
-		    transform,
-		    graphics,
-		    );
+                    (scale/zoom) + 3.0, //this needs tweaking
+                    color::Colors::white(),
+                    transform,
+                    graphics,
+                );
             }
-	   
-	    // draw planet
+
+            // draw planet
             self.symbol_drawer.draw(
                 scale/zoom,
                 planet.attr.color,
                 transform,
                 graphics,
-                );
+            );
 
-	    
-	    self.draw_name(
+            self.draw_name(
                 *id,
-		scale/zoom,
+                scale/zoom,
                 transform,
                 graphics,
-		);
+            );
         }
     }
 
-    fn draw_name(&mut self,
-		 id  : EntityId,
-		 size: f32,
-		 transform: Mat4<f32>,
-		 graphics : &mut Graphics,) {
-	let above = Vec2::new(0.0, size/2.0 + 5.0); // this needs tweaking
-	let _center = Vec2::new(0.0, 0.0); // TODO: perhaps draw at center instead?
-	
+    fn draw_name(
+        &mut self,
+        id  : EntityId,
+        size: f32,
+        transform: Mat4<f32>,
+        graphics : &mut Graphics,
+    ) {
+        let above = Vec2::new(0.0, size/2.0 + 5.0); // this needs tweaking
+        let _center = Vec2::new(0.0, 0.0); // TODO: perhaps draw at center instead?
+
         self.glyph_drawer.draw(
             &id.to_string(),
             above,
--- source/rust/vndf/src/shared/physics/collision.rs
+++ source/rust/vndf/src/shared/physics/collision.rs
@@ -1,7 +1,7 @@
 use nalgebra::{Pnt2,Vec2};
 use ncollide::shape::{Ball,Convex};
 use ncollide::bounding_volume::{bounding_sphere,
-				BoundingVolume,};
+                BoundingVolume,};
 
 use client::graphics::SHIP_SIZE;
 
@@ -21,26 +21,26 @@ pub struct Collider {
 
 impl Collider {
     pub fn new (kind: CollideKind) -> Collider {
-	Collider { kind: kind, }
+    Collider { kind: kind, }
     }
 
     /// builds based on current ship mesh layout (from equilateral triangle)
     // TODO: make ship mesh points as public in shapes module
     pub fn new_from_ship (scaling_factor: f32) -> Collider {
-	let size = (SHIP_SIZE/2.0 * scaling_factor) as f64;
-	let p = vec![Pnt2::new(-0.5, -0.5) * size,
-		     Pnt2::new( 0.5, -0.5) * size,
-		     Pnt2::new( 0.0,  0.5) * size,];
-	let c = Convex::new(p);
-	
-	Collider::new(CollideKind::Ship(c))
+        let size = (SHIP_SIZE/2.0 * scaling_factor) as f64;
+        let p = vec![Pnt2::new(-0.5, -0.5) * size,
+                 Pnt2::new( 0.5, -0.5) * size,
+                 Pnt2::new( 0.0,  0.5) * size,];
+        let c = Convex::new(p);
+
+        Collider::new(CollideKind::Ship(c))
     }
 
     pub fn new_from_planet (planet_size: f32, scaling_factor: f32) -> Collider {
-	let size = (planet_size/2.0 * scaling_factor) as f64;
-	let b = Ball::new(size);
-	
-	Collider::new(CollideKind::Planet(b))
+        let size = (planet_size/2.0 * scaling_factor) as f64;
+        let b = Ball::new(size);
+
+        Collider::new(CollideKind::Planet(b))
     }
 
     /// requires two colliders and their associated positions in the world
@@ -49,22 +49,22 @@ impl Collider {
                             other: (&Collider,&Vec2<f64>))
                             -> bool {
         let (other_kind, other_pos) = (&other.0.kind,other.1);
-	let mut is_collide = false;
-	match (&self.kind, other_kind) {
-	    (&CollideKind::Ship(ref c1), &CollideKind::Ship(ref c2)) => {
-                let c1_b = bounding_sphere(c1,pos);
-                let c2_b = bounding_sphere(c2,other_pos);
-                is_collide = c2_b.intersects(&c1_b);
-	    },
-	    (&CollideKind::Ship(ref c1), &CollideKind::Planet(ref c2)) => {
-		let c1_b = bounding_sphere(c1,pos);
-                let c2_b = bounding_sphere(c2,other_pos);
-                is_collide = c2_b.intersects(&c1_b);
-	    },
-	    _ => { warn!("Unsupported collision types"); }
-	}
+        let mut is_collide = false;
+        match (&self.kind, other_kind) {
+            (&CollideKind::Ship(ref c1), &CollideKind::Ship(ref c2)) => {
+                    let c1_b = bounding_sphere(c1,pos);
+                    let c2_b = bounding_sphere(c2,other_pos);
+                    is_collide = c2_b.intersects(&c1_b);
+            },
+            (&CollideKind::Ship(ref c1), &CollideKind::Planet(ref c2)) => {
+            let c1_b = bounding_sphere(c1,pos);
+                    let c2_b = bounding_sphere(c2,other_pos);
+                    is_collide = c2_b.intersects(&c1_b);
+            },
+            _ => { warn!("Unsupported collision types"); }
+        }
 
-	is_collide
+        is_collide
     }
 
     /// checks a collision between ships, while zoomed
@@ -73,13 +73,13 @@ impl Collider {
                                    other_pos: &Vec2<f64>,
                                    zoom: f32)
                                    -> bool {
-	let mut is_collide = false;
+    let mut is_collide = false;
 
         let c1 = {
             match Collider::new_from_ship(zoom).kind {
-	        CollideKind::Ship(c) => c,
-	        _ => panic!("Incompatible collidekind built"),
-	    }};
+            CollideKind::Ship(c) => c,
+            _ => panic!("Incompatible collidekind built"),
+        }};
 
         let c2 = c1.clone();
         
@@ -92,29 +92,29 @@ impl Collider {
     /// checks if position is inside collider
     /// requires this collider's position, and position of interest
     pub fn check_pos (&self, pos: &Vec2<f64>,
-		      other_pos: &Vec2<f64>,
-		      zoom: Option<f32>)
-		      -> bool {
-	let c1_b;
-	
-	match self.kind {
-	    CollideKind::Ship(ref c1) => {
-		let c = {if let Some(zoom) = zoom {
-		    match Collider::new_from_ship(zoom).kind {
-			CollideKind::Ship(c) => c,
-			_ => panic!("Incompatible collidekind built"),
-		    }
-		}
-			 else { c1.clone() }};
-                c1_b = bounding_sphere(&c,pos);
-	    },
-	    CollideKind::Planet(ref c1) => {
-		// NOTE: currently selecting a planet does not seem to need zoom factor
-		c1_b = bounding_sphere(c1,pos);
-	    },
-	}
+              other_pos: &Vec2<f64>,
+              zoom: Option<f32>)
+              -> bool {
+        let c1_b;
 
-	let c2_b = bounding_sphere(&Ball::new(1.0f64),other_pos);
-	c2_b.intersects(&c1_b)
+        match self.kind {
+            CollideKind::Ship(ref c1) => {
+            let c = {if let Some(zoom) = zoom {
+                match Collider::new_from_ship(zoom).kind {
+                CollideKind::Ship(c) => c,
+                _ => panic!("Incompatible collidekind built"),
+                }
+            }
+                 else { c1.clone() }};
+                    c1_b = bounding_sphere(&c,pos);
+            },
+            CollideKind::Planet(ref c1) => {
+            // NOTE: currently selecting a planet does not seem to need zoom factor
+            c1_b = bounding_sphere(c1,pos);
+            },
+        }
+
+        let c2_b = bounding_sphere(&Ball::new(1.0f64),other_pos);
+        c2_b.intersects(&c1_b)
     }
 }
