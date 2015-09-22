1205d9ca3474a66512aa9ada4dcfbf177f35b93c
viperscape <chrisfgill@gmail.com>
2015-09-22 14:49:32 -0400

    min/max for planet size/mass

--- source/rust/vndf/src/shared/planet.rs
+++ source/rust/vndf/src/shared/planet.rs
@@ -4,6 +4,13 @@ use nalgebra::Vec2;
 use shared::game::{Body};
 use client::graphics::base::{Color,Colors};
 
+pub const MAX_SIZE: i32 = 1000;
+pub const MIN_SIZE: i32 = 100;
+
+pub const MAX_MASS: i32 = 1000;
+pub const MIN_MASS: i32 = 100;
+
+
 #[derive(Clone, Copy, Debug, RustcDecodable, RustcEncodable, PartialEq)]
 pub struct Planet {
     pub body: Body,
@@ -18,6 +25,13 @@ pub struct PlanetAttr {
 
 impl Planet {
     pub fn new (pos: Vec2<f64>, size: f32, mass: f32, color: Color) -> Planet {
+        let size = { if (size < MIN_SIZE as f32) { MIN_SIZE as f32 }
+                     else if (size > MAX_SIZE as f32) { MAX_SIZE as f32 }
+                     else { size }};
+        let mass = { if (mass < MIN_MASS as f32) { MIN_MASS as f32 }
+                     else if (mass > MAX_MASS as f32) { MAX_MASS as f32 }
+                     else { mass }};
+        
         Planet { body: Body { position: pos,
                               velocity: Vec2::new(0.0,0.0),
                               mass: mass, },
@@ -34,16 +48,18 @@ impl PlanetBuilder {
     pub fn default () -> PlanetBuilder {
         // random placement and size
         let mut rng = thread_rng();
-        let a = sample(&mut rng, 1..1000, 4);
+        let pos = sample(&mut rng, 1..1000, 2);
+        let size = sample(&mut rng, MIN_SIZE..MAX_SIZE, 1);
+        let mass = sample(&mut rng, MIN_MASS..MAX_MASS, 1);
 
-	let x = a[0] * (if random::<bool>() { 1 }
+	let x = pos[0] * (if random::<bool>() { 1 }
 			else { -1 } );
-	let y = a[1] * (if random::<bool>() { 1 }
+	let y = pos[1] * (if random::<bool>() { 1 }
 			else { -1 } );
 	
         PlanetBuilder { planet: Planet::new(Vec2::new((x*2) as f64,y as f64),
-                                            a[2] as f32 * 2.0,
-                                            a[3] as f32 * 2.0,
+                                            size[0] as f32,
+                                            mass[0] as f32,
                                             Colors::random()) }
     }
 
