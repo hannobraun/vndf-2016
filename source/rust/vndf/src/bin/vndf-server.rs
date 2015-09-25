72774b9ae29ef5e7eb0e7b4e5ead162fb6f667e5
Hanno Braun <mail@hannobraun.de>
2015-09-25 10:37:26 +0200

    Improve legibility by adding some whitespace
    @viperscape: I've noticed you often write these very compact if
    expressions. While I think that having the whole expression in a single
    line can be fitting for very short if expressions, cramming whole
    if-else expressions into a few lines makes them quite hard to read. I
    much prefer the more spacious style, as condition, if-branch and
    else-branch are clearly demarcated.

--- source/rust/vndf/src/bin/vndf-server.rs
+++ source/rust/vndf/src/bin/vndf-server.rs
@@ -87,26 +87,40 @@ fn main() {
         'ships: for (ship_id,ship_body) in entities.bodies.iter() {
             // check only from the perspective of a ship
             if let Some(attr) = entities.attributes.get(&ship_id) {
-                if attr != &Attributes::Ship { continue 'ships }
+                if attr != &Attributes::Ship {
+                    continue 'ships
+                }
             } // if not found, likely a ship anyways
 
             let ship_coll = {
-                if let Some (coll) = entities.colliders.get(&ship_id) { coll }
-                else { warn!("No collider found for ship {}", ship_id);
-                       continue 'ships }
+                if let Some (coll) = entities.colliders.get(&ship_id) {
+                    coll
+                }
+                else {
+                    warn!("No collider found for ship {}", ship_id);
+                    continue 'ships
+                }
             };
 
             // check ship collisions with planets
             'planets: for planet_id in planets.iter() {
                 let planet_coll = {
-                    if let Some (coll) = entities.colliders.get(&planet_id) { coll }
-                    else { warn!("No collider found for planet {}", planet_id);
-                           continue 'planets }
+                    if let Some (coll) = entities.colliders.get(&planet_id) {
+                        coll
+                    }
+                    else {
+                        warn!("No collider found for planet {}", planet_id);
+                        continue 'planets
+                    }
                 };
                 let planet_body = {
-                    if let Some (body) = entities.bodies.get(&planet_id) { body }
-                    else { warn!("No body found for planet {}", planet_id);
-                           continue 'planets }
+                    if let Some (body) = entities.bodies.get(&planet_id) {
+                        body
+                    }
+                    else {
+                        warn!("No body found for planet {}", planet_id);
+                        continue 'planets
+                    }
                 };
 
                 if SphereCollider::check_collision((ship_coll,&cast(ship_body.position)),
@@ -119,16 +133,24 @@ fn main() {
 
             // check ship collisions with eachother
             'other_ships: for (ship_id2,ship_body2) in entities.bodies.iter() {
-                if ship_id == ship_id2 { continue 'other_ships }
+                if ship_id == ship_id2 {
+                    continue 'other_ships
+                }
                 
                 if let Some(attr) = entities.attributes.get(&ship_id2) {
-                    if attr != &Attributes::Ship { continue 'other_ships }
+                    if attr != &Attributes::Ship {
+                        continue 'other_ships
+                    }
                 } // if not found, likely a ship anyways
 
                 let ship_coll2 = {
-                    if let Some (coll) = entities.colliders.get(&ship_id2) { coll }
-                    else { warn!("No collider found for ship {}", ship_id2);
-                           continue 'other_ships }
+                    if let Some (coll) = entities.colliders.get(&ship_id2) {
+                        coll
+                    }
+                    else {
+                        warn!("No collider found for ship {}", ship_id2);
+                        continue 'other_ships
+                    }
                 };
                 if SphereCollider::check_collision((ship_coll,&cast(ship_body.position)),
                                                    (ship_coll2,&cast(ship_body2.position))) {
