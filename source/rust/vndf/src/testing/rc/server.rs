d3e03568072c984703f0d177cb9b56e35484dc71
Hanno Braun <mail@hannobraun.de>
2015-10-06 12:27:09 +0200

    Pass initial state to server in test code

--- source/rust/vndf/src/testing/rc/server.rs
+++ source/rust/vndf/src/testing/rc/server.rs
@@ -9,17 +9,19 @@ pub struct Server {
 }
 
 impl Server {
-    pub fn start(_initial_state: InitialState) -> Server {
-        // TODO: Configure server process to use initial state
+    pub fn start(initial_state: InitialState) -> Server {
+        let initial_state_file = "initial-state.json";
+        initial_state.to_file(initial_state_file);
 
         let port = random_port(40000, 50000);
 
         let mut process = Process::start(
             "vndf-server",
             &[
-                format!("--port={}"          , port).as_ref(),
-                format!("--client-timeout={}", 0.1 ).as_ref(),
-                format!("--sleep-duration={}", 5   ).as_ref(),
+                format!("--port={}"          , port              ).as_ref(),
+                format!("--client-timeout={}", 0.1               ).as_ref(),
+                format!("--sleep-duration={}", 5                 ).as_ref(),
+                format!("--initial-state={}" , initial_state_file).as_ref(),
             ]
         );
         process.read_stderr_line(); // Make sure it's ready
