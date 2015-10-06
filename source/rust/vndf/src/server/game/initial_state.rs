828ad3da2f32bf3a96dc7d3c4684e6fbf83c8651
Hanno Braun <mail@hannobraun.de>
2015-10-06 10:58:43 +0200

    Add intermediate representation for initial state
    Instead of randomly generating the entities directly, an immediate
    representation of the initial state is generated. The entities are then
    created from that immediate representation.
    
    This has the advantage that the intermediate representation can come
    from different sources. This means, it can be randomly generated in
    production use, or it can be tightly controlled by test code in a test
    scenario.

--- source/rust/vndf/src/server/game/initial_state.rs
+++ source/rust/vndf/src/server/game/initial_state.rs
@@ -18,29 +18,22 @@ use shared::game::{
 };
 
 
-pub struct InitialState;
+pub struct InitialState {
+    celestials    : Vec<Celestial>,
+    spawn_position: Vec2<f64>,
+}
 
 impl InitialState {
     pub fn new() -> InitialState {
-        InitialState
-    }
-
-    pub fn apply(&self, game_state: &mut GameState) {
-        let entities = &mut game_state.entities;
-
         let mut rng = thread_rng();
 
-        let planet_id = entities.create_entity()
-            .with_body(Body {
-                position: Vec2::new(0.0, 0.0),
-                velocity: Vec2::new(0.0, 0.0),
-                mass    : 1.0, // not used anywhere at the moment
-            })
-            .with_planet(Planet {
-                color: Colors::random(),
-                size : Range::new(5000.0, 10000.0).sample(&mut rng),
-            })
-            .return_id();
+        let mut celestials = Vec::new();
+
+        let planet = Celestial {
+            position: Vec2::new(0.0, 0.0),
+            size    : Range::new(5000.0, 10000.0).sample(&mut rng),
+        };
+        celestials.push(planet);
 
         let mut current_distance = 0.0;
 
@@ -52,24 +45,44 @@ impl InitialState {
             ));
             let position = rotation.rotate(&Vec2::new(current_distance, 0.0));
 
+            celestials.push(Celestial {
+                position: position,
+                size    : Range::new(500.0, 2000.0).sample(&mut rng),
+            });
+        }
+
+        let spawn_position =
+            planet.position + Vec2::new(0.0, planet.size + 500.0);
+
+        InitialState {
+            celestials    : celestials,
+            spawn_position: spawn_position,
+        }
+    }
+
+    pub fn apply(&self, game_state: &mut GameState) {
+        let entities = &mut game_state.entities;
+
+        for celestial in &self.celestials {
             entities.create_entity()
                 .with_body(Body {
-                    position: position,
+                    position: celestial.position,
                     velocity: Vec2::new(0.0, 0.0),
                     mass    : 1.0, // not used anywhere at the moment
                 })
                 .with_planet(Planet {
                     color: Colors::random(),
-                    size : Range::new(500.0, 2000.0).sample(&mut rng),
-                })
-                .return_id();
+                    size : celestial.size,
+                });
         }
 
-        game_state.spawn_position = {
-            let body   = entities.bodies[&planet_id];
-            let planet = entities.planets[&planet_id];
-
-            body.position + Vec2::new(0.0, planet.size + 500.0)
-        };
+        game_state.spawn_position = self.spawn_position;
     }
+}
+
+
+#[derive(Clone, Copy)]
+pub struct Celestial {
+    position: Vec2<f64>,
+    size    : f64,
 }
