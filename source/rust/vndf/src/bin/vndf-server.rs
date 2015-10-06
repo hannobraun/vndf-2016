f5d492a0fec0dcee3cae8b02458a60961859bd6d
Hanno Braun <mail@hannobraun.de>
2015-10-06 10:47:28 +0200

    Refactor

--- source/rust/vndf/src/bin/vndf-server.rs
+++ source/rust/vndf/src/bin/vndf-server.rs
@@ -19,7 +19,7 @@ use nalgebra::cast;
 
 use vndf::server::args::Args;
 use vndf::server::clients::Clients;
-use vndf::server::game::initial_state;
+use vndf::server::game::initial_state::InitialState;
 use vndf::server::game::state::GameState;
 use vndf::server::incoming_events::IncomingEvents;
 use vndf::server::network::Network;
@@ -43,7 +43,7 @@ fn main() {
     let mut clients    = Clients::new();
     let mut network    = Network::new(args.port);
 
-    initial_state::apply(&mut game_state);
+    InitialState::apply(&mut game_state);
 
     info!("Listening on port {}", args.port);
 
--- source/rust/vndf/src/server/game/initial_state.rs
+++ source/rust/vndf/src/server/game/initial_state.rs
@@ -18,50 +18,54 @@ use shared::game::{
 };
 
 
-pub fn apply(game_state: &mut GameState) {
-    let entities = &mut game_state.entities;
+pub struct InitialState;
 
-    let mut rng = thread_rng();
+impl InitialState {
+    pub fn apply(game_state: &mut GameState) {
+        let entities = &mut game_state.entities;
 
-    let planet_id = entities.create_entity()
-        .with_body(Body {
-            position: Vec2::new(0.0, 0.0),
-            velocity: Vec2::new(0.0, 0.0),
-            mass    : 1.0, // not used anywhere at the moment
-        })
-        .with_planet(Planet {
-            color: Colors::random(),
-            size : Range::new(5000.0, 10000.0).sample(&mut rng),
-        })
-        .return_id();
+        let mut rng = thread_rng();
 
-    let mut current_distance = 0.0;
-
-    for _ in 0 .. 5 {
-        current_distance += Range::new(15000.0, 100000.0).sample(&mut rng);
-
-        let rotation = Rot2::new(Vec1::new(
-            Range::new(0.0, 360.0).sample(&mut rng),
-        ));
-        let position = rotation.rotate(&Vec2::new(current_distance, 0.0));
-
-        entities.create_entity()
+        let planet_id = entities.create_entity()
             .with_body(Body {
-                position: position,
+                position: Vec2::new(0.0, 0.0),
                 velocity: Vec2::new(0.0, 0.0),
                 mass    : 1.0, // not used anywhere at the moment
             })
             .with_planet(Planet {
                 color: Colors::random(),
-                size : Range::new(500.0, 2000.0).sample(&mut rng),
+                size : Range::new(5000.0, 10000.0).sample(&mut rng),
             })
             .return_id();
+
+        let mut current_distance = 0.0;
+
+        for _ in 0 .. 5 {
+            current_distance += Range::new(15000.0, 100000.0).sample(&mut rng);
+
+            let rotation = Rot2::new(Vec1::new(
+                Range::new(0.0, 360.0).sample(&mut rng),
+            ));
+            let position = rotation.rotate(&Vec2::new(current_distance, 0.0));
+
+            entities.create_entity()
+                .with_body(Body {
+                    position: position,
+                    velocity: Vec2::new(0.0, 0.0),
+                    mass    : 1.0, // not used anywhere at the moment
+                })
+                .with_planet(Planet {
+                    color: Colors::random(),
+                    size : Range::new(500.0, 2000.0).sample(&mut rng),
+                })
+                .return_id();
+        }
+
+        game_state.spawn_position = {
+            let body   = entities.bodies[&planet_id];
+            let planet = entities.planets[&planet_id];
+
+            body.position + Vec2::new(0.0, planet.size + 500.0)
+        };
     }
-
-    game_state.spawn_position = {
-        let body   = entities.bodies[&planet_id];
-        let planet = entities.planets[&planet_id];
-
-        body.position + Vec2::new(0.0, planet.size + 500.0)
-    };
 }
