d9a4522285592d2c279682fcf8cb6f29120b35da
Hanno Braun <mail@hannobraun.de>
2015-10-06 11:48:35 +0200

    Convert indentation to spaces

--- source/rust/vndf/src/testing/rc/server.rs
+++ source/rust/vndf/src/testing/rc/server.rs
@@ -4,33 +4,33 @@ use testing::util::random_port;
 
 
 pub struct Server {
-	port    : u16,
-	_process: Process,
+    port    : u16,
+    _process: Process,
 }
 
 impl Server {
-	pub fn start(_initial_state: InitialState) -> Server {
-		// TODO: Configure server process to use initial state
+    pub fn start(_initial_state: InitialState) -> Server {
+        // TODO: Configure server process to use initial state
 
-		let port = random_port(40000, 50000);
+        let port = random_port(40000, 50000);
 
-		let mut process = Process::start(
-			"vndf-server",
-			&[
-				format!("--port={}"          , port).as_ref(),
-				format!("--client-timeout={}", 0.1 ).as_ref(),
-				format!("--sleep-duration={}", 5   ).as_ref(),
-			]
-		);
-		process.read_stderr_line(); // Make sure it's ready
+        let mut process = Process::start(
+            "vndf-server",
+            &[
+                format!("--port={}"          , port).as_ref(),
+                format!("--client-timeout={}", 0.1 ).as_ref(),
+                format!("--sleep-duration={}", 5   ).as_ref(),
+            ]
+        );
+        process.read_stderr_line(); // Make sure it's ready
 
-		Server {
-			port    : port,
-			_process: process,
-		}
-	}
+        Server {
+            port    : port,
+            _process: process,
+        }
+    }
 
-	pub fn port(&self) -> u16 {
-		self.port
-	}
+    pub fn port(&self) -> u16 {
+        self.port
+    }
 }
