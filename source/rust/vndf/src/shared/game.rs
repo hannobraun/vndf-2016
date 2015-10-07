b7c5560490d8969071fee6667dfd78eac17d1356
Hanno Braun <mail@hannobraun.de>
2015-10-07 12:57:45 +0200

    Refactor

--- source/rust/vndf/src/shared/game.rs
+++ source/rust/vndf/src/shared/game.rs
@@ -37,8 +37,8 @@ pub struct ManeuverData {
 #[derive(Clone, Copy, Debug, PartialEq, RustcDecodable, RustcEncodable)]
 pub struct Planet {
     pub position: Vec2<f64>,
-    pub color   : Color,
     pub size    : f64,
+    pub color   : Color,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, RustcDecodable, RustcEncodable)]
