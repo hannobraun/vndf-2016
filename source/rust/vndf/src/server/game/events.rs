58e64a64571b2258dfb0b00a79b7c3c7ccea1a16
Hanno Braun <mail@hannobraun.de>
2015-10-07 16:39:27 +0200

    Fix mixed indentation

--- source/rust/vndf/src/server/game/events.rs
+++ source/rust/vndf/src/server/game/events.rs
@@ -1,24 +1,24 @@
 use nalgebra::Vec2;
 
 use server::game::state::{
-	GameEvent,
-	GameState,
+    GameEvent,
+    GameState,
 };
 use shared::game::{
-	Body,
-	Broadcast,
-	EntityId,
-	Ship,
+    Body,
+    Broadcast,
+    EntityId,
+    Ship,
 };
 
 
 pub struct Enter;
 
 impl GameEvent for Enter {
-	type Output = EntityId;
+    type Output = EntityId;
 
-	fn execute(self, game_state: &mut GameState) -> EntityId {
-		game_state.entities.create_entity()
+    fn execute(self, game_state: &mut GameState) -> EntityId {
+        game_state.entities.create_entity()
             .with_body(Body {
                 position: game_state.spawner.position,
                 velocity: game_state.spawner.velocity,
@@ -27,36 +27,36 @@ impl GameEvent for Enter {
             })
             .with_ship(Ship)
             .return_id()
-	}
+    }
 }
 
 
 pub struct Leave {
-	pub ship_id: EntityId,
+    pub ship_id: EntityId,
 }
 
 impl GameEvent for Leave {
-	type Output = ();
+    type Output = ();
 
-	fn execute(self, game_state: &mut GameState) {
-		game_state.to_destroy.push(self.ship_id);
-	}
+    fn execute(self, game_state: &mut GameState) {
+        game_state.to_destroy.push(self.ship_id);
+    }
 }
 
 
 pub struct StartBroadcast {
-	pub ship_id: EntityId,
-	pub message: String,
+    pub ship_id: EntityId,
+    pub message: String,
 }
 
 impl GameEvent for StartBroadcast {
-	type Output = ();
+    type Output = ();
 
-	fn execute(self, game_state: &mut GameState) {
-		game_state.entities.update_entity(self.ship_id)
+    fn execute(self, game_state: &mut GameState) {
+        game_state.entities.update_entity(self.ship_id)
             .add_broadcast(Broadcast {
                 sender : self.ship_id,
                 message: self.message,
             });
-	}
+    }
 }
