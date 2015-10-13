69d7cbdf5ff9f750163eff4c9a78d880e11f705e
Hanno Braun <mail@hannobraun.de>
2015-10-13 19:40:22 +0200

    Reorder code

--- source/rust/vndf/src/shared/game/logic.rs
+++ source/rust/vndf/src/shared/game/logic.rs
@@ -9,13 +9,6 @@ use shared::game::data::{
 };
 
 
-pub fn integrate(body: &mut Body, delta_t_s: f64) {
-    body.velocity = body.velocity + body.force    * delta_t_s;
-    body.position = body.position + body.velocity * delta_t_s;
-
-    body.force = Vec2::new(0.0, 0.0);
-}
-
 pub fn apply_gravity(planet: &Planet, body: &mut Body) {
     let g = 6.674e-11; // unit: N * m^2 / kg^2
 
@@ -27,4 +20,11 @@ pub fn apply_gravity(planet: &Planet, body: &mut Body) {
         direction * -g * (planet.mass * body.mass) / distance;
 
     body.force = body.force + force;
+}
+
+pub fn integrate(body: &mut Body, delta_t_s: f64) {
+    body.velocity = body.velocity + body.force    * delta_t_s;
+    body.position = body.position + body.velocity * delta_t_s;
+
+    body.force = Vec2::new(0.0, 0.0);
 }
