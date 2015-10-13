1960e89c80a1d2a8828e733978db247d79b65e64
Hanno Braun <mail@hannobraun.de>
2015-10-13 13:36:34 +0200

    Add comment

--- source/rust/vndf/src/server/game/entities.rs
+++ source/rust/vndf/src/server/game/entities.rs
@@ -8,4 +8,9 @@ use shared::game::{
 };
 
 
+/// This modules includes auto-generated ECS code. As of this writing, the
+/// complete generation code resides in `build.rs` in the package's root
+/// directory.
+
+
 include!(concat!(env!("OUT_DIR"), "/entities.rs"));
