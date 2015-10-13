8df16678dee04d1bf35a8bf183700f63cf9a5605
Hanno Braun <mail@hannobraun.de>
2015-10-13 19:35:59 +0200

    Fix mixed indentation

--- source/rust/vndf/src/shared/game/logic.rs
+++ source/rust/vndf/src/shared/game/logic.rs
@@ -4,7 +4,7 @@ use shared::game::data::Body;
 
 
 pub fn integrate(body: &mut Body, delta_t_s: f64) {
-	body.velocity = body.velocity + body.force    * delta_t_s;
+    body.velocity = body.velocity + body.force    * delta_t_s;
     body.position = body.position + body.velocity * delta_t_s;
 
     body.force = Vec2::new(0.0, 0.0);
