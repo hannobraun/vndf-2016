7efa4522ddc04de4f1b0b0f461b5b9b252f9bdd6
Hanno Braun <mail@hannobraun.de>
2015-10-28 11:57:39 +0100

    Pass approximate server time to interpolator

--- source/rust/vndf/src/client/times.rs
+++ source/rust/vndf/src/client/times.rs
@@ -2,13 +2,15 @@ use time::precise_time_s;
 
 
 pub struct Times {
-	server_s: f64,
+	server_s                      : f64,
+	client_at_last_server_update_s: f64,
 }
 
 impl Times {
 	pub fn new() -> Self {
 		Times {
-			server_s: 0.0,
+			server_s                      : 0.0,
+			client_at_last_server_update_s: precise_time_s(),
 		}
 	}
 
@@ -17,7 +19,8 @@ impl Times {
 	}
 
 	pub fn update_server_s(&mut self, server_s: f64) {
-		self.server_s = server_s;
+		self.server_s                       = server_s;
+		self.client_at_last_server_update_s = self.client_now_s();
 	}
 
 	pub fn server_last_known_s(&self) -> f64 {
@@ -25,7 +28,13 @@ impl Times {
 	}
 
 	pub fn server_interpolated_s(&self) -> f64 {
-		// TODO: Implement
-		0.0
+		// This should give a good enough approximation, as long as the latency
+		// is somewhat constant, which of course it isn't in reality. I think it
+		// will be ok, as this isn't the kind of game that needs highly accurate
+		// synchronization between client and server. Accuracy will go down
+		// around latency spikes, but it should be fine.
+		self.server_s
+			+ self.client_now_s()
+			- self.client_at_last_server_update_s
 	}
 }
