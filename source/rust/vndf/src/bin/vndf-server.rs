7873fe69bb91e2788321590698e89256ec58540c
Hanno Braun <mail@hannobraun.de>
2015-12-04 19:11:38 +0100

    Clarify comment

--- source/rust/vndf/src/bin/vndf-server.rs
+++ source/rust/vndf/src/bin/vndf-server.rs
@@ -78,8 +78,9 @@ fn main() {
             .expect("Update event should never fail to validate");
 
         for id in game_state.destroyed_entities() {
-            // This sends the ids of all destroyed maneuvers to all clients. Not
-            // nice, but ok for now.
+            // This sends the ids of all destroyed maneuvers to all clients,
+            // even though a given maneuver is only visible to a single client
+            // in the first place. Not very nice, but ok for now.
             outgoing_events.push(
                 ServerEvent::RemoveEntity(id),
                 Recipients::All,
