136789869b0cfa6db04bd4f32339621e2b111b79
Hanno Braun <mail@hannobraun.de>
2015-12-04 19:02:00 +0100

    Convert formatting to spaces

--- source/rust/vndf/src/server/args.rs
+++ source/rust/vndf/src/server/args.rs
@@ -3,87 +3,87 @@ use std::fmt::Debug;
 use std::str::FromStr;
 
 use getopts::{
-	Matches,
-	Options,
+    Matches,
+    Options,
 };
 
 
 pub struct Args {
-	pub port            : u16,
-	pub client_timeout_s: f64,
-	pub sleep_ms        : u64,
-	pub initial_state   : Option<String>,
+    pub port            : u16,
+    pub client_timeout_s: f64,
+    pub sleep_ms        : u64,
+    pub initial_state   : Option<String>,
 }
 
 impl Args {
-	pub fn default() -> Self {
-		Args {
-			port            : 34481,
-			client_timeout_s: 5.0,
-			sleep_ms        : 500,
-			initial_state   : None,
-		}
-	}
+    pub fn default() -> Self {
+        Args {
+            port            : 34481,
+            client_timeout_s: 5.0,
+            sleep_ms        : 500,
+            initial_state   : None,
+        }
+    }
 
-	pub fn parse(cli_args: env::Args) -> Result<Self, String> {
-		let mut args = Args::default();
+    pub fn parse(cli_args: env::Args) -> Result<Self, String> {
+        let mut args = Args::default();
 
-		let mut options = Options::new();
-		options.optopt(
-			"",
-			"port",
-			"port to listen on",
-			&format!("{}", args.port),
-		);
-		options.optopt(
-			"",
-			"client-timeout",
-			"timeout after which a client is considered inactive (in seconds)",
-			&format!("{}", args.client_timeout_s),
-		);
-		options.optopt(
-			"",
-			"sleep-duration",
-			"Length of the sleep in the main loop (in milliseconds)",
-			&format!("{}", args.sleep_ms),
-		);
-		options.optopt(
-			"",
-			"initial-state",
-			"Path of the initial state file to load",
-			"PATH",
-		);
+        let mut options = Options::new();
+        options.optopt(
+            "",
+            "port",
+            "port to listen on",
+            &format!("{}", args.port),
+        );
+        options.optopt(
+            "",
+            "client-timeout",
+            "timeout after which a client is considered inactive (in seconds)",
+            &format!("{}", args.client_timeout_s),
+        );
+        options.optopt(
+            "",
+            "sleep-duration",
+            "Length of the sleep in the main loop (in milliseconds)",
+            &format!("{}", args.sleep_ms),
+        );
+        options.optopt(
+            "",
+            "initial-state",
+            "Path of the initial state file to load",
+            "PATH",
+        );
 
-		let matches = match options.parse(cli_args) {
-			Ok(matches) => matches,
-			Err(error)  => return Err(format!("{}", error)),
-		};
+        let matches = match options.parse(cli_args) {
+            Ok(matches) => matches,
+            Err(error)  => return Err(format!("{}", error)),
+        };
 
-		try!(parse_arg("port"          , &mut args.port            , &matches));
-		try!(parse_arg("client-timeout", &mut args.client_timeout_s, &matches));
-		try!(parse_arg("sleep-duration", &mut args.sleep_ms        , &matches));
+        try!(parse_arg("port"          , &mut args.port            , &matches));
+        try!(parse_arg("client-timeout", &mut args.client_timeout_s, &matches));
+        try!(parse_arg("sleep-duration", &mut args.sleep_ms        , &matches));
 
-		if let Some(initial_state) = matches.opt_str("initial-state") {
-			args.initial_state = Some(initial_state);
-		}
+        if let Some(initial_state) = matches.opt_str("initial-state") {
+            args.initial_state = Some(initial_state);
+        }
 
-		Ok(args)
-	}
+        Ok(args)
+    }
 }
 
 
 fn parse_arg<T>(name: &str, target: &mut T, matches: &Matches)
-	-> Result<(), String>
-	where
-		T     : FromStr,
-		T::Err: Debug,
+    -> Result<(), String>
+    where
+        T     : FromStr,
+        T::Err: Debug,
 {
-	if let Some(arg) = matches.opt_str(name) {
-		*target = match arg.parse() {
-			Ok(value)  => value,
-			Err(error) => return Err(format!("{:?}", error)),
-		};
-	}
+    if let Some(arg) = matches.opt_str(name) {
+        *target = match arg.parse() {
+            Ok(value)  => value,
+            Err(error) => return Err(format!("{:?}", error)),
+        };
+    }
 
-	Ok(())
+    Ok(())
 }
